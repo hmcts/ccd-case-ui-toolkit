1127dce97ebbc4117b381625040a12481d72deef
andrewf <andrzej.folga@hmcts.net>
2018-10-31 10:00:37 +0000

    RDM-3230 : Remove commented out code.

--- demo/client/src/app/case-progress-consumer.component.ts
+++ demo/client/src/app/case-progress-consumer.component.ts
@@ -15,55 +15,11 @@ export class CaseProgressConsumerComponent {
   eventTriggerId = 'enterCaseIntoLegacy';
 
   submit(event: any): void {
-    console.log('CONSUMER submit event=', event);
-    // let caseId: string = event['caseId'];
-    // let eventStatus: string = event['status'];
-    // this.router
-    //   .navigate(['case', this.jurisdictionId, this.caseTypeId, caseId])
-    //   .then(() => {
-    //     let caseReference = this.caseReferencePipe.transform(String(caseId));
-    //     if (EventStatusService.isIncomplete(eventStatus)) {
-    //       this.alertFailure(eventStatus, caseReference);
-    //     } else {
-    //       this.alertSuccess(eventStatus, caseReference);
-    //     }
-    // });
+    console.log('CaseProgressConsumerComponent submit event=', event);
+
   }
 
   cancel(event: any): void {
-    console.log('CONSUMER cancel event=', event);
-    // switch (event.status) {
-    //   case CaseEditPageComponent.NEW_FORM_DISCARD:
-    //     return this.router.navigate(['list/case']);
-    //   case CaseEditPageComponent.RESUMED_FORM_DISCARD:
-    //     return this.router.navigate([`case/${this.jurisdictionId}/${this.caseTypeId}/${this.eventTrigger.case_id}`]);
-    //   case CaseEditPageComponent.NEW_FORM_SAVE:
-    //     this.saveDraft().call(null, event.data).subscribe(_ => {
-    //       return this.router.navigate(['list/case'])
-    //         .then(() => {
-    //           this.alertService.setPreserveAlerts(true);
-    //           this.alertService.success(`The draft has been successfully saved`);
-    //         })
-    //     }, error => {
-    //       console.log('error=', error);
-    //       this.alertService.setPreserveAlerts(true);
-    //       this.alertService.warning(error.message);
-    //       this.router.navigate(['list/case'])
-    //     });
-    //     break;
-    //   case CaseEditPageComponent.RESUMED_FORM_SAVE:
-    //     this.saveDraft().call(null, event.data).subscribe(_ => {
-    //       return this.router.navigate([`case/${this.jurisdictionId}/${this.caseTypeId}/${this.eventTrigger.case_id}`])
-    //         .then(() => {
-    //           this.alertService.setPreserveAlerts(true);
-    //           this.alertService.success(`The draft has been successfully saved`);
-    //         })
-    //       }, error => {
-    //         console.log('error=', error);
-    //         this.alertService.setPreserveAlerts(true);
-    //         this.alertService.warning(error.message);
-    //     });
-    //     break;
-    // }
+    console.log('CaseProgressConsumerComponent cancel event=', event);
   }
 }
