b731a85ac1d6c203519ef9b7de8781d5ecc70f89
Karamath <karamath.mohamed@hmcts.net>
2018-11-19 17:57:59 +0000

    RDM-3238: Fix

--- src/shared/components/palette/document/write-document-field.component.ts
+++ src/shared/components/palette/document/write-document-field.component.ts
@@ -88,8 +88,8 @@ export class WriteDocumentFieldComponent extends AbstractFieldWriteComponent imp
   private createDocumentGroup(url?: string, binaryUrl?: string, filename?: string): void {
     this.uploadedDocument = this.registerControl(new FormGroup({
       document_url: new FormControl(url || ''),
-      document_binary_url: new FormControl(binaryUrl),
-      document_filename: new FormControl(filename)
+      document_binary_url: new FormControl(binaryUrl || ''),
+      document_filename: new FormControl(filename || '')
     }));
   }
 
