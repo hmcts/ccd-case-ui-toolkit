66bd15d4804be1b04a9b46e15ae89d9f85013f61
andrewf <andrzej.folga@hmcts.net>
2018-11-26 14:14:07 +0000

    RDM-3427 Start using get event trigger V2 endpoint in both mgmt web and demo app. Fix demo app by adding missing POST validate response.

--- demo/server.js
+++ demo/server.js
@@ -14,14 +14,14 @@ app.use(function (req, res) {
       res.setHeader('Content-Type', 'application/json;charset=UTF-8')
       if (req.url.indexOf('events') !== -1) {
         response = db.get('submitted_event').value();
+      } else if (req.url.indexOf('validate') !== -1) {
+        response = db.get('validate').value();
       } else {
         response = db.get('submitted_case').value();
-        }
+      }
     } else {
       if (req.url.indexOf('event-triggers/createCase') !== -1) {
         response = db.get('create-case-event-trigger').value();
-      } else if (req.url.indexOf('validate') !== -1) {
-        response = db.get('validate').value();
       } else if (req.url.indexOf('profile') !== -1) {
         response = db.get('profile').value();
       } else if (req.url.indexOf('internal/cases/1111222233334444') !== -1) {
