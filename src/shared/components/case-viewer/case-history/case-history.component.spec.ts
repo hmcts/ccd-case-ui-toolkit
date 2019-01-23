4717161de7402ad6966af96b4315459458cce7d3
andrewf <andrzej.folga@hmcts.net>
2019-01-23 13:58:57 +0000

    RDM-3675 Fix case header component test.

--- src/shared/components/case-viewer/case-history/case-history.component.spec.ts
+++ src/shared/components/case-viewer/case-history/case-history.component.spec.ts
@@ -9,7 +9,7 @@ import createSpyObj = jasmine.createSpyObj;
 import any = jasmine.any;
 import { attr } from '../../../test/helpers';
 import { CaseHistory } from '../domain';
-import { HttpError } from '../../../domain';
+import { HttpError, CaseView } from '../../../domain';
 import { OrderService, FieldsUtils } from '../../../services';
 import { PaletteUtilsModule } from '../../palette';
 import { LabelSubstitutorDirective, PlaceholderService } from '../../../directives';
@@ -33,6 +33,25 @@ describe('CaseHistoryComponent', () => {
   const $CASE_DETAIL_HEADERS = 'h3';
 
   const CASE_HISTORY: CaseHistory = createCaseHistory();
+  const CASE_VIEW: CaseView = {
+    case_id: '1',
+    case_type: {
+      id: 'TestAddressBookCase',
+      name: 'Test Address Book Case',
+      jurisdiction: {
+        id: 'TEST',
+        name: 'Test',
+      }
+    },
+    channels: [],
+    state: {
+      id: 'CaseCreated',
+      name: 'Case created'
+    },
+    tabs: [],
+    triggers: [],
+    events: []
+  };
   const FIELDS = CASE_HISTORY.tabs[1].fields;
   const SIMPLE_FIELDS = CASE_HISTORY.tabs[1].fields.slice(0, 2);
   const COMPLEX_FIELDS = CASE_HISTORY.tabs[1].fields.slice(2);
@@ -47,7 +66,8 @@ describe('CaseHistoryComponent', () => {
   let mockRoute: any = {
     snapshot: {
       data: {
-        caseHistory: CASE_HISTORY
+        caseHistory: CASE_HISTORY,
+        case: CASE_VIEW
       }
     }
   };
@@ -107,7 +127,7 @@ describe('CaseHistoryComponent', () => {
   it('should render a case header', () => {
     let header = de.query(By.directive(CaseHeaderComponent));
     expect(header).toBeTruthy();
-    expect(header.componentInstance.caseDetails).toEqual(CASE_HISTORY);
+    expect(header.componentInstance.caseDetails).toEqual(CASE_VIEW);
   });
 
   it('should render the correct case details based on show_condition', () => {
