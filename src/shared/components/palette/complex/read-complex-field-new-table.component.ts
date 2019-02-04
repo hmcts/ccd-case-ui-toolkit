3ee6feef173c30c5408345c421dfee43ed992e05
Mohammed Anas Khatri <mohammed.khatri@hmcts.net>
2019-02-04 12:21:47 +0000

    First cut of sort table and table padding

--- src/shared/components/palette/complex/read-complex-field-new-table.component.ts
+++ src/shared/components/palette/complex/read-complex-field-new-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractFieldReadComponent } from '../base-field/abstract-field-read.component';
+import { SortOrder } from './sort-order'
 
 @Component({
   selector: 'ccd-read-complex-field-new-table',
@@ -15,7 +16,9 @@ export class ReadComplexFieldNewTableComponent extends AbstractFieldReadComponen
   public columnsHorizontalLabel: any;
   public columnsHorizontalLabelVisibility: any;
   public columnsAllLabels: any;
-
+  public sortType: string;
+  public sortReverse: boolean;
+  public rows: any[] = [];
   public isHidden: boolean[] = [];
 
   ngOnInit(): void {
@@ -24,32 +27,42 @@ export class ReadComplexFieldNewTableComponent extends AbstractFieldReadComponen
       let displayContextParamter = this.caseField.display_context_parameter.trim();
       let result: string = displayContextParamter.replace('#TABLE(', '');
       this.columns = result.replace(')', '').split(',');
+
       let labels = '';
       let labelsVertical: { [k: string]: any } = {};
       let labelsHorizontal: { [k: string]: any } = {};
-      let labelsHorizontalVisibility: { [k: string]: any } = {};
       let allLabels: { [k: string]: any } = {};
-      for (let obj of this.caseField.value) {
-        this.isHidden.push(true);
-      }
-      for (let obj of this.caseField.field_type.complex_fields) {
-        labelsVertical[obj.id] = {label: obj.label, type: obj.field_type.type};
-        allLabels[obj.id] = {label: obj.label, type: obj.field_type.type};
-      }
-
-      for (let id of this.columns) {
-        let horizontalColumnFields: any;
-        let verticalColumnFields: any;
-        labelsHorizontal[id.trim()] = allLabels[id.trim()];
-        labelsHorizontalVisibility[id.trim()] = true;
-        delete labelsVertical[id.trim()];
-      }
+      this.populateCaseFieldValuesIntoRows();
+      this.populateLabels(labelsVertical, allLabels);
+      this.populateHorizontalLabels(labelsHorizontal, allLabels, labelsVertical);
 
       this.columnsLabel = labels.split(',');
       this.columnsVerticalLabel = labelsVertical;
       this.columnsHorizontalLabel = labelsHorizontal;
-      this.columnsHorizontalLabelVisibility = labelsHorizontalVisibility;
       this.columnsAllLabels = allLabels;
+    }
+  }
+
+  private populateHorizontalLabels(labelsHorizontal: { [p: string]: any }, allLabels: { [p: string]: any }, labelsVertical: { [p: string]: any }) {
+
+    for (let id of this.columns) {
+      labelsHorizontal[id.trim()] = allLabels[id.trim()];
+      labelsHorizontal[id.trim()].sortOrder = SortOrder.UNSORTED;
+      delete labelsVertical[id.trim()];
+    }
+  }
+
+  private populateLabels(labelsVertical: { [p: string]: any }, allLabels: { [p: string]: any }) {
+    for (let obj of this.caseField.field_type.complex_fields) {
+      labelsVertical[obj.id] = {label: obj.label, type: obj.field_type.type};
+      allLabels[obj.id] = {label: obj.label, type: obj.field_type.type};
+    }
+  }
+
+  private populateCaseFieldValuesIntoRows() {
+    for (let obj of this.caseField.value) {
+      this.rows.push(obj.value);
+      this.isHidden.push(true);
     }
   }
 
@@ -59,6 +72,55 @@ export class ReadComplexFieldNewTableComponent extends AbstractFieldReadComponen
     } else {
       return 'img/accordion-minus.png';
     }
+  }
+
+  sortRowsByColumns(column) {
+    let shouldSortInAscendingOrder = this.columnsHorizontalLabel[column].sortOrder === SortOrder.UNSORTED || this.columnsHorizontalLabel[column].sortOrder === SortOrder.DESCENDING;
+    switch (this.columnsHorizontalLabel[column].type) {
+      case 'Number':
+      case 'MoneyGBP': {
+        if (shouldSortInAscendingOrder) {
+          this.rows.sort((a, b) => a[column] - b[column]);
+          this.columnsHorizontalLabel[column].sortOrder = SortOrder.ASCENDING;
+        } else {
+          this.rows.sort((a, b) => b[column] - a[column]);
+          this.columnsHorizontalLabel[column].sortOrder = SortOrder.DESCENDING;
+        }
+        break;
+      }
+      case 'Text':
+      case 'TextArea':
+      case 'Email':
+      case 'Date':
+      case 'DateTime':
+      case 'Label':
+      case 'Postcode':
+      case 'YesOrNo':
+      case 'PhoneUK':
+      case 'FixedList': {
+        if (shouldSortInAscendingOrder) {
+          this.rows.sort((a, b) => a[column] < b[column] ? -1 : a[column] > b[column] ? 1 : 0);
+          this.columnsHorizontalLabel[column].sortOrder = SortOrder.ASCENDING;
+        } else {
+          this.rows.sort((a, b) => a[column] < b[column] ? 1 : a[column] > b[column] ? -1 : 0);
+          this.columnsHorizontalLabel[column].sortOrder = SortOrder.DESCENDING;
+        }
+      }
+    }
+  }
+
+  private isSortAscending(column: any): boolean {
+    return !    (column.sortOrder === SortOrder.UNSORTED || column.sortOrder === SortOrder.DESCENDING);
+  }
+
+
+  sortWidget(column: any) {
+    return this.isSortAscending(column) ? '&#9660;' : '&#9650;';
+  }
+
+
+  trackByIndex(index: number, obj: any): any {
+    return index;
   }
 
 }
