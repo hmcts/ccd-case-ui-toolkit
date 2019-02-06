6e5859fab9af9c74cf2bbef2064b0d7e8ef47183
Ashley Noronha <ashleydeannoronha@gmail.com>
2019-02-06 14:42:34 +0000

    run unit tests on chrome

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -30,7 +30,14 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    browsers: ["PhantomJS"],
+    browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+    // browsers: ["PhantomJS"],
     autoWatch: false,
     singleRun: true
   });
