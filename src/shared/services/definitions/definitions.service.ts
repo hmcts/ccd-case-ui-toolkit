74d9d417e7e5190253c9689af86122abadf23a09
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-02-08 15:46:53 +0000

    RDM-3915 Extracted case filters from mgmt web as is
    #
    # Please provide your commit's subject above this line.
    #
    # Guideline (http://chris.beams.io/posts/git-commit/)
    #
    #   1. Separate subject from body with a blank line
    #   2. Limit the subject line to 50 characters
    #   3. Capitalize the first letter subject line
    #   4. Do not end the subject line with a period
    #   5. Use the imperative mood in the subject line
    #   6. Wrap the body at 72 characters
    #   7. Use the body to explain what and why vs. how
    #
    # Description of your commit should go below. Make sure to leave
    # one empty line after your description.
    #
    
    
    #
    # Finally, please add a reference to other issues or pull
    # request on GitHub here below.
    #
    # Example
    # -------
    #
    #   Resolves #1
    #   Resolves #2
    #   Closes #3
    #
    # Accepted keywords are:
    #
    #   * Resolves
    #   * Closes
    #   * See also
    #

---
+++ src/shared/services/definitions/definitions.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AbstractAppConfig as AppConfig } from '../../../app.config';
+import { HttpService } from '../http/http.service';
+import { CaseTypeLite } from '../../domain/definition/case-type-lite.model';
+import { Jurisdiction } from '../../domain/definition/jurisdiction.model';
+
+@Injectable()
+export class DefinitionsService {
+
+  constructor(private http: HttpService, private appConfig: AppConfig) {}
+
+  getCaseTypes(jurisdictionId: string, access: string): Observable<CaseTypeLite[]> {
+    const url = this.appConfig.getApiUrl()
+      + `/caseworkers/:uid`
+      + `/jurisdictions/${jurisdictionId}`
+      + `/case-types?access=${access}`;
+
+    return this.http
+      .get(url)
+      .map(response => response.json());
+  }
+
+  getJurisdictions(access: string): Observable<Jurisdiction[]> {
+    const url = this.appConfig.getApiUrl()
+      + `/caseworkers/:uid`
+      + `/jurisdictions?access=${access}`;
+
+    return this.http
+      .get(url)
+      .map(response => response.json());
+  }
+}
