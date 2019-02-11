77e8abf8c9057803948d9efefd5b98b88656b31c
andrewf <andrzej.folga@hmcts.net>
2019-02-11 11:06:52 +0000

    RDM-3830 Add Consumer component for serach filters in demo app.

---
+++ demo/src/app/search-filters-consumer.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Jurisdiction, CaseType } from '@hmcts/ccd-case-ui-toolkit';
+
+@Component({
+  selector: 'search-filters-consumer',
+  template: `<div class="container-fluid">
+                <ccd-search-filters [jurisdictions]="jurisdictions"
+                                    [autoApply]="true"
+                                    (onApply)="applied()"
+                                    (onReset)="reset()"
+                                    (onJuridiction)="jurisdictionSelected()"></ccd-search-filters>
+             </div>`
+})
+export class SearchFiltersConsumerComponent implements OnInit {
+
+    readonly CASE_TYPE_1: CaseType = {
+        id: 'CT0',
+        name: 'Case type 0',
+        description: '',
+        states: [],
+        events: [],
+        case_fields: [],
+        jurisdiction: null
+      };
+
+    readonly CASE_TYPE_2: CaseType = {
+        id: 'CT2',
+        name: 'Case type 2',
+        description: '',
+        states: [],
+        events: [],
+        case_fields: [],
+        jurisdiction: null
+      };
+
+    readonly CASE_TYPE_3: CaseType = {
+        id: 'CT3',
+        name: 'Case type 3',
+        description: '',
+        states: [],
+        events: [],
+        case_fields: [],
+        jurisdiction: null
+      };
+
+    readonly JURISDICTION_1: Jurisdiction = {
+        id: 'J1',
+        name: 'Jurisdiction 1',
+        description: '',
+        caseTypes: [this.CASE_TYPE_1, this.CASE_TYPE_2]
+      };
+
+    readonly JURISDICTION_2: Jurisdiction = {
+        id: 'J2',
+        name: 'Jurisdiction 2',
+        description: '',
+        caseTypes: [this.CASE_TYPE_3]
+      };
+
+    jurisdictions: Jurisdiction[];
+
+    constructor(
+        private route: ActivatedRoute
+    ) {}
+
+    ngOnInit() {
+        this.jurisdictions = [this.JURISDICTION_1, this.JURISDICTION_2];
+    }
+
+    applied(selected) {
+        console.log('selected:', selected);
+    }
+
+    reset() {
+        console.log('reset:');
+    }
+
+    jurisdictionSelected(selectedJurisdiction) {
+        console.log('selected jurisdiction:', selectedJurisdiction);
+    }
+}
