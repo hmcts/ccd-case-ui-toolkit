829508475a5bd18e8a58a0a6c0537529ec626384
Ashley Noronha <ashleydeannoronha@gmail.com>
2019-02-21 10:28:55 +0000

    run rx-js polling outside angular zone

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -64,6 +64,7 @@ import 'core-js/es7/reflect';
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js/dist/zone-patch-rxjs';
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
