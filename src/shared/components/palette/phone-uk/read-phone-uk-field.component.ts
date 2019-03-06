4229f985a50aab94fe7d383d1505ab810b1a6645
Karamath <karamath.mohamed@hmcts.net>
2019-03-06 14:33:44 +0000

    RDM-3699: Header menu items styling improved

--- src/shared/components/palette/phone-uk/read-phone-uk-field.component.ts
+++ src/shared/components/palette/phone-uk/read-phone-uk-field.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ccd-read-phone-uk-field',
-  template: `{{caseField.value}}`
+  template: `<span class="text-16">{{caseField.value}}</span>`
 })
 export class ReadPhoneUKFieldComponent extends AbstractFieldReadComponent {}
