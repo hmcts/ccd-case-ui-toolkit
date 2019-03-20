8ee6b07840e9a69be1cd6b7965cc16e50c19fe67
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-03-20 12:35:07 +0000

    RDM-4129 services and pipes

--- src/shared/pipes/pipes.module.ts
+++ src/shared/pipes/pipes.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CaseReferencePipe } from './case-reference/case-reference.pipe';
+import { SortSearchResultPipe } from './search-result/sorting/sort-search-result.pipe';
 
 @NgModule({
   imports: [
@@ -8,9 +9,11 @@ import { CaseReferencePipe } from './case-reference/case-reference.pipe';
   ],
   declarations: [
     CaseReferencePipe,
+    SortSearchResultPipe,
   ],
   exports: [
     CaseReferencePipe,
+    SortSearchResultPipe,
   ]
 })
 export class PipesModule {}
