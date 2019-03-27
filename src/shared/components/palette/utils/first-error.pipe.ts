fae2a5ad95c60ffd268e22588aa8f6b6ab8a60fa
MKaramath <37309023+MKaramath@users.noreply.github.com>
2019-03-27 12:29:16 +0000

    RDM-3325-1: CCD validation rules containing whitespace characters (#271)
    * RDM-3699: Address complex header styling
    
    * RDM-3325-1: Error message updated
    
    * RDM-3325-1: Updated the error display message
    
    * updated release notes

--- src/shared/components/palette/utils/first-error.pipe.ts
+++ src/shared/components/palette/utils/first-error.pipe.ts
@@ -18,6 +18,12 @@ export class FirstErrorPipe implements PipeTransform {
     }
     if (keys[0] ===  'required') {
       return 'This field is required';
+    } else if (keys[0] ===  'pattern') {
+      return 'The data entered is not valid for this type of field.';
+    } else if (keys[0] ===  'minlength') {
+      return 'Required minimum length';
+    } else if (keys[0] ===  'maxlength') {
+      return 'Exceeds maximum length';
     }
     return value[keys[0]];
   }
