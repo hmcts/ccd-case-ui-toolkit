4c759723f01af534a11afd49d1fbcd99d38d0f31
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-04-15 12:04:24 +0100

    RDM-4131 fixed test

--- src/shared/components/search-result/search-result.component.spec.ts
+++ src/shared/components/search-result/search-result.component.spec.ts
@@ -321,7 +321,6 @@ describe('SearchResultComponent', () => {
         jurisdiction: JURISDICTION,
         caseType: CASE_TYPE,
         caseState: CASE_STATE,
-        formGroup: jasmine.any(Object),
         metadataFields: METADATA_FIELDS,
         page: 2
       };
