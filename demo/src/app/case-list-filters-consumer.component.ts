e5e2f019729ab70e83a3c32fdb6da5a27d7fff85
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-05-17 14:00:41 +0100

    RDM-4537 made reset work

--- demo/src/app/case-list-filters-consumer.component.ts
+++ demo/src/app/case-list-filters-consumer.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
- @Component({
-  selector: 'case-list-filters-consumer',
-  templateUrl: './case-list-filters-consumer.component.html',
-  styleUrls: ['./elements-documentation.scss']
+@Component({
+   selector: 'case-list-filters-consumer',
+   templateUrl: './case-list-filters-consumer.component.html',
+   styleUrls: ['./elements-documentation.scss']
 })
 export class CaseListFiltersConsumerComponent implements OnInit {
 
@@ -18,20 +19,28 @@ export class CaseListFiltersConsumerComponent implements OnInit {
 
    json: any;
 
+   constructor(
+      private router: Router,
+   ) { }
+
    ngOnInit() {
       this.defaults = {
          jurisdiction_id: 'J1',
          case_type_id: 'CT1',
          state_id: 'S1'
-     };
-     this.json = JSON;
+      };
+      this.json = JSON;
    }
 
-   applied(selected) {
-      console.log('selected:', selected);
+   applied(arg: any) {
+      this.router.navigate(['/case/case-list-filters'], {
+         queryParams: arg.queryParams
+      });
+      console.log('selected:', arg);
    }
 
    reset() {
+      this.router.navigate(['/case/case-list-filters']);
       console.log('reset');
    }
 
