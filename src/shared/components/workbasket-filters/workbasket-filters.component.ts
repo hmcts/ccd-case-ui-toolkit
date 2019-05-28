f76f3c03f17e874f3c5c5e151a177ecf6a5a900d
Ashley Noronha <ashleydeannoronha@gmail.com>
2019-05-28 09:31:09 +0100

    copy changes over from man-web commit

--- src/shared/components/workbasket-filters/workbasket-filters.component.ts
+++ src/shared/components/workbasket-filters/workbasket-filters.component.ts
@@ -140,16 +140,14 @@ export class WorkbasketFiltersComponent implements OnInit {
             .sort(this.orderService.sortAsc);
           const formValue = this.windowService.getLocalStorage(FORM_GROUP_VAL_LOC_STORAGE);
 
-          workbasketInputs.forEach(item => {
-            if (item.field.elementPath) {
-              item.field.id = item.field.id + '.' + item.field.elementPath;
-            }
+          for (let i = 0; i < workbasketInputs.length; i++) {
+            let item = workbasketInputs[i];
             item.field.label = item.label;
             if (formValue) {
               const searchFormValueObject = JSON.parse(formValue);
               item.field.value = searchFormValueObject[item.field.id];
             }
-          });
+          }
 
         }, error => {
           console.log('Workbasket input fields request will be discarded reason: ', error.message);
