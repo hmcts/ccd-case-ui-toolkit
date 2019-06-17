0082991e71b21bbcbd0eb97e4eb9e8097faa4771
Sateesh Mathangi <a-smathangi@hotels.com>
2019-06-17 14:16:42 +0100

    RDM-4685 - automate toolkit release

---
+++ scripts/create-release-tag.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+createTag() {
+    git ls-remote --tags origin $TAG_NAME 2>/dev/null | grep $TAG_NAME 1>/dev/null
+    if [ "$?" == 0 ]; then
+       echo Tag $TAG_NAME already exists!;
+    else
+       git tag $TAG_NAME -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
+       git push https://${GH_TOKEN}@github.com/covfefe-spikes/nodejs-release-spike.git --tags >/dev/null
+    fi
+}
+
+git config user.name "Travis CI"
+git config user.email "travis@travis-ci.org"
+
+export PACKAGE_VERSION=$(jq -r ".version" package.json)
+export TAG_NAME="v$PACKAGE_VERSION"
+
+if [[ "$TRAVIS_PULL_REQUEST" != "false" && "$PACKAGE_VERSION" != *RDM*-prerelease ]]; then
+      echo "PR branch must have a prerelease version with RDM number to tag (Eg: 1.2.3-RDM-4685-prerelease)"
+    else
+     createTag
+fi
