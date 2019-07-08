02f21fe67776ca896a71e9d4659e62114a78a49e
andrewf <andrzej.folga@hmcts.net>
2019-07-08 10:32:15 +0100

    RDM-5044 Fix first column interpolation. Injext placeholder service to programmatically interpolate value.

--- src/shared/components/search-result/search-result.component.ts
+++ src/shared/components/search-result/search-result.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, Input, Output, EventEmitter, SimpleChanges, OnInit } from '@angular/core';
+import { Component, OnChanges, Input, Output, EventEmitter, SimpleChanges } from '@angular/core';
 import { DisplayMode, Jurisdiction, CaseType, CaseState, SearchResultView, SearchResultViewColumn,
   SearchResultViewItem, CaseField, DRAFT_PREFIX, PaginationMetadata, SortParameters,
   SearchResultViewItemComparator, SortOrder } from '../../domain';
@@ -6,13 +6,14 @@ import { FormGroup } from '@angular/forms';
 import { ActivityService, SearchResultViewItemComparatorFactory } from '../../services';
 import { CaseReferencePipe } from '../../pipes';
 import { AbstractAppConfig } from '../../../app.config';
+import { PlaceholderService } from '../../directives';
 
 @Component({
   selector: 'ccd-search-result',
   templateUrl: './search-result.component.html',
   styleUrls: ['./search-result.component.scss']
 })
-export class SearchResultComponent implements OnChanges, OnInit {
+export class SearchResultComponent implements OnChanges {
 
   public static readonly PARAM_JURISDICTION = 'jurisdiction';
   public static readonly PARAM_CASE_TYPE = 'case-type';
@@ -51,8 +52,6 @@ export class SearchResultComponent implements OnChanges, OnInit {
 
   hideRows: boolean;
 
-  firstColumnFields: CaseField[] = [];
-
   selected: {
     init?: boolean,
     jurisdiction?: Jurisdiction,
@@ -71,7 +70,8 @@ export class SearchResultComponent implements OnChanges, OnInit {
     searchResultViewItemComparatorFactory: SearchResultViewItemComparatorFactory,
     appConfig: AbstractAppConfig,
     private activityService: ActivityService,
-    private caseReferencePipe: CaseReferencePipe
+    private caseReferencePipe: CaseReferencePipe,
+    private placeholderService: PlaceholderService
   ) {
     this.searchResultViewItemComparatorFactory = searchResultViewItemComparatorFactory;
     this.paginationPageSize = appConfig.getPaginationPageSize();
@@ -100,23 +100,6 @@ export class SearchResultComponent implements OnChanges, OnInit {
     if (changes['page']) {
       this.selected.page = (changes['page']).currentValue;
     }
-  }
-
-  ngOnInit() {
-    // Ensure first column field values are resolved by label interpolation before the view is rendered.
-    this.populateFirstColumnFields();
-  }
-
-  populateFirstColumnFields() {
-    this.resultView.results.forEach(result => {
-      this.firstColumnFields.push(this.getColumnFieldWithPrefix(
-        result.columns[this.resultView.columns[0].case_field_id],
-        result));
-    });
-  }
-
-  getFirstColumnField(rowIndex) {
-    return this.firstColumnFields[rowIndex];
   }
 
   /**
@@ -180,8 +163,9 @@ export class SearchResultComponent implements OnChanges, OnInit {
     });
   }
 
-  getColumnFieldWithPrefix(col: CaseField, result: SearchResultViewItem): CaseField {
+  getColumnsWithPrefix(col: CaseField, result: SearchResultViewItem): CaseField {
     col.value = this.draftPrefixOrGet(col, result);
+    col.value = this.placeholderService.resolvePlaceholders(result.case_fields, col.value);
     return col;
   }
 
