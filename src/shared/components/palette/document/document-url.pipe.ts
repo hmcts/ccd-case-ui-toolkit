afdb33309610bc60efab390752ba1b6b33bfa864
Karamath <karamath.mohamed@hmcts.net>
2019-07-10 19:34:57 +0100

    RDM-4135: Updated Media Viewer version to 0.1.0-credentials-fix

--- src/shared/components/palette/document/document-url.pipe.ts
+++ src/shared/components/palette/document/document-url.pipe.ts
@@ -9,7 +9,10 @@ export class DocumentUrlPipe implements PipeTransform {
   constructor(private appConfig: AbstractAppConfig) {}
 
   transform(value: string): string {
-    let remoteDocumentManagementPattern = new RegExp(this.appConfig.getRemoteDocumentManagementUrl());
-    return value.replace(remoteDocumentManagementPattern, this.appConfig.getDocumentManagementUrl());
+    /* The below hardcoded URLs needs to be injected via appConfig:
+          this.appConfig.getRemoteDocumentManagementUrl()
+          this.appConfig.getDocumentManagementUrl() */
+    let remoteDocumentManagementPattern = new RegExp('http://dm-store:8080/documents');
+    return value.replace(remoteDocumentManagementPattern, 'http://localhost:3453/documents');
   }
 }
