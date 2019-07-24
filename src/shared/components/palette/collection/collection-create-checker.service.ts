52d034313db779546d201f5d24830f51dfb44cf6
tzarmango <fatih.ozceylan@gmail.com>
2019-07-24 16:46:46 +0100

    RDM-5310 Fix for Proceed button does not render correctly

---
+++ src/shared/components/palette/collection/collection-create-checker.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { CaseField } from '../../../domain/definition';
+import { Profile } from '../../../domain/profile';
+
+@Injectable()
+export class CollectionCreateCheckerService {
+
+  public setDisplayContextForChildren(caseField: CaseField, caseFields: CaseField[], profile: Profile) {
+    let children = this.getCaseFieldChildren(caseField);
+
+    if (children && children.length > 0) {
+      children.forEach(child => {
+        if (!!profile.user.idam.roles.find(role => this.hasCreateAccess(child, role))) {
+          child.display_context = caseField.display_context;
+        }
+      });
+    }
+  }
+
+  private getCaseFieldChildren(caseField: CaseField): CaseField[] {
+    let childrenCaseFields = [];
+    if (this.isCollection(caseField)) {
+      childrenCaseFields = caseField.field_type.collection_field_type.complex_fields || [];
+    } else if (this.isComplex(caseField)) {
+      childrenCaseFields = caseField.field_type.complex_fields || [];
+    }
+    return childrenCaseFields;
+  }
+
+  private isComplex(case_field: CaseField) {
+    return case_field.field_type.type === 'Complex';
+  }
+
+  private isCollection(case_field: CaseField) {
+    return case_field.field_type.type === 'Collection';
+  }
+
+  private hasCreateAccess(caseField: CaseField, role: any) {
+    return !!caseField.acls.find( acl => acl.role === role && acl.create === true);
+  }
+}
