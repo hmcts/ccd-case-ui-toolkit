1107445d17ebf4ade4494643d158f5eda6c5b341
tzarmango <fatih.ozceylan@gmail.com>
2019-08-14 12:22:39 +0100

    RDM-5635 Addressing PR comments

--- src/shared/components/palette/base-field/field-read.component.ts
+++ src/shared/components/palette/base-field/field-read.component.ts
@@ -4,6 +4,7 @@ import { AbstractFieldReadComponent } from './abstract-field-read.component';
 import { CaseField } from '../../../domain/definition/case-field.model';
 import { FormGroup } from '@angular/forms';
 import { plainToClassFromExist } from 'class-transformer';
+import { FieldsUtils } from '../../../services/fields';
 
 @Component({
   selector: 'ccd-field-read',
@@ -29,9 +30,7 @@ export class FieldReadComponent extends AbstractFieldReadComponent implements On
 
   ngOnInit(): void {
     // Ensure this.caseField is actually a CaseField instance even if instantiated with {}
-    if (!(this.caseField instanceof CaseField)) {
-      this.caseField = plainToClassFromExist(new CaseField(), this.caseField);
-    }
+    this.caseField = FieldsUtils.convertToCaseField(this.caseField);
     // Ensure all field values are resolved by label interpolation before the component is fully initialised.
     Promise.resolve(null).then(() => {
       let componentClass = this.paletteService.getFieldComponentClass(this.caseField, false);
@@ -49,5 +48,4 @@ export class FieldReadComponent extends AbstractFieldReadComponent implements On
       this.fieldContainer.insert(component.hostView);
     });
   }
-
 }
--- src/shared/services/fields/fields.utils.ts
+++ src/shared/services/fields/fields.utils.ts
@@ -5,6 +5,7 @@ import { DatePipe } from '../../components/palette/utils';
 import { WizardPage } from '../../components/case-editor/domain';
 import { Predicate } from '../../domain/predicate.model';
 import { CaseView } from '../../domain/case-view';
+import { plainToClassFromExist } from 'class-transformer';
 
 // @dynamic
 @Injectable()
@@ -13,6 +14,13 @@ export class FieldsUtils {
   private static readonly currencyPipe: CurrencyPipe = new CurrencyPipe('en-GB');
   private static readonly datePipe: DatePipe = new DatePipe();
   public static readonly LABEL_SUFFIX = '-LABEL';
+
+  public static convertToCaseField(obj: any): CaseField {
+    if (!(obj instanceof CaseField)) {
+      return plainToClassFromExist(new CaseField(), obj);
+    }
+    return obj;
+  }
 
   public static toValuesMap(caseFields: CaseField[]): any {
     let valueMap = {};
