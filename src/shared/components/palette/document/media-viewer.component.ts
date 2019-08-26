fcb137b9d43cf876802f0359c391f4572065aa4a
mutlu <mutlu.sancaktutar@hmcts.net>
2019-08-26 04:28:56 +0100

    safe guard against server side execution of browser-only code

--- src/shared/components/palette/document/media-viewer.component.ts
+++ src/shared/components/palette/document/media-viewer.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { WindowService } from '../../../services/window';
 
 const MEDIA_VIEWER = 'media-viewer';
@@ -15,10 +16,15 @@ export class MediaViewerComponent implements OnInit {
 
   public constructor(public renderer: Renderer2,
                      private el: ElementRef,
-                     private windowService: WindowService) {
+                     private windowService: WindowService,
+                     @Inject(PLATFORM_ID) private platformId: Object) {
   }
 
   ngOnInit() {
+    console.log("Platform ID is " + this.platformId);
+    if(!isPlatformBrowser(this.platformId)) {
+      return;
+    }
     const localStorageMedia = this.windowService.getLocalStorage(MEDIA_VIEWER);
     if (localStorageMedia) {
       const media = JSON.parse(localStorageMedia);
