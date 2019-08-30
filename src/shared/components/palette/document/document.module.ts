d9bca0461be75d44e8d4fedb25504a81a66ebfe6
mutlu <mutlu.sancaktutar@hmcts.net>
2019-08-30 12:06:36 +0100

    move mv integration temporarily to case man web

--- src/shared/components/palette/document/document.module.ts
+++ src/shared/components/palette/document/document.module.ts
@@ -5,36 +5,23 @@ import { ReadDocumentFieldComponent } from './read-document-field.component';
 import { WriteDocumentFieldComponent } from './write-document-field.component';
 import { MarkdownModule } from '../../markdown/markdown.module';
 import { PaletteUtilsModule } from '../utils/utils.module';
-import { MediaViewerWrapperComponent } from './media-viewer-wrapper.component';
-import { MediaViewerModule } from '@hmcts/media-viewer';
-import { DocumentRoutingModule } from './document-routing.module';
-import { WindowService } from '../../../services/window';
-
 @NgModule({
   imports: [
     CommonModule,
     MarkdownModule,
     PaletteUtilsModule,
-    DocumentRoutingModule,
-    MediaViewerModule,
   ],
   declarations: [
     DocumentUrlPipe,
     ReadDocumentFieldComponent,
-    MediaViewerWrapperComponent,
     WriteDocumentFieldComponent
   ],
   entryComponents: [
     ReadDocumentFieldComponent,
     WriteDocumentFieldComponent,
-    MediaViewerWrapperComponent
   ],
   exports: [
     DocumentUrlPipe,
-    MediaViewerWrapperComponent
-  ],
-  providers: [
-    WindowService
   ]
 })
 export class DocumentModule { }
