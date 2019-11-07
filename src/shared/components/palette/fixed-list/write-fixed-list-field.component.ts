bc64ffb402bfb685fe9a77921c43e961741305f4
Tobias <it.consulting@tutanota.com>
2019-11-07 10:40:56 +0000

    RDM-5735: Fixed list display order should be ASC, not DESC
              [RDM-5735](https://tools.hmcts.net/jira/browse/RDM-5735).

--- src/shared/components/palette/fixed-list/write-fixed-list-field.component.ts
+++ src/shared/components/palette/fixed-list/write-fixed-list-field.component.ts
@@ -13,6 +13,8 @@ export class WriteFixedListFieldComponent extends AbstractFieldWriteComponent im
   ngOnInit() {
     let notEmpty = this.caseField.value !== null && this.caseField.value !== undefined;
     this.fixedListControl = this.registerControl(new FormControl(notEmpty ? this.caseField.value : null));
+    this.sortListItems();
+    console.log('hasADefinedDisplayOrder: ' + this.hasADefinedDisplayOrder());
     if (this.hasSanitisedDynamicListData()) {
       this.fixedListControl.setValue(this.caseField.value);
     }
@@ -20,5 +22,18 @@ export class WriteFixedListFieldComponent extends AbstractFieldWriteComponent im
 
   private hasSanitisedDynamicListData() {
     return this.fixedListControl.value && this.fixedListControl.value.value;
+  }
+
+  private sortListItems() {
+    if (this.hasADefinedDisplayOrder()) {
+      this.caseField.list_items.sort((param1, param2) => param1.order.localeCompare(param2.order));
+    }
+    this.caseField.list_items.reverse();
+  }
+
+  private hasADefinedDisplayOrder(): boolean {
+
+    const isDifferentToNull = function (element) { return (element.order !== null); }
+    return this.caseField.list_items.every(isDifferentToNull);
   }
 }
