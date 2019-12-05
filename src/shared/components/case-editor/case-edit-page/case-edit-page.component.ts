38cd8a1cc80b23e6442c28c23c438ac5095151a1
Tobias <it.consulting@tutanota.com>
2019-12-05 08:33:56 +0000

    RDM-5303: Document file name disappears when you come back to the page from a previous page.
           [RDM-5303](https://tools.hmcts.net/jira/browse/RDM-5303).

--- src/shared/components/case-editor/case-edit-page/case-edit-page.component.ts
+++ src/shared/components/case-editor/case-edit-page/case-edit-page.component.ts
@@ -118,18 +118,6 @@ export class CaseEditPageComponent implements OnInit, AfterViewChecked {
     return !this.pageValidationService.isPageValid(this.currentPage, this.editForm);
   }
 
-   buildCaseEventData(): CaseEventData {
-    let pageFormFields = this.formValueService.filterCurrentPageFields(this.currentPage.case_fields, this.editForm.value);
-    this.formValueService.sanitiseDynamicLists(this.currentPage.case_fields, pageFormFields);
-    let caseEventData: CaseEventData = this.formValueService.sanitise(pageFormFields) as CaseEventData;
-    caseEventData.event_token = this.eventTrigger.event_token;
-    caseEventData.ignore_warning = this.ignoreWarning;
-    caseEventData.event_data = this.editForm.value.data;
-    if (this.caseEdit.caseDetails) {
-      caseEventData.case_reference = this.caseEdit.caseDetails.case_id;
-    }
-    return caseEventData;
-  }
   toPreviousPage() {
     let caseEventData: CaseEventData = this.buildCaseEventData();
     this.updateFormData(caseEventData as CaseEventData);
@@ -304,5 +292,18 @@ export class CaseEditPageComponent implements OnInit, AfterViewChecked {
     }
 
     return this.eventTrigger.case_fields;
+  }
+
+  private buildCaseEventData(): CaseEventData {
+    let pageFormFields = this.formValueService.filterCurrentPageFields(this.currentPage.case_fields, this.editForm.value);
+    this.formValueService.sanitiseDynamicLists(this.currentPage.case_fields, pageFormFields);
+    let caseEventData: CaseEventData = this.formValueService.sanitise(pageFormFields) as CaseEventData;
+    caseEventData.event_token = this.eventTrigger.event_token;
+    caseEventData.ignore_warning = this.ignoreWarning;
+    caseEventData.event_data = this.editForm.value.data;
+    if (this.caseEdit.caseDetails) {
+      caseEventData.case_reference = this.caseEdit.caseDetails.case_id;
+    }
+    return caseEventData;
   }
 }
