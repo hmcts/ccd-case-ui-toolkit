62a45d6d923e7060f0232e909287b7494642f475
kiran.yenigala@hmcts.net <Yrlks1091327~>
2019-12-17 15:43:00 +0000

    RDM-6719: Add Banner Feature to CCD UI

--- src/shared/domain/definition/jurisdiction.model.ts
+++ src/shared/domain/definition/jurisdiction.model.ts
@@ -1,8 +1,10 @@
 import { CaseTypeLite } from './case-type-lite.model';
+import { Banner } from './banner.model';
 
 export class Jurisdiction {
   id: string;
   name: string;
   description: string;
   caseTypes: CaseTypeLite[];
+  banners: Banner[];
 }
