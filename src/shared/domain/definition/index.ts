493609877e46021eb3000f2a0e611e5298053641
kiran.yenigala@hmcts.net <Yrlks1091327~>
2019-12-23 13:47:42 +0000

    RDM-6719: Add Banner Feature to CCD UI

--- src/shared/domain/definition/index.ts
+++ src/shared/domain/definition/index.ts
@@ -8,4 +8,4 @@ export * from './field-type-enum.model';
 export * from './field-type.model';
 export * from './fixed-list-item.model';
 export * from './jurisdiction.model';
-export * from './banner.model';
+export * from "./banner.model";
