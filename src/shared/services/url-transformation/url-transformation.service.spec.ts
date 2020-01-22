4e37f769fc9c2c40a3ea7c6a36f2cdf6bd361093
Dan Lysiak <daniel.lysiak@cgi.com>
2020-01-22 15:45:06 +0000

    RDM-7293 - Update logic for new URLs

--- src/shared/services/url-transformation/url-transformation.service.spec.ts
+++ src/shared/services/url-transformation/url-transformation.service.spec.ts
@@ -6,32 +6,18 @@ function mockDocument(url: string) {
 
 describe('UrlTransformationService', () => {
   const CCD_INTERNAL_URL = 'https://ccd-case-management-web-demo.service.core-compute-demo.internal';
-  const CCD_EXTERNAL_URL = 'https://www-ccd.demo.platform.hmcts.net';
+  const CCD_EXTERNAL_URL = 'https://www.ccd.demo.platform.hmcts.net';
+  const CCD_INTERNAL_PROD_URL = 'https://ccd-case-management-web-prod.service.core-compute-prod.internal';
+  const CCD_EXTERNAL_PROD_URL = 'https://www.ccd.platform.hmcts.net';
 
-  const XUI_INTERNAL_URL = 'https://xui-mo-webapp-demo.service.core-compute-demo.internal';
-  const XUI_EXTERNAL_URL = 'https://www-xui-mo-webapp.demo.platform.hmcts.net';
+  const XUI_INTERNAL_URL = 'https://xui-webapp-ithc.service.core-compute-ithc.internal';
+  const XUI_EXTERNAL_URL = 'https://manage-case.ithc.platform.hmcts.net';
+  const XUI_INTERNAL_PROD_URL = 'https://xui-webapp-prod.service.core-compute-prod.internal';
+  const XUI_EXTERNAL_PROD_URL = 'https://manage-case.platform.hmcts.net';
 
   let urlTransformationService: UrlTransformationService;
 
   describe('getPreferredEquivalentOf()', () => {
-
-    it('should return non-mapped app\'s internal URL when on external URL', () => {
-      urlTransformationService = new UrlTransformationService(mockDocument(CCD_EXTERNAL_URL));
-      const testUrl = XUI_INTERNAL_URL;
-
-      const result = urlTransformationService.getPreferredEquivalentOf(testUrl);
-
-      expect(result).toEqual(XUI_EXTERNAL_URL);
-    });
-
-    it('should return non-mapped app\'s external URL when on internal URL', () => {
-      urlTransformationService = new UrlTransformationService(mockDocument(CCD_INTERNAL_URL));
-      const testUrl = XUI_EXTERNAL_URL;
-
-      const result = urlTransformationService.getPreferredEquivalentOf(testUrl);
-
-      expect(result).toEqual(XUI_INTERNAL_URL);
-    });
 
     it('should return mapped app\'s internal URL when on external URL', () => {
       urlTransformationService = new UrlTransformationService(mockDocument(CCD_EXTERNAL_URL));
@@ -49,6 +35,42 @@ describe('UrlTransformationService', () => {
       const result = urlTransformationService.getPreferredEquivalentOf(testUrl);
 
       expect(result).toEqual(CCD_INTERNAL_URL);
+    });
+
+    it('should return mapped app\'s internal prod URL when on external prod URL', () => {
+      urlTransformationService = new UrlTransformationService(mockDocument(CCD_EXTERNAL_PROD_URL));
+      const testUrl = XUI_INTERNAL_PROD_URL;
+
+      const result = urlTransformationService.getPreferredEquivalentOf(testUrl);
+
+      expect(result).toEqual(XUI_EXTERNAL_PROD_URL);
+    });
+
+    it('should return mapped app\'s external prod URL when on internal prod URL', () => {
+      urlTransformationService = new UrlTransformationService(mockDocument(CCD_INTERNAL_PROD_URL));
+      const testUrl = CCD_EXTERNAL_PROD_URL;
+
+      const result = urlTransformationService.getPreferredEquivalentOf(testUrl);
+
+      expect(result).toEqual(CCD_INTERNAL_PROD_URL);
+    });
+
+    it('should return non-mapped app\'s internal URL when on external URL', () => {
+      urlTransformationService = new UrlTransformationService(mockDocument(CCD_EXTERNAL_URL));
+      const testUrl = 'https://test-app-aat.service.core-compute-aat.internal';
+
+      const result = urlTransformationService.getPreferredEquivalentOf(testUrl);
+
+      expect(result).toEqual('https://test-app.aat.platform.hmcts.net');
+    });
+
+    it('should return non-mapped app\'s external URL when on internal URL', () => {
+      urlTransformationService = new UrlTransformationService(mockDocument(CCD_INTERNAL_URL));
+      const testUrl = 'https://test-app.aat.platform.hmcts.net';
+
+      const result = urlTransformationService.getPreferredEquivalentOf(testUrl);
+
+      expect(result).toEqual('https://test-app-aat.service.core-compute-aat.internal');
     });
 
     it('should return original URL when already on correct type', () => {
--- src/shared/services/url-transformation/url-transformation.service.ts
+++ src/shared/services/url-transformation/url-transformation.service.ts
@@ -5,20 +5,23 @@ import { DOCUMENT } from '@angular/common';
 export class UrlTransformationService {
   // Key/value pair of internal/external application names
   public static readonly APPLICATION_NAME_MAPPINGS = {
-    'ccd-case-management-web': 'ccd'
+    'ccd-case-management-web': 'www.ccd',
+    'xui-webapp': 'manage-case'
   };
 
   public static readonly APPLICATION_NAME_PLACEHOLDER = '{{APPLICATION_NAME}}';
   public static readonly ENV_PLACEHOLDER = '{{ENV}}';
 
-  public static readonly INTERNAL_PATTERN = '(?:https:\/\/)(.*)-(.*)\\.service\\.core-compute-(.*)\\.internal(.*)';
-  public static readonly EXTERNAL_PATTERN = '(?:https:\/\/)?www-(.*)\\.(.*)\\.platform\\.hmcts\\.net(.*)';
+  public static readonly INTERNAL_PATTERN = '(?:https:\/\/)?(.*)-(.*)\\.service\\.core-compute-(.*)\\.internal(.*)';
+  public static readonly EXTERNAL_PATTERN = '(?:https:\/\/)?(.*?)(?:\\.(ithc|demo|aat)\\.|(?:\\.))platform\\.hmcts\\.net(.*)';
 
   public static readonly EXTERNAL_TEMPLATE =
-    `https://www-${UrlTransformationService.APPLICATION_NAME_PLACEHOLDER}.${UrlTransformationService.ENV_PLACEHOLDER}.platform.hmcts.net`;
+    `https://${UrlTransformationService.APPLICATION_NAME_PLACEHOLDER}${UrlTransformationService.ENV_PLACEHOLDER}.platform.hmcts.net`;
   public static readonly INTERNAL_TEMPLATE =
     `https://${UrlTransformationService.APPLICATION_NAME_PLACEHOLDER}-${UrlTransformationService.ENV_PLACEHOLDER}` +
     `.service.core-compute-${UrlTransformationService.ENV_PLACEHOLDER}.internal`;
+
+  private static readonly PROD_NAME = 'prod';
 
   private document: Document;
 
@@ -43,11 +46,11 @@ export class UrlTransformationService {
     }
   }
 
-  isInternalUrl(url: string) {
+  isInternalUrl(url: string): boolean {
     return this.isMatchFound(url, new RegExp(UrlTransformationService.INTERNAL_PATTERN, 'i'));
   }
 
-  isExternalUrl(url: string) {
+  isExternalUrl(url: string): boolean {
     return this.isMatchFound(url, new RegExp(UrlTransformationService.EXTERNAL_PATTERN, 'i'));
   }
 
@@ -58,14 +61,16 @@ export class UrlTransformationService {
 
   private getInternalUrlOf(externalUrl: string): string {
     const matches = externalUrl.match(new RegExp(UrlTransformationService.EXTERNAL_PATTERN, 'i'));
-    const key = this.getKeyByValue(UrlTransformationService.APPLICATION_NAME_MAPPINGS, matches[1]);
-    return this.formUrl(UrlTransformationService.INTERNAL_TEMPLATE, key ? key : matches[1], matches[2], matches[3]);
+    const appName = this.getKeyByValue(UrlTransformationService.APPLICATION_NAME_MAPPINGS, matches[1]);
+    const envValue = matches[2] || UrlTransformationService.PROD_NAME;
+    return this.formUrl(UrlTransformationService.INTERNAL_TEMPLATE, appName || matches[1], envValue, matches[3]);
   }
 
   private getExternalUrlOf(internalUrl: string): string {
     const matches = internalUrl.match(new RegExp(UrlTransformationService.INTERNAL_PATTERN, 'i'));
-    const value = UrlTransformationService.APPLICATION_NAME_MAPPINGS[matches[1]];
-    return this.formUrl(UrlTransformationService.EXTERNAL_TEMPLATE, value ? value : matches[1], matches[2], matches[4]);
+    const appName = UrlTransformationService.APPLICATION_NAME_MAPPINGS[matches[1]];
+    const envValue = matches[2] === UrlTransformationService.PROD_NAME ? '' : `.${matches[2]}`;
+    return this.formUrl(UrlTransformationService.EXTERNAL_TEMPLATE, appName || matches[1], envValue, matches[4]);
   }
 
   private formUrl(template: string, appName: string, env: string, segments: string): string {
