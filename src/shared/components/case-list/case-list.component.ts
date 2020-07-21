916e48273f5c0f5aa82490eaa1f59377884f1296
Lam <daniel.2.lam@atos.net>
2020-07-21 17:10:02 +0100

    Simplify Case List component workings
    Refactor to use simpler domain model class and remove unnecessary display logic.

--- src/shared/components/case-list/case-list.component.ts
+++ src/shared/components/case-list/case-list.component.ts
@@ -1,5 +1,15 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { CaseField, SearchResultView, SearchResultViewColumn, SearchResultViewItem } from '../../domain';
+
+export interface CaseListItem {
+  caseCreatedDate: Date;
+  caseDueDate: Date;
+  caseRef: string;
+  petFirstName: string;
+  petLastName: string;
+  respFirstName: string;
+  respLastName: string;
+  sRef: string;
+}
 
 @Component({
   selector: 'ccd-case-list',
@@ -8,8 +18,11 @@ import { CaseField, SearchResultView, SearchResultViewColumn, SearchResultViewIt
 })
 export class CaseListComponent implements OnInit, OnChanges {
 
-  @Input()
-  resultView: SearchResultView
+  @Input() caseListItems: CaseListItem[];
+
+  @Input() columnNames: string[]; // Need some validation to check number of columns equals number of item properties
+
+  private valueItems: Object[];
 
   constructor() { }
 
@@ -17,56 +30,20 @@ export class CaseListComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['resultView']) {
-      this.hydrateResultView();
-    }
-  }
+    /* if (changes['caseListData']) {
+      this.valueItems = [];
 
-  /**
-   * Hydrates result view with case field definitions.
-   */
-  // A longer term resolution is to move this piece of logic to the backend
-  hydrateResultView(): void {
-    this.resultView.results.forEach(result => {
-      const caseFields = [];
+      this.caseListItems.forEach(item => {
+        Object.keys(item).forEach(key => {
 
-      Object.keys(result.case_fields).forEach(fieldId => {
-
-        const field = result.case_fields[fieldId];
-
-        caseFields.push(Object.assign(new CaseField(), {
-          id: fieldId,
-          label: null,
-          field_type: {},
-          value: field,
-          display_context: null,
-        }));
+        })
+        const valueItem = Object.assign({}, Object.values(item));
+        this.valueItems.push(valueItem);
       });
-
-      result.hydrated_case_fields = caseFields;
-      result.columns = {};
-
-      this.resultView.columns.forEach(col => {
-        result.columns[col.case_field_id] = this.buildCaseField(col, result);
-      });
-    });
-  }
-
-  buildCaseField(col: SearchResultViewColumn, result: SearchResultViewItem): CaseField {
-    return Object.assign(new CaseField(), {
-      id: col.case_field_id,
-      label: col.label,
-      field_type: col.case_field_type,
-      value: result.case_fields[col.case_field_id],
-      display_context: null,
-    });
+    } */
   }
 
   hasResults(): any {
-    return this.resultView.results.length;
-  }
-
-  hasDrafts(): boolean {
-    return this.resultView.hasDrafts();
+    return this.caseListItems.length;
   }
 }
