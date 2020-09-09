ce6125b26868225c9eb28d6d26ef389a4573cc5e
Uday Denduluri <uday.denduluri@gmail.com>
2020-09-09 11:42:46 +0100

    make getActiveOrganisations to get mapped Organisation

--- src/shared/services/organisation/organisation.service.ts
+++ src/shared/services/organisation/organisation.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, timer } from 'rxjs';
-import { publishReplay, refCount, take } from 'rxjs/operators';
+import { publishReplay, refCount, take, map } from 'rxjs/operators';
 import { AbstractAppConfig } from '../../../app.config';
 
 export interface OrganisationSuperUser {
@@ -34,17 +34,53 @@ export interface Organisation {
     contactInformation: OrganisationAddress [];
 }
 
+export interface OrganisationVm {
+    organisationIdentifier: string;
+    name: string;
+    addressLine1: string;
+    addressLine2: string;
+    addressLine3: string;
+    townCity: string;
+    county: string;
+    country: string;
+    postCode: string;
+}
+
 @Injectable()
 export class OrganisationService {
-    private organisations$: Observable<Organisation[]>;
+    private organisations$: Observable<OrganisationVm[]>;
+
+    public static mapOrganisation(organisations: Organisation[]): OrganisationVm [] {
+        const organisationsVm = new Array<OrganisationVm>();
+        organisations.forEach(org => {
+            let contactInformation = null;
+            if (org.contactInformation &&  org.contactInformation[0]) {
+                contactInformation = org.contactInformation[0];
+            }
+            organisationsVm.push({
+                organisationIdentifier: org.organisationIdentifier,
+                name: org.name,
+                addressLine1: contactInformation !== null ? contactInformation.addressLine1 : null,
+                addressLine2: contactInformation !== null ? contactInformation.addressLine2 : null,
+                addressLine3: contactInformation !== null ? contactInformation.addressLine3 : null,
+                townCity: contactInformation !== null ? contactInformation.townCity : null,
+                county: contactInformation !== null ? contactInformation.county : null,
+                country: contactInformation !== null ? contactInformation.country : null,
+                postCode: contactInformation !== null ? contactInformation.postCode : null,
+            });
+        });
+        return organisationsVm;
+      }
 
     constructor(private readonly http: HttpClient,
                 private readonly appconfig: AbstractAppConfig) {}
-    public getActiveOrganisations(): Observable<Organisation[]> {
+    public getActiveOrganisations(): Observable<OrganisationVm[]> {
         if (!this.organisations$) {
             const url = this.appconfig.getPrdUrl();
             const cacheTimeOut = this.appconfig.getCacheTimeOut();
-            this.organisations$ = this.http.get<Organisation[]>(url).pipe(publishReplay(1), refCount(), take(1));
+            this.organisations$ = this.http.get<Organisation[]>(url)
+            .pipe(map((orgs) => OrganisationService.mapOrganisation(orgs)),
+            publishReplay(1), refCount(), take(1));
             timer(cacheTimeOut).subscribe(() => {
                 console.log('subscribe');
                 this.organisations$ = null;
