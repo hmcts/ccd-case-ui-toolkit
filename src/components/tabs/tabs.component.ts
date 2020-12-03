924f5d14c98ad6a4ad2de162116415b1245a905f
Paul Howes <paul.howes@HMCTS.NET>
2020-12-03 14:42:20 +0000

    Focus order for tabs, case create, list and search

--- src/components/tabs/tabs.component.ts
+++ src/components/tabs/tabs.component.ts
@@ -23,19 +23,44 @@ export class TabsComponent implements AfterContentInit {
 
   public ngAfterContentInit(): void {
     this.panels.forEach((panel) => this.panelIds.push(panel.id));
-
-    this.show(this.route.snapshot.fragment);
   }
 
-  public show(id: string) {
+  public show(id: string, showPanel = true) {
     const panels: TabComponent[] = this.panels.toArray();
-
-    id = id || panels[0].id;
-
     if (0 > this.panelIds.indexOf(id)) {
       id = panels[0].id;
     }
 
-    panels.forEach((panel) => panel.selected = id === panel.id);
+    if (showPanel) {
+      panels.forEach(panel => panel.selected = id === panel.id);
+    }
+  }
+
+  setTabFocus(id: string) {
+      const selectedTab = this.tabs.find((element, index) => index === this.panelIds.indexOf(id));
+      selectedTab.nativeElement.focus();
+  }
+
+  handleKeyUp(event: KeyboardEvent, id: string) {
+    const panels: TabComponent[] = this.panels.toArray();
+    switch (event.key) {
+      case 'ArrowLeft':
+        if (this.panelIds.indexOf(id) > 0) {
+          id = panels[this.panelIds.indexOf(id) - 1].id;
+        }
+        this.setTabFocus(id);
+        break;
+
+      case 'ArrowRight':
+        if (this.panelIds.indexOf(id) < this.panelIds.length - 1) {
+          id = panels[this.panelIds.indexOf(id) + 1].id;
+        }
+        this.setTabFocus(id);
+        break;
+
+      case 'Enter': case ' ': // 'Space' is not recognised - we have to check the space character
+        this.show(id);
+        break;
+    }
   }
 }
