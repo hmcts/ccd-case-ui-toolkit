b59ff048c3254f1bd6db09529bbba567a8ed8778
leepavlouhmcts <lefkos.hadjiPavlou@HMCTS.NET>
2020-12-18 09:16:32 +0000

    added keyboard event and browser check service to make sure it calls onChange on firefox and safari and ie11

--- src/shared/components/search-result/search-result.component.ts
+++ src/shared/components/search-result/search-result.component.ts
@@ -6,7 +6,7 @@ import { CaseField, CaseState, CaseType, CaseView, DisplayMode,
   DRAFT_PREFIX, Jurisdiction, PaginationMetadata, SearchResultView, SearchResultViewColumn,
   SearchResultViewItem, SearchResultViewItemComparator, SortOrder, SortParameters } from '../../domain';
 import { CaseReferencePipe } from '../../pipes';
-import { ActivityService, SearchResultViewItemComparatorFactory } from '../../services';
+import { ActivityService, SearchResultViewItemComparatorFactory, BrowserService } from '../../services';
 
 @Component({
   selector: 'ccd-search-result',
@@ -99,7 +99,8 @@ export class SearchResultComponent implements OnChanges, OnInit {
     appConfig: AbstractAppConfig,
     private activityService: ActivityService,
     private caseReferencePipe: CaseReferencePipe,
-    private placeholderService: PlaceholderService
+    private placeholderService: PlaceholderService,
+    private browserService: BrowserService
   ) {
     this.searchResultViewItemComparatorFactory = searchResultViewItemComparatorFactory;
     this.paginationPageSize = appConfig.getPaginationPageSize();
@@ -428,5 +429,11 @@ export class SearchResultComponent implements OnChanges, OnInit {
     this.clickCase.emit({
       caseId: caseId
     });
+  }
+
+  onKeydown(event: KeyboardEvent, c: SearchResultViewItem): void {
+    if (this.browserService.isFirefox || this.browserService.isSafari || this.browserService.isIEOrEdge) {
+      this.changeSelection(c);
+    }
   }
 }
---
+++ src/shared/services/browser/browser.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class BrowserService {
+  isFirefox(): boolean {
+    return window.navigator.userAgent.indexOf("Firefox") > -1;
+  }
+  isSafari(): boolean {
+    const isSafariAgent = window.navigator.userAgent.indexOf("Safari") > -1;
+    const isChromeAgent = window.navigator.userAgent.indexOf("Chrome") > -1;
+    if ((isChromeAgent) && (isSafariAgent)) {
+      return false;
+    }
+    return isSafariAgent;
+  }
+  isIEOrEdge(): boolean {
+    return /msie\s|trident\/|edge\//i.test(window.navigator.userAgent);
+  }
+}
