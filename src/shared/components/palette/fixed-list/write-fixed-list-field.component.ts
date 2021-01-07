6056d47595bd4d87a500cb7d476b259a9b5e38db
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2021-01-07 15:29:37 +0000

    Bug/eui 2350 dynamic lists in complex types 2 aa (#623)
    * EUI-2350 initial solution
    
    * EUI-2350 linting
    
    * EUI-2350 Tests and comments
    
    * EUI-2350 linting
    
    * EUI-2350 added constants
    
    * EUI-2350 A more sustainable solution
    
    * EUI-2350 initial solution
    EUI-2350 linting
    
    
    EUI-2350 Tests and comments
    
    
    EUI-2350 linting
    
    
    EUI-2350 added constants
    
    
    EUI-2350 A more sustainable solution
    
    * EUI-2350 initial solution
    
    * EUI-2350 linting
    
    * EUI-2350 Tests and comments
    
    * EUI-2350 linting
    
    * EUI-2350 added constants
    
    * EUI-2350 A more sustainable solution
    
    * Update case-field.model.ts
    
    * EUI-2350 assigned correct selected value
    
    * EUI-2350 fixed test
    
    * EUI-2350 reassigning list_items in read mode
    
    * EUI-2350 removed test

--- src/shared/components/palette/fixed-list/write-fixed-list-field.component.ts
+++ src/shared/components/palette/fixed-list/write-fixed-list-field.component.ts
@@ -11,6 +11,15 @@ export class WriteFixedListFieldComponent extends AbstractFieldWriteComponent im
   fixedListFormControl: FormControl;
 
   ngOnInit() {
+
+    /**
+     *
+     * Reassigning list_items from formatted_list when list_items is empty for DynamicList's
+     */
+    if (!this.caseField.list_items && this.caseField.formatted_value && this.caseField.formatted_value.list_items) {
+      this.caseField.list_items = this.caseField.formatted_value.list_items;
+    }
+
     let isNull = this.caseField.value === undefined || this.caseField.value === '';
     if (isNull) {
       this.caseField.value = null;
