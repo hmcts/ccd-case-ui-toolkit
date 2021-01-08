0dc4df01aa1eae6facdcacc9e05e8ec7bcebf22a
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2021-01-08 14:18:01 +0000

    EUI-2350 fixed test

--- src/shared/components/palette/complex/read-complex-field.component.ts
+++ src/shared/components/palette/complex/read-complex-field.component.ts
@@ -21,12 +21,13 @@ export class ReadComplexFieldComponent extends AbstractFieldReadComponent implem
     if (this.caseField.display_context_parameter) {
       this.context = PaletteContext.TABLE_VIEW;
     }
-
-    this.caseField.field_type.complex_fields.map(field => {
-      if (field.field_type.type === ReadComplexFieldComponent.FIELD_TYPE_DYNAMIC_LIST) {
-        field.value = this.caseField.value[field.id].value;
-      }
-    });
+    if (this.caseField.field_type) {
+      this.caseField.field_type.complex_fields.map(field => {
+        if (field.field_type.type === ReadComplexFieldComponent.FIELD_TYPE_DYNAMIC_LIST) {
+          field.value = this.caseField.value[field.id].value;
+        }
+      });
+    }
 
   }
 
