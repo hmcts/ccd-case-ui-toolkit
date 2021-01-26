d05e228b6a52cfccae42bcf9d1e762537d4fbaf8
Paul Graham <paul@smartcasual.io>
2021-01-26 21:05:08 +0000

    Dynamic list items
    For some reason, the attempt to set the list_items on dynamic lists is no longer working. Added a getter to essentially do the same thing.

--- src/shared/components/palette/fixed-list/write-fixed-list-field.component.ts
+++ src/shared/components/palette/fixed-list/write-fixed-list-field.component.ts
@@ -10,6 +10,18 @@ export class WriteFixedListFieldComponent extends AbstractFieldWriteComponent im
 
   fixedListFormControl: FormControl;
 
+  public get listItems(): any[] {
+    if (this.caseField) {
+      if (this.caseField.list_items) {
+        return this.caseField.list_items;
+      }
+      if (this.caseField.formatted_value && this.caseField.formatted_value.list_items) {
+        return this.caseField.formatted_value.list_items;
+      }
+    }
+    return [];
+  }
+
   ngOnInit() {
 
     /**
