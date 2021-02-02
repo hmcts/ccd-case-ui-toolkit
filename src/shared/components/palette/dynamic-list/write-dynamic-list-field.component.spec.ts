e60dcd2be4e412d2979b27d98207ec955fb0a917
Phillip Whitaker <=philyorkshire@philyorkshire.uk>
2021-02-02 08:14:52 +0000

    added a specific dynamic list component that sets the default value to null if a 'value' property has not already been extracted and set

---
+++ src/shared/components/palette/dynamic-list/write-dynamic-list-field.component.spec.ts
@@ -0,0 +1,116 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormGroup, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { DebugElement } from '@angular/core';
+import { FieldType } from '../../../domain/definition/field-type.model';
+import { CaseField } from '../../../domain/definition/case-field.model';
+import { PaletteUtilsModule } from '../utils/utils.module';
+import { By } from '@angular/platform-browser';
+import { WriteDynamicListFieldComponent } from './write-dynamic-list-field.component';
+
+const VALUE = 'F';
+const EXPECTED_LABEL = 'Female';
+const FIELD_ID = 'MarritalStatus';
+const FIELD_TYPE: FieldType = {
+  id: 'Gender',
+  type: 'FixedList',
+  fixed_list_items: [
+    {
+      code: 'M',
+      label: 'Male',
+      order: 1
+    },
+    {
+      code: VALUE,
+      label: EXPECTED_LABEL,
+      order: 2
+    },
+    {
+      code: 'O',
+      label: 'Other',
+      order: 3
+    }
+  ]
+};
+const CASE_FIELD: CaseField = Object.assign(new CaseField(), {
+  id: FIELD_ID,
+  label: 'X',
+  display_context: 'OPTIONAL',
+  field_type: FIELD_TYPE,
+  value: VALUE
+});
+
+describe('WriteDynamicListFieldComponent', () => {
+
+  const $SELECT = By.css('.form-group select');
+  const $OPTION = By.css('.form-group option');
+
+  let fixture: ComponentFixture<WriteDynamicListFieldComponent>;
+  let component: WriteDynamicListFieldComponent;
+  let de: DebugElement;
+
+  beforeEach(async(() => {
+    TestBed
+      .configureTestingModule({
+        imports: [
+          ReactiveFormsModule,
+          PaletteUtilsModule,
+          FormsModule
+        ],
+        declarations: [
+          WriteDynamicListFieldComponent,
+        ],
+        providers: []
+      })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(WriteDynamicListFieldComponent);
+    component = fixture.componentInstance;
+    component.caseField = CASE_FIELD;
+    const FORM_GROUP: FormGroup = new FormGroup({});
+    const REGISTER_CONTROL = (control) => {
+      FORM_GROUP.addControl(FIELD_ID, control);
+      return control;
+    };
+    component.registerControl = REGISTER_CONTROL;
+
+    de = fixture.debugElement;
+    fixture.detectChanges();
+  }));
+
+  it('should select default value', () => {
+    component.caseField.value = null;
+    fixture.detectChanges();
+    let options = de.queryAll($OPTION);
+
+    expect(options.length).toEqual(4);
+    expect(options[0].nativeElement.textContent).toEqual('--Select a value--');
+    expect(options[1].nativeElement.textContent).toEqual('Male');
+    expect(options[2].nativeElement.textContent).toEqual('Female');
+    expect(options[3].nativeElement.textContent).toEqual('Other');
+    fixture
+        .whenStable()
+        .then(() => {
+          let select = de.query($SELECT);
+          expect(select.nativeElement.selectedIndex).toEqual(0);
+        });
+  });
+
+  it('should render all options', () => {
+    let options = de.queryAll($OPTION);
+    expect(options[0].nativeElement.textContent).toEqual('--Select a value--');
+    expect(options[1].nativeElement.textContent).toEqual('Male');
+    expect(options[2].nativeElement.textContent).toEqual('Female');
+    expect(options[3].nativeElement.textContent).toEqual('Other');
+  });
+
+  it('should link select element to formControl', () => {
+    component.caseField.value = 'M';
+    fixture.detectChanges();
+    fixture
+        .whenStable()
+        .then(() => {
+          let select = de.query($SELECT);
+          expect(select.nativeElement.selectedIndex).toEqual(1);
+        });
+  });
+});
--- src/shared/components/palette/fixed-list/read-fixed-list-field.component.ts
+++ src/shared/components/palette/fixed-list/read-fixed-list-field.component.ts
@@ -1,20 +1,10 @@
 import { AbstractFieldReadComponent } from '../base-field/abstract-field-read.component';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ccd-read-fixed-list-field',
   template: '<span class="text-16">{{caseField.value | ccdFixedList:caseField.list_items}}</span>',
 })
-export class ReadFixedListFieldComponent extends AbstractFieldReadComponent implements OnInit {
+export class ReadFixedListFieldComponent extends AbstractFieldReadComponent {
 
-  ngOnInit() {
-    /**
-     *
-     * Reassigning list_items from formatted_list when list_items is empty for DynamicList's
-     */
-    if (!this.caseField.list_items && this.caseField.formatted_value && this.caseField.formatted_value.list_items) {
-      this.caseField.list_items = this.caseField.formatted_value.list_items;
-    }
-
-  }
 }
