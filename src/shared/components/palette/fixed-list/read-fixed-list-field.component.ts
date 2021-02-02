a3d2a025da9352442bb10a33f80a3e7cc36836eb
Phillip Whitaker <=philyorkshire@philyorkshire.uk>
2021-02-02 08:14:52 +0000

    added a specific dynamic list component that sets the default value to null if a 'value' property has not already been extracted and set

--- src/shared/components/palette/fixed-list/read-fixed-list-field.component.ts
+++ src/shared/components/palette/fixed-list/read-fixed-list-field.component.ts
@@ -1,11 +1,11 @@
 import { AbstractFieldReadComponent } from '../base-field/abstract-field-read.component';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ccd-read-fixed-list-field',
   template: '<span class="text-16">{{caseField.value | ccdFixedList:caseField.list_items}}</span>',
 })
-export class ReadFixedListFieldComponent extends AbstractFieldReadComponent implements OnInit {
+export class ReadFixedListFieldComponent extends AbstractFieldReadComponent {
 
   ngOnInit() {
     /**
