0df03a1b4cf468b447c4c085d0895f085f526a54
rayliangatos <57181888+rayliangatos@users.noreply.github.com>
2021-02-05 16:23:10 +0000

    EUI-2243 upload document issue (#671)
    * EUI-2243-upload-document-issue:fix replacement issue
    
    * EUI-2243-upload-document-issue:add more tests
    
    * EUI-2243-upload-document-issue:upload readme and bump up version
    
    * EUI-2243-upload-document-issue: get better condition judgement

--- src/shared/components/case-editor/services/cases.service.ts
+++ src/shared/components/case-editor/services/cases.service.ts
@@ -152,7 +152,6 @@ export class CasesService {
       .get(url, {headers})
       .pipe(
         map(response => {
-
           return this.handleNestedDynamicListsInComplexTypes(response.json());
         }),
         catchError(error => {
