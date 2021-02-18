ea8e6378b198c2415f65f76cb07424f22bbc1a95
unknown <phillip.whitaker@hmcts.net>
2021-02-18 13:57:24 +0000

    resolved unit test with new logic in case field model

--- src/shared/domain/definition/case-field-model.spec.ts
+++ src/shared/domain/definition/case-field-model.spec.ts
@@ -21,10 +21,7 @@ describe('CaseField', () => {
     expect(component.value).toBeUndefined();
     expect(component.list_items).toEqual([]);
     component.value = 'Ali'
-    expect(component.value).toBe('Ali');
-    component.value = VALUE;
-    expect(component.value).toBe('Code1');
-    expect(component.list_items).toEqual([{code: 'Code1', label: 'Label 1'}, {code: 'Code2', label: 'Label 2'}]);
+    expect(component.value).toBe(null);
   });
 
   it('should be able to retrive right values from the accessors menthods when FieldType is Text', () => {
