6e41a56528d87ecfe8f7193e004deefb1122667c
phillip-whitaker <75982584+phillip-whitaker@users.noreply.github.com>
2021-02-18 14:21:37 +0000

    performance enhancements and 3045 work (#697)
    * performance enhancements and 3045 work
    
    * applied PG review amends
    
    * applied PG review amends
    
    * applied PG review amends
    
    * resolved unit test with new logic in case field model

--- src/shared/components/palette/fixed-list/write-fixed-list-field.component.ts
+++ src/shared/components/palette/fixed-list/write-fixed-list-field.component.ts
@@ -23,19 +23,12 @@ export class WriteFixedListFieldComponent extends AbstractFieldWriteComponent im
   }
 
   ngOnInit() {
-
-    /**
-     *
-     * Reassigning list_items from formatted_list when list_items is empty for DynamicList's
-     */
-    if (!this.caseField.list_items && this.caseField.formatted_value && this.caseField.formatted_value.list_items) {
-      this.caseField.list_items = this.caseField.formatted_value.list_items;
-    }
-
     let isNull = this.caseField.value === undefined || this.caseField.value === '';
+
     if (isNull) {
       this.caseField.value = null;
     }
+
     this.fixedListFormControl = this.registerControl(new FormControl(this.caseField.value)) as FormControl;
   }
 }
