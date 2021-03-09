3e880adda365b1905e2a7764286812b99f7e1e22
unknown <phillip.whitaker@hmcts.net>
2021-03-09 16:57:04 +0000

    made changes as per PG comments

--- src/shared/components/palette/dynamic-radio-list/dynamic-radio-list.pipe.spec.ts
+++ src/shared/components/palette/dynamic-radio-list/dynamic-radio-list.pipe.spec.ts
@@ -30,30 +30,30 @@ describe('FixedRadioListPipe', () => {
 
   describe('given a value matching the code of a list item', () => {
     it('should return the associated label', () => {
-      let label = fixedRadioListPipe.transform('F', FIXED_LIST);
+      const label = fixedRadioListPipe.transform('F', FIXED_LIST);
       expect(label).toBe('Female');
 
-      let label2 = fixedRadioListPipe.transform('M', FIXED_LIST);
+      const label2 = fixedRadioListPipe.transform('M', FIXED_LIST);
       expect(label2).toBe('Male');
 
-      let label3 = fixedRadioListPipe.transform('O', FIXED_LIST);
+      const label3 = fixedRadioListPipe.transform('O', FIXED_LIST);
       expect(label3).toBe('Other');
     });
   });
 
   describe('given a value NOT matching the code of ANY list item', () => {
     it('should return an empty string for unknown value', () => {
-      let label = fixedRadioListPipe.transform('X', FIXED_LIST);
+      const label = fixedRadioListPipe.transform('X', FIXED_LIST);
       expect(label).toBe(EMPTY);
     });
 
     it('should return an empty string for undefined value', () => {
-      let label = fixedRadioListPipe.transform(undefined, FIXED_LIST);
+      const label = fixedRadioListPipe.transform(undefined, FIXED_LIST);
       expect(label).toBe(EMPTY);
     });
 
     it('should return an empty string for null value', () => {
-      let label = fixedRadioListPipe.transform(null, FIXED_LIST);
+      const label = fixedRadioListPipe.transform(null, FIXED_LIST);
       expect(label).toBe(EMPTY);
     });
   });
