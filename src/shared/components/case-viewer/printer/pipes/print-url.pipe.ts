0ff358a93ab844f4e366208b3e60aba3673b519a
Lam <daniel.2.lam@atos.net>
2021-03-29 06:00:01 +0100

    Refactor IE browser identifiers to static constants

--- src/shared/components/case-viewer/printer/pipes/print-url.pipe.ts
+++ src/shared/components/case-viewer/printer/pipes/print-url.pipe.ts
@@ -6,6 +6,9 @@ import { AbstractAppConfig } from '../../../../../app.config';
 })
 export class PrintUrlPipe implements PipeTransform {
 
+  private static readonly MSIE_BROWSER_NAME = 'MSIE';
+  private static readonly IE11_BROWSER_ENGINE = 'Trident';
+
   constructor(private appConfig: AbstractAppConfig) {}
 
   /**
@@ -16,8 +19,8 @@ export class PrintUrlPipe implements PipeTransform {
    * (e.g. `/jurisdictions/TEST/case-types/Test1/cases/1111222233334444`)
    *
    * @param remoteUrl The "remote" URL to rewrite
-   * @returns A rewritten URL as per the above description, or the original `remoteUrl` if it was `null`,
-   * `undefined`, or the empty string
+   * @returns A rewritten URL as per the above description, or the original `remoteUrl` if it is `null`, `undefined`,
+   * or the empty string
    */
   transform(remoteUrl: string): string {
     if (remoteUrl && remoteUrl.length > 0) {
@@ -28,13 +31,14 @@ export class PrintUrlPipe implements PipeTransform {
        * the rendering engine, "Trident" (used to identify IE 11 because it no longer reports as "MSIE").
        * Note: IE does not support the URL interface and requires a workaround to parse URLs.
        */
-      if (navigator.userAgent.indexOf("MSIE") != -1 || navigator.userAgent.indexOf("Trident") != -1) {
+      if (navigator.userAgent.indexOf(PrintUrlPipe.MSIE_BROWSER_NAME) !== -1 ||
+          navigator.userAgent.indexOf(PrintUrlPipe.IE11_BROWSER_ENGINE) !== -1) {
         // Workaround for not being able to use the URL interface
         const urlParser = document.createElement('a');
         urlParser.href = remoteUrl;
         // Get the pathname from the anchor element with the "remote" Print Service URL
         printServiceUrlPathname = urlParser.pathname;
-        if (printServiceUrlPathname[0] != '/') {
+        if (printServiceUrlPathname[0] !== '/') {
           // Fix for IE11; it returns the pathname without leading slash
           printServiceUrlPathname = '/' + printServiceUrlPathname;
         }
