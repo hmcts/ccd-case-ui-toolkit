8483be826aac8bea57bb1dc1b3e60aed6fce008f
connorpgpmcelroy <74015088+connorpgpmcelroy@users.noreply.github.com>
2021-04-09 17:24:39 +0100

    Adding display_context_parameter outside of the caseField

--- src/shared/domain/definition/case-field.model.ts
+++ src/shared/domain/definition/case-field.model.ts
@@ -74,10 +74,13 @@ export class CaseField implements Orderable {
 
   @Expose()
   get dateTimeEntryFormat(): string {
-    if (this.display_context && this.display_context.toUpperCase() !== "READONLY") {
-      if (this.display_context_parameter) {
-        return this.extractBracketValue(this.display_context_parameter, '#DATETIMEENTRY');
+    if (this.display_context) {
+      if (this.display_context.toUpperCase() === "READONLY") {
+        return null;
       }
+    }
+    if (this.display_context_parameter) {
+      return this.extractBracketValue(this.display_context_parameter, '#DATETIMEENTRY');
     }
     return null;
   }
