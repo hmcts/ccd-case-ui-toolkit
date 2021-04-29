933586ccceaa675b6364808bc23812ecf2d25a70
connorpgpmcelroy <74015088+connorpgpmcelroy@users.noreply.github.com>
2021-04-29 12:40:23 +0100

    Stop DATETIMEDISPLAY editing time zones

--- src/shared/components/palette/date/read-date-field.component.ts
+++ src/shared/components/palette/date/read-date-field.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AbstractFieldReadComponent } from '../base-field/abstract-field-read.component';
 
 @Component({
   selector: 'ccd-read-date-field',
-  template: `<span class="text-16">{{caseField.value | ccdDate:'utc':caseField.dateTimeDisplayFormat}}</span>`
+  template: `<span class="text-16">{{caseField.value | ccdDate:timeZone:caseField.dateTimeDisplayFormat}}</span>`
 })
-export class ReadDateFieldComponent extends AbstractFieldReadComponent {}
+export class ReadDateFieldComponent extends AbstractFieldReadComponent implements OnInit {
+  public timeZone = 'utc';
+  ngOnInit() {
+    if (this.caseField.value && this.caseField.value.includes('T')) {
+      // when the value includes a time separator, stop the value from editing time
+      this.timeZone = 'local';
+    } else {
+      this.timeZone = 'utc';
+    }
+  }
+}
