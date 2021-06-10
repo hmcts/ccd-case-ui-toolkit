ce912f5ebeac4c472c34caed9e51268358295c79
RiteshHMCTS <ritesh.dsouza@HMCTS.net>
2021-06-10 11:53:59 +0100

    Added some tests for first-error-pipe

--- src/shared/components/palette/utils/first-error.pipe.spec.ts
+++ src/shared/components/palette/utils/first-error.pipe.spec.ts
@@ -3,6 +3,7 @@ import { FirstErrorPipe } from './first-error.pipe';
 describe('FirstErrorPipe', () => {
 
   const ERROR_MESSAGE = 'This is wrong';
+  const Field_Label = 'Field1'
 
   let firstError: FirstErrorPipe;
 
@@ -37,5 +38,37 @@ describe('FirstErrorPipe', () => {
     });
 
     expect(message).toBe(ERROR_MESSAGE);
+  });
+
+  it('should return exact error along with label name when field value is MANDATORY', () => {
+    let message = firstError.transform({
+      'required': true
+    }, Field_Label);
+
+    expect(message).toBe(`${Field_Label} is required`);
+  });
+
+  it('should return exact error along with label name when pattern does not match', () => {
+    let message = firstError.transform({
+      'pattern': {'actualValue': 'test ', 'requiredPattern': '^[0-9 +().-]{9,}$'}
+    }, Field_Label);
+
+    expect(message).toBe(`The data entered is not valid for ${Field_Label}`);
+  });
+
+  it('should return exact error along with label name when field value is below minimum length', () => {
+    let message = firstError.transform({
+      'minlength': {'actualValue': 'test', 'requiredLength': 5}
+    }, Field_Label);
+
+    expect(message).toBe(`${Field_Label} is below the minimum length`);
+  });
+
+  it('should return exact error along with label name when field value exceeds maximum length', () => {
+    let message = firstError.transform({
+      'maxlength': {'actualValue': 'test is done', 'requiredLength': 10}
+    }, Field_Label);
+
+    expect(message).toBe(`${Field_Label} exceeds the maximum length`);
   });
 });
