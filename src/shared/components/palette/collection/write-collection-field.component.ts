13f73a45f28f6447ced33a319dd850a8fb832c86
Paul Howes <paul.howes@HMCTS.NET>
2021-06-23 12:02:35 +0100

    Update removeItem. Remove test (see ticket).

--- src/shared/components/palette/collection/write-collection-field.component.ts
+++ src/shared/components/palette/collection/write-collection-field.component.ts
@@ -238,6 +238,7 @@ export class WriteCollectionFieldComponent extends AbstractFieldWriteComponent i
 
     this.formArray.removeAt(this.formArray.length - 1);
     this.collItems.pop();
+    this.caseField.value.pop();
   }
 
   itemLabel(index: number) {
