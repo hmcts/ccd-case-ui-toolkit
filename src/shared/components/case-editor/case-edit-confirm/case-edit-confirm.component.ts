1150e407dd2bd8f0516f3192e52689d2cec34065
RiteshHMCTS <ritesh.dsouza@HMCTS.net>
2021-07-12 12:35:07 +0100

    removed OnInit from import as its not used

--- src/shared/components/case-editor/case-edit-confirm/case-edit-confirm.component.ts
+++ src/shared/components/case-editor/case-edit-confirm/case-edit-confirm.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { CaseEditComponent } from '../case-edit/case-edit.component';
 import { Router } from '@angular/router';
