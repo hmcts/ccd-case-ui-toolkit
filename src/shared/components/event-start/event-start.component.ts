c740eba511af6f8104b4f18711f0fc773a02e899
Uday Denduluri <uday.denduluri@gmail.com>
2021-09-17 14:55:57 +0100

    seting various states and moving them

--- src/shared/components/event-start/event-start.component.ts
+++ src/shared/components/event-start/event-start.component.ts
@@ -1,24 +1,73 @@
-import { AfterViewInit, Component, Input, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { ComponentPortal, Portal, TemplatePortal } from '@angular/cdk/portal';
+import { State, StateMachine } from '@edium/fsm';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ccd-event-start',
   templateUrl: './event-start.component.html',
   styleUrls: ['./event-start.component.scss']
 })
-export class EventStartComponent implements AfterViewInit {
+export class EventStartComponent implements OnInit, AfterViewInit {
   @Input() public event: string;
   @ViewChild('templatePortalContent') templatePortalContent: TemplateRef<any>;
   public selectedPortal: Portal<any>;
   public componentPortal1: ComponentPortal<ComponentPortalExample1Component>;
   public componentPortal2: ComponentPortal<ComponentPortalExample2Component>;
   public templatePortal: TemplatePortal<any>;
-  constructor(private _viewContainerRef: ViewContainerRef) {}
+  public stateMachine: StateMachine;
+  public s1: State;
+  public s2: State;
+  public s3: State;
+  public firstAction = (state: State, context ) => {
+    context.task$.subscribe(task => {
+      if(task && task.length > 0) {
+        this.componentPortal1 = new ComponentPortal(ComponentPortalExample1Component);
+        this.selectedPortal = this.componentPortal1;
+      }
+    })
+  };
+  public secondAction = (state: State, context ) => {
+    context.cases$.subscribe(cases => {
+      if(!cases || cases.length === 0) {
+        this.componentPortal2 = new ComponentPortal(ComponentPortalExample2Component);
+        this.selectedPortal = this.componentPortal2;
+      }
+    })
+  };
+  public lastAction = (state: State, context ) => {
+    this.router.navigate(['/cases/case-details/1546883526751282']);
+  }
+  public exitAction = ( state: State, context: any) => {
+    return true;
+  };
+  constructor(private _viewContainerRef: ViewContainerRef, private router: Router) {}
+
+  public ngOnInit() {
+    const context = {
+      task$: of([{}]),
+      cases$: of([]),
+    };
+    this.stateMachine = new StateMachine('My first state machine', context);
+    this.s1 = this.stateMachine.createState( "My first", false, this.firstAction);
+    this.s2 = this.stateMachine.createState( "My second", false, this.secondAction);
+    this.s3 = this.stateMachine.createState("Final State", true, this.lastAction);
+    this.s1.addTransition("next", this.s2);
+    this.s2.addTransition("next", this.s3);
+
+  }
 
   ngAfterViewInit() {
-    this.componentPortal1 = new ComponentPortal(ComponentPortalExample1Component);
-    this.componentPortal2 = new ComponentPortal(ComponentPortalExample2Component);
     this.templatePortal = new TemplatePortal(this.templatePortalContent, this._viewContainerRef);
+  }
+
+  public onNextClick() {
+    if(!this.stateMachine.started) {
+      this.stateMachine.start(this.s1);
+    } else {
+      this.stateMachine.currentState.trigger('next');
+    }
   }
 }
 
