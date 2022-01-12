316737a28d50d13a4592be2a1edc1fe5fc3426bf
John Benjamin <john.benjamin@hmcts.net>
2022-01-12 14:45:32 +0000

    EUI-4615 New Task Check At Event Completion

--- src/shared/components/case-editor/case-event-completion/case-event-completion.component.ts
+++ src/shared/components/case-editor/case-event-completion/case-event-completion.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { StateMachine } from '@edium/fsm';
 import { EventCompletionStateMachineService, WorkAllocationService } from '../services';
@@ -10,7 +10,7 @@ import { EventCompletionParams } from '../domain/event-completion-params.model';
   selector: 'ccd-case-event-completion',
   templateUrl: './case-event-completion.html'
 })
-export class CaseEventCompletionComponent implements OnInit, EventCompletionComponentEmitter {
+export class CaseEventCompletionComponent implements OnChanges, EventCompletionComponentEmitter {
 
   @Input()
   public eventCompletionParams: EventCompletionParams;
@@ -28,29 +28,31 @@ export class CaseEventCompletionComponent implements OnInit, EventCompletionComp
     private readonly workAllocationService: WorkAllocationService) {
   }
 
-  public ngOnInit(): void {
-
-    // Setup the context
-    this.context = {
-      task: this.eventCompletionParams.task,
-      caseId: this.eventCompletionParams.caseId,
-      eventId: this.eventCompletionParams.eventId,
-      router: this.router,
-      route: this.route,
-      sessionStorageService: this.sessionStorageService,
-      workAllocationService: this.workAllocationService,
-      canBeCompleted: false,
-      component: this
-    };
-
-    // Initialise state machine
-    this.service = new EventCompletionStateMachineService();
-    this.stateMachine = this.service.initialiseStateMachine(this.context);
-    // Create states
-    this.service.createStates(this.stateMachine);
-    // Add transitions for the states
-    this.service.addTransitions();
-    // Start state machine
-    this.service.startStateMachine(this.stateMachine);
+  public ngOnChanges(changes?: SimpleChanges): void {
+    if (changes.eventCompletionParams && changes.eventCompletionParams.currentValue) {
+      console.log('ONE');
+      // Setup the context
+      this.context = {
+        task: this.eventCompletionParams.task,
+        caseId: this.eventCompletionParams.caseId,
+        eventId: this.eventCompletionParams.eventId,
+        router: this.router,
+        route: this.route,
+        sessionStorageService: this.sessionStorageService,
+        workAllocationService: this.workAllocationService,
+        canBeCompleted: false,
+        component: this
+      };
+      console.log('TWO');
+      // Initialise state machine
+      this.service = new EventCompletionStateMachineService();
+      this.stateMachine = this.service.initialiseStateMachine(this.context);
+      // Create states
+      this.service.createStates(this.stateMachine);
+      // Add transitions for the states
+      this.service.addTransitions();
+      // Start state machine
+      this.service.startStateMachine(this.stateMachine);
+    }
   }
 }
