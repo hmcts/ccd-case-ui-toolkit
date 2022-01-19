c523d2cd7f50250ef2abe6ff09189f99ff86b169
John Benjamin <john.benjamin@hmcts.net>
2022-01-19 09:36:19 +0000

    Task event completion merge

--- src/shared/components/case-viewer/case-viewer.module.ts
+++ src/shared/components/case-viewer/case-viewer.module.ts
@@ -8,13 +8,13 @@ import { TabsModule } from '../../../components/tabs';
 import { ConditionalShowModule, LabelSubstitutorModule } from '../../directives';
 import { CaseReferencePipe, PipesModule } from '../../pipes';
 import {
-  ActivityPollingService,
-  ActivityService,
-  DraftService,
-  ErrorNotifierService,
-  HttpService,
-  NavigationNotifierService,
-  OrderService
+	ActivityPollingService,
+	ActivityService,
+	DraftService,
+	ErrorNotifierService,
+	HttpService,
+	NavigationNotifierService,
+	OrderService
 } from '../../services';
 import { ActivityModule } from '../activity';
 import { CaseEditorModule, CaseNotifier } from '../case-editor';
@@ -22,21 +22,21 @@ import { CaseHeaderModule } from '../case-header';
 import { CaseHistoryModule } from '../case-history';
 import { ErrorMessageComponent } from '../error-message/error-message.component';
 import { ErrorsModule } from '../error/errors.module';
+import { EventStartModule } from '../event-start/event-start.module';
 import { EventTriggerModule } from '../event-trigger';
 import { ComplexModule, EventLogModule, PaletteModule } from '../palette';
 import { CaseBasicAccessViewComponent } from './case-basic-access-view/case-basic-access-view.component';
 import { CaseChallengedAccessRequestComponent } from './case-challenged-access-request/case-challenged-access-request.component';
 import { CaseChallengedAccessSuccessComponent } from './case-challenged-access-success/case-challenged-access-success.component';
-import { CaseSpecificAccessRequestComponent } from './case-specific-access-request/case-specific-access-request.component';
-import { CaseReviewSpecificAccessRequestComponent } from './case-review-specific-access-request/case-review-specific-access-request.component'
 import { CaseEventTriggerComponent } from './case-event-trigger';
 import { CaseFullAccessViewComponent } from './case-full-access-view/case-full-access-view.component';
+import { CaseReviewSpecificAccessRejectComponent } from './case-review-specific-access-reject';
+import { CaseReviewSpecificAccessRequestComponent } from './case-review-specific-access-request/case-review-specific-access-request.component';
+import { CaseSpecificAccessRequestComponent } from './case-specific-access-request/case-specific-access-request.component';
 import { CaseViewComponent } from './case-view';
 import { CaseViewerComponent } from './case-viewer.component';
 import { CasePrinterComponent, PrintUrlPipe } from './printer';
 import { CaseResolver, EventTriggerResolver } from './services';
-import { CaseReviewSpecificAccessRejectComponent } from './case-review-specific-access-reject';
-import { EventStartModule } from '../event-start';
 
 @NgModule({
   imports: [
