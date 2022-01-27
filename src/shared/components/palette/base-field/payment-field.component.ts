0a15d8b9e60fb2996a9ce3f8f893f825f0dd12eb
Ronald Mansveld <ronald.mansveld@atos.net>
2022-01-27 14:59:18 +0000

    Refactor to make Sonos happy

---
+++ src/shared/components/palette/base-field/payment-field.component.ts
@@ -0,0 +1,40 @@
+import { AbstractAppConfig } from '../../../../app.config';
+import { SessionStorageService } from '../../../services/session/session-storage.service';
+import { AbstractFieldReadComponent } from './abstract-field-read.component';
+
+export abstract class PaymentField extends AbstractFieldReadComponent {
+    constructor(
+        private appConfig: AbstractAppConfig,
+        private readonly sessionStorage: SessionStorageService
+    ) {
+        super();
+    }
+
+    public getBaseURL() {
+        return this.appConfig.getPaymentsUrl();
+    }
+
+    public getPayBulkScanBaseURL() {
+        return this.appConfig.getPayBulkScanBaseUrl();
+    }
+
+    public getRefundsUrl() {
+        return this.appConfig.getRefundsUrl();
+    }
+
+    public getUserRoles() {
+        const userDetails = JSON.parse(this.sessionStorage.getItem('userDetails') || null);
+        if (!userDetails || !userDetails.hasOwnProperty('roles')) {
+            return [];
+        }
+        return userDetails.roles;
+    }
+
+    public getUserEmail() {
+        const userDetails = JSON.parse(this.sessionStorage.getItem('userDetails') || null);
+        if (!userDetails || !userDetails.hasOwnProperty('sub')) {
+            return '';
+        }
+        return userDetails.sub;
+    }
+}
--- src/shared/components/palette/payment/case-payment-history-viewer-field.component.ts
+++ src/shared/components/palette/payment/case-payment-history-viewer-field.component.ts
@@ -1,47 +1,17 @@
-import { AbstractFieldReadComponent } from '../base-field/abstract-field-read.component';
 import { Component } from '@angular/core';
 import { AbstractAppConfig } from '../../../../app.config';
 import { SessionStorageService } from '../../../services/session/session-storage.service';
+import { PaymentField } from '../base-field/payment-field.component';
 
 @Component({
   selector: 'ccd-case-payment-history-viewer-field',
   templateUrl: 'case-payment-history-viewer-field.html',
 })
-export class CasePaymentHistoryViewerFieldComponent extends AbstractFieldReadComponent {
-
+export class CasePaymentHistoryViewerFieldComponent extends PaymentField {
   constructor(
-    private appConfig: AbstractAppConfig,
-    private readonly sessionStorage: SessionStorageService
+    appConfig: AbstractAppConfig,
+    sessionStorage: SessionStorageService
   ) {
-    super();
+    super(appConfig, sessionStorage);
   }
-
-  public getBaseURL() {
-    return this.appConfig.getPaymentsUrl();
-  }
-
-  public getPayBulkScanBaseURL() {
-    return this.appConfig.getPayBulkScanBaseUrl();
-  }
-
-  public getRefundsUrl() {
-    return this.appConfig.getRefundsUrl();
-  }
-
-  public getUserRoles() {
-    const userDetails = JSON.parse(this.sessionStorage.getItem('userDetails') || null);
-    if (!userDetails || !userDetails.hasOwnProperty('roles')) {
-      return [];
-    }
-    return userDetails.roles;
-  }
-
-  public getUserEmail() {
-    const userDetails = JSON.parse(this.sessionStorage.getItem('userDetails') || null);
-    if (!userDetails || !userDetails.hasOwnProperty('sub')) {
-      return '';
-    }
-    return userDetails.sub;
-  }
-
 }
