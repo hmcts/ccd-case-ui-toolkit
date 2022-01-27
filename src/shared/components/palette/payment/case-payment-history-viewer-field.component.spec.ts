003d6d7fd23a60587607eada3b4708d1b7767356
Ronald Mansveld <ronald.mansveld@atos.net>
2022-01-27 11:04:11 +0000

    Fix tests

--- src/shared/components/palette/payment/case-payment-history-viewer-field.component.spec.ts
+++ src/shared/components/palette/payment/case-payment-history-viewer-field.component.spec.ts
@@ -5,6 +5,7 @@ import { MockComponent } from 'ng2-mock-component';
 import { AbstractAppConfig } from '../../../../app.config';
 import { CaseField } from '../../../domain/definition/case-field.model';
 import { FieldType } from '../../../domain/definition/field-type.model';
+import { SessionStorageService } from '../../../services/session/session-storage.service';
 import { CasePaymentHistoryViewerFieldComponent } from './case-payment-history-viewer-field.component';
 import createSpyObj = jasmine.createSpyObj;
 
@@ -23,6 +24,7 @@ describe('CasePaymentHistoryViewerFieldComponent', () => {
   const CASE_REFERENCE = '1234123412341234';
   const PAYMENTS_URL = 'http://payment-api:123';
   const BULKSCAN_API_URL = 'http://bulkscant-api:123';
+  const REFUNDS_URL = 'http://refunds-api:123';
 
   let appConfig;
   let PaymentWebComponent;
@@ -32,9 +34,10 @@ describe('CasePaymentHistoryViewerFieldComponent', () => {
   let de: DebugElement;
 
   beforeEach(async(() => {
-    appConfig = createSpyObj<AbstractAppConfig>('AppConfig', ['getPaymentsUrl', 'getPayBulkScanBaseUrl']);
+    appConfig = createSpyObj<AbstractAppConfig>('AppConfig', ['getPaymentsUrl', 'getPayBulkScanBaseUrl', 'getRefundsUrl']);
     appConfig.getPaymentsUrl.and.returnValue(PAYMENTS_URL);
     appConfig.getPayBulkScanBaseUrl.and.returnValue(BULKSCAN_API_URL);
+    appConfig.getRefundsUrl.and.returnValue(REFUNDS_URL);
 
     PaymentWebComponent = MockComponent({ selector: 'ccpay-payment-lib', inputs: [
         'API_ROOT',
@@ -42,7 +45,14 @@ describe('CasePaymentHistoryViewerFieldComponent', () => {
         'BULKSCAN_API_ROOT',
         'ISBSENABLE',
         'SELECTED_OPTION',
-        'VIEW'
+        'VIEW',
+        'REFUNDS_API_ROOT',
+        'TAKEPAYMENT',
+        'SERVICEREQUEST',
+        'PAYMENT_GROUP_REF',
+        'EXC_REFERENCE',
+        'DCN_NUMBER',
+        'LOGGEDINUSERROLES'
       ]});
 
     TestBed
@@ -55,7 +65,8 @@ describe('CasePaymentHistoryViewerFieldComponent', () => {
           PaymentWebComponent
         ],
         providers: [
-          { provide: AbstractAppConfig, useValue: appConfig }
+          { provide: AbstractAppConfig, useValue: appConfig },
+          SessionStorageService
         ]
       })
       .compileComponents();
