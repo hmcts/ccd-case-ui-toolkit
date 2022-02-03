3e22273c5ac1c8384e3c76d7cca0eb0f0aaae7a7
John Benjamin <90200268+johnbenjamin-hmcts@users.noreply.github.com>
2022-02-03 10:34:32 +0000

    WA Task Event Completion for Release (#1003)
    * Task event completion merge
    
    * Task event completion merge
    
    * Update work-allocation.service.ts
    
    * Task event completion merge
    
    * Update case-ui-toolkit.module.ts
    
    * Task event completion merge
    
    * Task event completion merge
    
    * Sonar cloud test coverage
    
    * Sonar cloud test coverage
    
    * Sonar cloud test coverage
    
    * Sonar cloud test coverage
    
    * Created tag
    
    * Fixes to cancel button redirect from task reassigned component
    
    * Update yarn.lock
    
    * EUI-4166 Rework in progress
    
    * Update version
    
    * Update package.json
    
    * Event completion API change
    
    * Additional fixes and updates
    
    * Additional fixes and updates
    
    * Additional fixes and updates

--- src/shared/components/case-viewer/case-viewer.module.ts
+++ src/shared/components/case-viewer/case-viewer.module.ts
@@ -22,20 +22,21 @@ import { CaseHeaderModule } from '../case-header';
 import { CaseHistoryModule } from '../case-history';
 import { ErrorMessageComponent } from '../error-message/error-message.component';
 import { ErrorsModule } from '../error/errors.module';
+import { EventStartModule } from '../event-start/event-start.module';
 import { EventTriggerModule } from '../event-trigger';
 import { ComplexModule, EventLogModule, PaletteModule } from '../palette';
 import { CaseBasicAccessViewComponent } from './case-basic-access-view/case-basic-access-view.component';
 import { CaseChallengedAccessRequestComponent } from './case-challenged-access-request/case-challenged-access-request.component';
 import { CaseChallengedAccessSuccessComponent } from './case-challenged-access-success/case-challenged-access-success.component';
-import { CaseSpecificAccessRequestComponent } from './case-specific-access-request/case-specific-access-request.component';
-import { CaseReviewSpecificAccessRequestComponent } from './case-review-specific-access-request/case-review-specific-access-request.component'
 import { CaseEventTriggerComponent } from './case-event-trigger';
 import { CaseFullAccessViewComponent } from './case-full-access-view/case-full-access-view.component';
+import { CaseReviewSpecificAccessRejectComponent } from './case-review-specific-access-reject';
+import { CaseReviewSpecificAccessRequestComponent } from './case-review-specific-access-request/case-review-specific-access-request.component';
+import { CaseSpecificAccessRequestComponent } from './case-specific-access-request/case-specific-access-request.component';
 import { CaseViewComponent } from './case-view';
 import { CaseViewerComponent } from './case-viewer.component';
 import { CasePrinterComponent, PrintUrlPipe } from './printer';
 import { CaseResolver, EventTriggerResolver } from './services';
-import { CaseReviewSpecificAccessRejectComponent } from './case-review-specific-access-reject';
 
 @NgModule({
   imports: [
@@ -44,6 +45,7 @@ import { CaseReviewSpecificAccessRejectComponent } from './case-review-specific-
     ErrorsModule,
     ActivityModule,
     CaseHeaderModule,
+    EventStartModule,
     EventTriggerModule,
     TabsModule,
     PaletteModule,
@@ -75,7 +77,7 @@ import { CaseReviewSpecificAccessRejectComponent } from './case-review-specific-
   ],
   exports: [
     CaseViewerComponent,
-    CaseViewComponent,
+    CaseViewComponent
   ],
   providers: [
     CaseNotifier,
