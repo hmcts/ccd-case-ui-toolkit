1e74447a5ab7158d0b2f5bc75b253c198b7c372e
John Benjamin <john.benjamin@hmcts.net>
2022-02-15 13:43:43 +0000

    Update select-flag-type.component.ts

--- src/shared/components/palette/case-flag/components/select-flag-type/select-flag-type.component.ts
+++ src/shared/components/palette/case-flag/components/select-flag-type/select-flag-type.component.ts
@@ -32,7 +32,7 @@ export class SelectFlagTypeComponent implements OnInit {
   public ngOnInit(): void {
     this.flagTypes = this.getFlagTypes();
     this.flagTypes.forEach(flagType => {
-      this.formGroup.addControl(flagType.id, new FormControl(''));
+      this.formGroup.addControl('flagType', new FormControl(''));
     });
     this.formGroup.addControl('other', new FormControl(''));
     this.formGroup.addControl('otherFlagTypeDescription', new FormControl(''));
