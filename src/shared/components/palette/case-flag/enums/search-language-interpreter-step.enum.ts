a3b953f19fb74bfd0ac69cff0c5849c596e1ede3
John Benjamin <john.benjamin@hmcts.net>
2022-02-15 18:56:36 +0000

    EUI-4846 Changes after code review

---
+++ src/shared/components/palette/case-flag/enums/search-language-interpreter-step.enum.ts
@@ -0,0 +1,5 @@
+export enum SearchLanguageInterpreterStep {
+  TITLE_DESCRIPTION = 'Enter the language that will need to be interpreted. If this language is not listed, you can enter it manually.',
+  CHECKBOX_LABEL = 'Enter the language manually',
+  INPUT_LABEL = 'Enter the language'
+}
