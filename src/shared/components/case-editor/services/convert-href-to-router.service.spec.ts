6a7c490e5cb58c05df3555c0c4d414fb7c618b66
Surekha-HMCTS <surekha.athikesavan@hmcts.net>
2022-05-02 11:26:17 +0100

    updated tests

---
+++ src/shared/components/case-editor/services/convert-href-to-router.service.spec.ts
@@ -0,0 +1,24 @@
+import { TestBed, async } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { ConvertHrefToRouterService } from './convert-href-to-router.service';
+describe('ConvertHrefToRouterService', () => {
+  let store: ConvertHrefToRouterService
+  let router = {
+          navigate: jasmine.createSpy('navigate'),
+        };
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ ConvertHrefToRouterService , { provide: Router, useValue: router },],
+    })
+    store = TestBed.get(ConvertHrefToRouterService);
+  });
+
+  it('get href markdown link', async(() => {
+    const mockData = 'abcd';
+
+    store.updateHrefLink(mockData);
+    store.getHrefMarkdownLinkContent().subscribe(res => {
+      expect(res).toBe(mockData);
+    });
+  }));
+});
