40835940b200d26024584996391afb5ddd369d6b
RiteshHMCTS <ritesh.dsouza@HMCTS.net>
2022-05-03 16:11:28 +0100

    removing default view

--- src/shared/components/palette/complex/read-complex-field.component.ts
+++ src/shared/components/palette/complex/read-complex-field.component.ts
@@ -18,11 +18,6 @@ export class ReadComplexFieldComponent extends AbstractFieldReadComponent implem
     super.ngOnInit();
     if (this.caseField.display_context_parameter) {
       this.context = PaletteContext.TABLE_VIEW;
-    } else {
-      // default to default views
-      if (!this.caseField.display_context_parameter && this.context) {
-        this.context = PaletteContext.DEFAULT;
-      }
     }
     if (this.caseField.field_type) {
       this.caseField.field_type.complex_fields.map(field => {
