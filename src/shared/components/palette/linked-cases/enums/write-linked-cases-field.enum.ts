0b0f13c30b4128e0ec97f5e10731c09228dfca61
John Benjamin <john.benjamin@hmcts.net>
2022-05-04 15:46:40 +0100

    Setup write linked cases component

---
+++ src/shared/components/palette/linked-cases/enums/write-linked-cases-field.enum.ts
@@ -0,0 +1,6 @@
+export enum LinkedCasesPages {
+  BEFORE_YOU_START,
+  LINK_CASE,
+  UNLINK_CASE,
+  CHECK_YOUR_ANSWERS
+}
--- src/shared/components/palette/linked-cases/write-linked-cases-field.component.ts
+++ src/shared/components/palette/linked-cases/write-linked-cases-field.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ErrorMessage } from '../../../domain';
 import { AbstractFieldWriteComponent } from '../base-field';
+import { LinkedCasesPages } from './enums';
 
 @Component({
   selector: 'ccd-write-linked-cases-field',
@@ -7,10 +9,26 @@ import { AbstractFieldWriteComponent } from '../base-field';
 })
 export class WriteLinkedCasesFieldComponent extends AbstractFieldWriteComponent implements OnInit {
 
+  public linkedCasesPage: number;
+  public linkedCasesPages = LinkedCasesPages;
+  public errorMessages: ErrorMessage[] = [];
+
   constructor() {
     super();
   }
 
   public ngOnInit(): void {
+    // Initialise the first page to display
+    this.linkedCasesPage = this.linkedCasesPages.BEFORE_YOU_START;
+  }
+
+  public navigateToErrorElement(elementId: string): void {
+    if (elementId) {
+      const htmlElement = document.getElementById(elementId);
+      if (htmlElement) {
+        htmlElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        htmlElement.focus();
+      }
+    }
   }
 }
