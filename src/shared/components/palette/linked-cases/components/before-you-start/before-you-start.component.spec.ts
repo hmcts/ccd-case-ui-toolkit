b1044af719e6ec8e257f573419bfbb5103ab4fd6
John Benjamin <john.benjamin@hmcts.net>
2022-05-06 14:23:37 +0100

    EUI-5723 Before you start error handling

--- src/shared/components/palette/linked-cases/components/before-you-start/before-you-start.component.spec.ts
+++ src/shared/components/palette/linked-cases/components/before-you-start/before-you-start.component.spec.ts
@@ -1,10 +1,12 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { LinkedCasesPages } from '../../enums';
 import { BeforeYouStartComponent } from './before-you-start.component';
 
 describe('BeforeYouStartComponent', () => {
   let component: BeforeYouStartComponent;
   let fixture: ComponentFixture<BeforeYouStartComponent>;
+  let nextButton: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,10 +21,22 @@ describe('BeforeYouStartComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BeforeYouStartComponent);
     component = fixture.componentInstance;
+    spyOn(component.linkedCasesStateEmitter, 'emit');
+    nextButton = fixture.debugElement.nativeElement.querySelector('button[type="button"]');
     fixture.detectChanges();
   });
 
   it('should create component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should next event emit linked cases state with no error', () => {
+    nextButton.click();
+    fixture.detectChanges();
+    expect(component.errorMessages).toBeUndefined();
+    const errorMessageElement = fixture.debugElement.nativeElement.querySelector('.govuk-error-message');
+    expect(errorMessageElement).toBeNull();
+    expect(component.linkedCasesStateEmitter.emit).toHaveBeenCalledWith(
+      { currentLinkedCasesPage: LinkedCasesPages.BEFORE_YOU_START, errorMessages: component.errorMessages });
   });
 });
