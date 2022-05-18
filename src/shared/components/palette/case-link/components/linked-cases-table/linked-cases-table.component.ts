991e336ec619a167b97dc50db57efae8fbdee366
Sivaprakasam Sivaprakasam <>
2022-05-18 21:51:34 +0100

    linked cases top/bottom table integration

--- src/shared/components/palette/case-link/components/linked-cases-table/linked-cases-table.component.ts
+++ src/shared/components/palette/case-link/components/linked-cases-table/linked-cases-table.component.ts
@@ -1,18 +1,14 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { AbstractFieldReadComponent } from '../../../base-field/abstract-field-read.component';
 import { CaseField } from '../../../../../domain/definition';
-import { switchMap } from 'rxjs/operators';
-import { OrganisationService, OrganisationVm } from '../../../../../services/organisation';
-import { forkJoin, Observable, of, Subscription } from 'rxjs';
-import { OrganisationConverter, SimpleOrganisationModel } from '../../../../../domain/organisation';
-import { CaseLink } from '../../domain/linked-cases.model';
+import { OrganisationVm } from '../../../../../services/organisation';
+import { Observable, Subscription } from 'rxjs';
+import { SimpleOrganisationModel } from '../../../../../domain/organisation';
 import { CaseView } from '../../../../../domain';
-import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { SearchService } from '../../../../../services/search/search.service';
 
 export enum PageType {
-  LINKEDCASESTABLBEVIEW = 'linkedCasesTableView',
+  LINKEDCASESTOTABLBEVIEW = 'linkedCasesToTableView',
+  LINKEDCASESFROMTABLBEVIEW = 'linkedCasesFromTableView',
   PROPOSEDCASELINK = 'proposedCaseLink',
 }
 
@@ -21,13 +17,15 @@ export enum PageType {
   templateUrl: './linked-cases-table.component.html'
 })
 
-export class LinkedCasesTableComponent extends AbstractFieldReadComponent implements OnInit, AfterViewInit {
+export class LinkedCasesTableComponent implements AfterViewInit {
+
   @Input()
   caseField: CaseField;
   @Input()
-  public type: PageType = PageType.LINKEDCASESTABLBEVIEW;
-  pageType = PageType;
-
+  linkedCasesResponse: [];
+  @Input() tableHeading: string = ""
+  @Input() tableSubHeading: string = "";
+  
   public sub: Subscription;
   caseDetails: CaseView;
   parentUrl: string;
@@ -37,64 +35,11 @@ export class LinkedCasesTableComponent extends AbstractFieldReadComponent implem
   public organisations$: Observable<OrganisationVm[]>;
   public selectedOrg$: Observable<SimpleOrganisationModel>;
 
-  constructor(
-    private route: ActivatedRoute,
-    private readonly searchService: SearchService    ) {
-      super();
-    }
   ngAfterViewInit(): void {
     const labelField = document.getElementsByClassName('case-viewer-label');
-    if(labelField) {
+    if(labelField && labelField.length) {
       labelField[0].replaceWith('')
     }
   }
-
-  ngOnInit(): void {
-    this.getAllCaseInformation();
-    this.route.parent.url.subscribe(path => {
-      this.parentUrl = `/${path.join('/')}`;
-    });
-  }
-
-  groupByCaseType = (arrObj, key) => {
-    return arrObj.reduce((rv, x) => {
-      (rv[x[key]] = rv[x[key]] || []).push(x['caseReference']);
-      return rv;
-    }, {});
-  };
-
-  public getAllCaseInformation() {
-    const hearingServices = [];
-    const caseTypeId = this.route.snapshot.data.case.case_type.id;
-    const linkedCaseIds = this.groupByCaseType(this.caseField.value, 'caseType');
-    Object.keys(linkedCaseIds).map(key => {
-      const esQuery = this.constructElasticSearchQuery(linkedCaseIds[key], 100)
-      const query = this.searchService.searchCases('Benefit_SCSS', {}, {reference: linkedCaseIds[key].join(',')}, SearchService.VIEW_WORKBASKET);
-      hearingServices.push(query);
-    })
-    this.sub = forkJoin(hearingServices).subscribe((hearingsList: any) => {
-      hearingsList.forEach(response => response.results.map((result: any) => this.linkedCasesFromResponse.push(result)))
-      this.isLoaded = true;
-    });
-  }
-
-  constructElasticSearchQuery(caseIds: any[], size: number) {
-    return {
-      native_es_query: {
-        query: {
-          terms: {
-            reference: caseIds,
-          },
-        },
-        sort: [
-          // does not seem to allow sorting by case name (attempted both pre and post v6.8 syntax)
-          // this is either because case name not present for all cases or because nested data cannot be sorted in this instance
-          // { "case_data.caseName": {mode: "max", order: "asc", nested_path: "case_data"}},
-          { id: {order: 'asc'} },
-        ],
-        size,
-      },
-      supplementary_data: ['*'],
-    };
-  }
 }
+
