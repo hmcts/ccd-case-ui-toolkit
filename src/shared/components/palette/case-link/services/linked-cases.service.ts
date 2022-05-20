2d23fabb031d1b24983988ee3e3f15086d805a02
John Benjamin <john.benjamin@hmcts.net>
2022-05-20 10:31:01 +0100

    EUI-5626 Unlink case check your answers page

--- src/shared/components/palette/case-link/services/linked-cases.service.ts
+++ src/shared/components/palette/case-link/services/linked-cases.service.ts
@@ -6,5 +6,55 @@ export class LinkedCasesService {
 
   public linkedCases: LinkedCase[] = [];
   public preLinkedCases: LinkedCase[] = [];
-  public casesToUnlink: LinkedCase[] = [];
+  
+  public casesToUnlink: LinkedCase[] = [
+    {
+      caseLink: {
+        caseReference: '5283-8196-7254-2864',
+        caseName: 'Jane Smith vs DWP',
+        caseService: 'withDwp',
+        caseState: '',
+        caseType: 'Benefit_SCSS',
+        createdDateTime: '10/01/2022',
+        linkReason: [
+          {
+            reason: 'Progressed as part of this lead case'
+          },
+          {
+            reason: 'Linked for a hearing'
+          }
+        ]
+      }
+    },
+    {
+      caseLink: {
+        caseReference: '8254-9025-7233-6147',
+        caseName: 'Johnny Smith',
+        caseService: 'withDwp',
+        caseState: '',
+        caseType: 'Benefit_SCSS',
+        createdDateTime: '11/05/2022',
+        linkReason: [
+          {
+            reason: 'Case consolidated Familial Guardian Linked for a hearing'
+          }
+        ]
+      }
+    },
+    {
+      caseLink: {
+        caseReference: '4652-7249-0269-6213',
+        caseName: 'Dwayne Bravo',
+        caseService: 'withDwp',
+        caseState: '',
+        caseType: 'Benefit_SCSS',
+        createdDateTime: '12/03/2022',
+        linkReason: [
+          {
+            reason: 'Familial'
+          }
+        ]
+      }
+    }
+  ];
 }
