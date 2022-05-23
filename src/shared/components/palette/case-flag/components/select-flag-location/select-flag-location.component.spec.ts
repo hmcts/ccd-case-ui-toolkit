376fa063a3d12107562e73f6d35c24d7a0702f41
Lam <daniel.2.lam@atos.net>
2022-05-23 11:00:05 +0100

    EUI-4838: Create Case Flag - "Select Location" step unit tests
    Add missing unit tests for selecting flag location (similar to Manage Case Flags "Select case flag" page).

--- src/shared/components/palette/case-flag/components/select-flag-location/select-flag-location.component.spec.ts
+++ src/shared/components/palette/case-flag/components/select-flag-location/select-flag-location.component.spec.ts
@@ -1,12 +1,52 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FlagDetail, Flags } from '../../domain';
 import { CaseFlagFieldState, SelectFlagLocationErrorMessage } from '../../enums';
 import { SelectFlagLocationComponent } from './select-flag-location.component';
 
 describe('SelectFlagLocationComponent', () => {
   let component: SelectFlagLocationComponent;
   let fixture: ComponentFixture<SelectFlagLocationComponent>;
+  const flagsData = [
+    {
+      partyName: 'Rose Bank',
+      details: [
+        {
+          name: 'Flag 1',
+          flagComment: 'First flag',
+          dateTimeCreated: new Date(),
+          path: ['Reasonable adjustment'],
+          hearingRelevant: false,
+          flagCode: 'FL1',
+          status: 'Active'
+        },
+        {
+          name: 'Flag 2',
+          flagComment: 'Rose\'s second flag',
+          dateTimeCreated: new Date(),
+          path: ['Reasonable adjustment'],
+          hearingRelevant: false,
+          flagCode: 'FL2',
+          status: 'Inactive'
+        }
+      ] as FlagDetail[]
+    },
+    {
+      partyName: 'Tom Atin',
+      details: [
+        {
+          name: 'Flag 3',
+          flagComment: 'First flag',
+          dateTimeCreated: new Date(),
+          path: ['Reasonable adjustment'],
+          hearingRelevant: false,
+          flagCode: 'FL1',
+          status: 'Active'
+        }
+      ] as FlagDetail[]
+    }
+  ] as Flags[];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,6 +61,7 @@ describe('SelectFlagLocationComponent', () => {
     fixture = TestBed.createComponent(SelectFlagLocationComponent);
     component = fixture.componentInstance;
     component.formGroup = new FormGroup({});
+    component.flagsData = flagsData;
     fixture.detectChanges();
   });
 
@@ -46,5 +87,59 @@ describe('SelectFlagLocationComponent', () => {
     const nextButtonElement = nativeElement.querySelector('.button');
     // The "Next" button should not be present if the error condition has been set
     expect(nextButtonElement).toBeNull();
+  });
+
+  it('should display a radio button for each party', () => {
+    component.ngOnInit();
+    expect(component.filteredFlagsData.length).toBe(2);
+    const nativeElement = fixture.debugElement.nativeElement;
+    const radioButtonElements = nativeElement.querySelectorAll('.govuk-radios__input');
+    expect(radioButtonElements.length).toBe(2);
+    // Cannot check a radio button input element's value directly (it is just "on"), so check it via the associated
+    // FormControl when the button is clicked
+    radioButtonElements[0].click();
+    expect(component.formGroup.get(component.selectedLocationControlName).value).toEqual(flagsData[0].partyName);
+    radioButtonElements[1].click();
+    expect(component.formGroup.get(component.selectedLocationControlName).value).toEqual(flagsData[1].partyName);
+    const radioButtonLabelElements = nativeElement.querySelectorAll('.govuk-radios__label');
+    expect(radioButtonLabelElements.length).toBe(2);
+    expect(radioButtonLabelElements[0].textContent).toEqual(flagsData[0].partyName);
+    expect(radioButtonLabelElements[1].textContent).toEqual(flagsData[1].partyName);
+  });
+
+  it('should emit to parent if the validation succeeds', () => {
+    spyOn(component, 'onNext').and.callThrough();
+    spyOn(component.caseFlagStateEmitter, 'emit');
+    const nativeElement = fixture.debugElement.nativeElement;
+    nativeElement.querySelector('#flag-location-0').click();
+    nativeElement.querySelector('.button').click();
+    expect(component.onNext).toHaveBeenCalled();
+    expect(component.caseFlagStateEmitter.emit).toHaveBeenCalledWith({
+      currentCaseFlagFieldState: CaseFlagFieldState.FLAG_LOCATION,
+      errorMessages: component.errorMessages
+    });
+    expect(component.errorMessages.length).toBe(0);
+  });
+
+  it('should fail validation and emit to parent if no flag location is selected', () => {
+    spyOn(component, 'onNext').and.callThrough();
+    spyOn(component.caseFlagStateEmitter, 'emit');
+    expect(component.filteredFlagsData.length).toBe(2);
+    expect(component.caseFlagsConfigError).toBe(false);
+    const nativeElement = fixture.debugElement.nativeElement;
+    nativeElement.querySelector('.button').click();
+    fixture.detectChanges();
+    expect(component.onNext).toHaveBeenCalled();
+    expect(component.caseFlagStateEmitter.emit).toHaveBeenCalledWith({
+      currentCaseFlagFieldState: CaseFlagFieldState.FLAG_LOCATION,
+      errorMessages: component.errorMessages
+    });
+    expect(component.errorMessages[0]).toEqual({
+      title: '',
+      description: SelectFlagLocationErrorMessage.FLAG_LOCATION_NOT_SELECTED,
+      fieldId: 'conditional-radios-list'
+    });
+    const flagNotSelectedErrorMessageElement = nativeElement.querySelector('#flag-location-not-selected-error-message');
+    expect(flagNotSelectedErrorMessageElement.textContent).toContain(SelectFlagLocationErrorMessage.FLAG_LOCATION_NOT_SELECTED);
   });
 });
