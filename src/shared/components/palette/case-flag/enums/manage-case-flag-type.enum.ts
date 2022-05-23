15e96bc98c2489dc397ac194e10e56fcb978d98c
Lam <daniel.2.lam@atos.net>
2022-05-23 11:00:01 +0100

    EUI-5190: Manage Case Flags - "Select case flag" page validation
    Add page validation to display an error if the user makes no selection or does not click "Next".

--- src/shared/components/palette/case-flag/enums/manage-case-flag-type.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum SelectManageCaseFlagErrorMessage {
-  MANAGE_CASE_FLAG_NOT_SELECTED = 'Please make a selection',
-  MANAGE_CASE_FLAG_NOT_CONFIGURED = 'Flags have not been configured for this case type',
-}
