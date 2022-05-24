1040f761c29889d84bceed1972d113fc157d684d
Sivaprakasam Sivaprakasam <>
2022-05-24 09:35:22 +0100

    linked cases tables integration

--- src/shared/components/palette/case-link/services/linked-cases.service.ts
+++ src/shared/components/palette/case-link/services/linked-cases.service.ts
@@ -9,30 +9,5 @@ export class LinkedCasesService {
   public linkedCases: LinkedCase[] = [];
   public preLinkedCases: LinkedCase[] = [];
 
-  constructor(
-    private http: HttpService,
-  ) {
-  }
-
-  getLinkedCases(caseId: string): Observable<GetLinkedCases> { 
-    return this.http
-    .get('assets/getLinkedCases.json')
-    .pipe(
-      catchError(error => {
-        return throwError(error);
-      })
-    );
-  }
-
-  getCaseLinkReasons(categoryKey: string): Observable<LinkCaseReason[]> { 
-    return this.http
-    .get('assets/getCaseLinkReasons.json')
-    .pipe(
-      catchError(error => {
-        return throwError(error);
-      })
-    );
-  }
-
 }
 
