18608133a209a3d70043dc6053d0bc283cee9073
Sivaprakasam Sivaprakasam <>
2022-05-26 21:49:56 +0100

    linked cases error handling/reload

--- src/shared/components/palette/case-link/components/read-linked-cases.component.ts
+++ src/shared/components/palette/case-link/components/read-linked-cases.component.ts
@@ -11,19 +11,22 @@ export class ReadLinkedCasesComponent {
   @Input()
   caseField: CaseField;
 
+  reload = false
   public serverError: { id: string, message: string } = null;
 
   constructor(private router: Router) {}
 
-  public reloadCurrentRoute() {
+  reloadCurrentRoute() {
+    this.reload = true;
     const currentUrl = this.router.url;
-    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
-    this.router.onSameUrlNavigation = 'reload';
-    this.router.navigate([currentUrl]);
+    // this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    // this.router.onSameUrlNavigation = 'reload';
+    // this.router.navigate([currentUrl]);
+    return currentUrl;
   }
 
-  public getFailureNotification(evt) {
-    const errorMessage = "There has been a system error and your request could not be processed.";
+  getFailureNotification(evt) {
+    const errorMessage = "There has been a system error and your request could not be processed."
     this.serverError = {
       id: 'backendError', message: errorMessage
     };
