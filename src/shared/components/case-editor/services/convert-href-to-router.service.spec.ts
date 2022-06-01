ec5367095b1d31d5730022ce375eff92d4f7311a
Surekha-HMCTS <surekha.athikesavan@hmcts.net>
2022-06-01 10:53:41 +0100

    tests

--- src/shared/components/case-editor/services/convert-href-to-router.service.spec.ts
+++ src/shared/components/case-editor/services/convert-href-to-router.service.spec.ts
@@ -17,24 +17,24 @@ describe('ConvertHrefToRouterService', () => {
   });
 
   it('get href markdown link', async(() => {
-    const mockData = '/case/IA/Asylum/1632395877596617/trigger/addCaseNote';
-    store.updateHrefLink(mockData);
+    const hrefMarkdownLinkContent = '/case/IA/Asylum/1632395877596617/trigger/addCaseNote';
+    store.updateHrefLink(hrefMarkdownLinkContent);
     store.getHrefMarkdownLinkContent().subscribe(res => {
-      expect(res).toBe(mockData);
+      expect(res).toBe(hrefMarkdownLinkContent);
     });
   }));
 
   it('should call callAngularRouter() without queryParams', async(() => {
-    const mockData = '/case/IA/Asylum/1632395877596617/trigger/addCaseNote';
-    store.callAngularRouter(mockData);
-    expect(router.navigate).toHaveBeenCalledWith([mockData], {
+    const hrefMarkdownLinkContent = '/case/IA/Asylum/1632395877596617/trigger/addCaseNote';
+    store.callAngularRouter(hrefMarkdownLinkContent);
+    expect(router.navigate).toHaveBeenCalledWith([hrefMarkdownLinkContent], {
       queryParams: ''
     });
   }));
 
   it('should call callAngularRouter() with multiple queryParams', async(() => {
-    const mockData = '/role-access/allocate-role/allocate?caseId=1652-7000-9981-7227&roleCategory=JUDICIAL&jurisdiction=IA&tid=d8f01ae1-d51b-11ec-bd5d-2aeb959399b9';
-    store.callAngularRouter(mockData);
+    const hrefMarkdownLinkContent = '/role-access/allocate-role/allocate?caseId=1652-7000-9981-7227&roleCategory=JUDICIAL&jurisdiction=IA&tid=d8f01ae1-d51b-11ec-bd5d-2aeb959399b9';
+    store.callAngularRouter(hrefMarkdownLinkContent);
     expect(router.navigate).toHaveBeenCalledWith(['/role-access/allocate-role/allocate'], {queryParams: {caseId: "1652-7000-9981-7227",
     roleCategory: "JUDICIAL",
     jurisdiction: "IA",
@@ -42,8 +42,8 @@ describe('ConvertHrefToRouterService', () => {
   }));
 
   it('should call callAngularRouter() with single queryParams', async(() => {
-    const mockData = '/role-access/allocate-role/allocate?caseId=1652-7000-9981-7227';
-    store.callAngularRouter(mockData);
+    const hrefMarkdownLinkContent = '/role-access/allocate-role/allocate?caseId=1652-7000-9981-7227';
+    store.callAngularRouter(hrefMarkdownLinkContent);
     expect(router.navigate).toHaveBeenCalledWith(['/role-access/allocate-role/allocate'], {queryParams: {caseId: "1652-7000-9981-7227"}});
   }));
 });
