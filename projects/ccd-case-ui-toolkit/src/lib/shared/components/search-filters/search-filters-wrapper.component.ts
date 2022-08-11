9d18ee8cac3427cf5c04eed247420d9fd74d7760
Phillip Whitaker <phillip.whitaker1@hmcts.net>
2022-08-11 10:36:02 +0100

    fixed some imports

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/search-filters/search-filters-wrapper.component.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/search-filters/search-filters-wrapper.component.ts
@@ -1,7 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Jurisdiction } from '../../domain';
 import { READ_ACCESS } from '../../domain/case-view/access-types.model';
-import { DefinitionsService } from '../../services';
+import { Jurisdiction } from '../../domain/definition/jurisdiction.model';
+import { DefinitionsService } from '../../services/definitions/definitions.service';
+
 
 @Component({
     selector: 'ccd-search-filters-wrapper',
