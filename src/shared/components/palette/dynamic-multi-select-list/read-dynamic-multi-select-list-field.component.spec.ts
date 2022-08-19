7168eb6651df6c57c10f8fc308e8835981169958
Phillip Whitaker <phillip.whitaker1@hmcts.net>
2022-08-19 11:38:56 +0100

    Added tests to read field

--- src/shared/components/palette/dynamic-multi-select-list/read-dynamic-multi-select-list-field.component.spec.ts
+++ src/shared/components/palette/dynamic-multi-select-list/read-dynamic-multi-select-list-field.component.spec.ts
@@ -6,10 +6,21 @@ import { By } from '@angular/platform-browser';
 import { MockComponent } from 'ng2-mock-component';
 import { ReadDynamicMultiSelectListFieldComponent } from './read-dynamic-multi-select-list-field.component';
 import { FixedListPipe } from '../fixed-list/fixed-list.pipe';
-import { FormGroup } from '@angular/forms';
 
 const $VALUES = By.css('table>tbody>tr>td');
 const FIELD_ID = 'ReadOnlyFieldId';
+const FORMATTED_LIST_ITEMS = [
+  {
+    label: 'Boris',
+    code: 'P',
+    order: 1
+  },
+  {
+    label: 'Rishi',
+    code: 'PA',
+    order: 2
+  }
+]
 const FIELD_LIST_ITEMS = [
   {
     label: 'Pierre',
@@ -52,23 +63,23 @@ let CASE_FIELD: CaseField;
 describe('ReadDynamicMultiSelectListFieldComponent', () => {
 
   describe('Non-persistable readonly multi-select-list field', () => {
-    CASE_FIELD = new CaseField();
-    CASE_FIELD.id = FIELD_ID;
-    CASE_FIELD.label = 'X';
-    CASE_FIELD.field_type = FIELD_TYPE;
-    CASE_FIELD.value = VALUES;
-    CASE_FIELD.list_items = FIELD_LIST_ITEMS;
-
-    const FieldReadComponent = MockComponent({
-      selector: 'ccd-field-read',
-      inputs: ['caseField']
-    });
-
     let fixture: ComponentFixture<ReadDynamicMultiSelectListFieldComponent>;
     let component: ReadDynamicMultiSelectListFieldComponent;
     let de: DebugElement;
 
     beforeEach(async(() => {
+      CASE_FIELD = new CaseField();
+      CASE_FIELD.id = FIELD_ID;
+      CASE_FIELD.label = 'X';
+      CASE_FIELD.field_type = FIELD_TYPE;
+      CASE_FIELD.value = VALUES;
+      CASE_FIELD.list_items = FIELD_LIST_ITEMS;
+
+      const FieldReadComponent = MockComponent({
+        selector: 'ccd-field-read',
+        inputs: ['caseField']
+      });
+
       TestBed
         .configureTestingModule({
           imports: [],
@@ -111,6 +122,60 @@ describe('ReadDynamicMultiSelectListFieldComponent', () => {
       fixture.detectChanges();
 
       expect(de.children.length).toBe(0);
+    });
+  });
+
+  describe('Non-persistable readonly multi-select-list field with just formatted_value', () => {
+    let fixture: ComponentFixture<ReadDynamicMultiSelectListFieldComponent>;
+    let component: ReadDynamicMultiSelectListFieldComponent;
+    let de: DebugElement;
+
+    beforeEach(async(() => {
+      CASE_FIELD = new CaseField();
+      CASE_FIELD.id = FIELD_ID;
+      CASE_FIELD.label = 'X';
+      CASE_FIELD.field_type = FIELD_TYPE;
+      CASE_FIELD.value = null;
+      CASE_FIELD.list_items = null;
+      CASE_FIELD.formatted_value = {
+        value: [FORMATTED_LIST_ITEMS[0]],
+        list_items: FORMATTED_LIST_ITEMS
+      }
+
+      const FieldReadComponent = MockComponent({
+        selector: 'ccd-field-read',
+        inputs: ['caseField']
+      });
+
+      TestBed
+        .configureTestingModule({
+          imports: [],
+          declarations: [
+            ReadDynamicMultiSelectListFieldComponent,
+            FixedListPipe,
+
+            // Mock
+            FieldReadComponent
+          ],
+          providers: []
+        })
+        .compileComponents();
+
+      fixture = TestBed.createComponent(ReadDynamicMultiSelectListFieldComponent);
+      component = fixture.componentInstance;
+
+      component.caseField = CASE_FIELD;
+
+      de = fixture.debugElement;
+      fixture.detectChanges();
+    }));
+
+    it('should render formatted list items when list_items is null', () => {
+      expect(de.children.length).toBe(1);
+    });
+
+    it('should set formatted list value when value is not set', () => {
+      expect(component.caseField.value).toEqual([FORMATTED_LIST_ITEMS[0]]);
     });
   });
 
