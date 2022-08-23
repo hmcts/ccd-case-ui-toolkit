bdb5376dcac06727442e8b7cd4f5a2bec39968a2
DavidJayakumar <David.jayakumar@hmctsc.net>
2022-08-23 09:09:09 +0100

    removing Mvp code

--- src/shared/components/event-start/event-guard/event-start.guard.spec.ts
+++ src/shared/components/event-start/event-guard/event-start.guard.spec.ts
@@ -9,7 +9,6 @@ import { UserInfo } from '../../../domain/user/user-info.model';
 
 describe('EventStartGuard', () => {
   const WORK_ALLOCATION_1_API_URL = 'workallocation';
-  const WORK_ALLOCATION_2_API_URL = 'workallocation2';
   const tasks: any[] = [
     {
       assignee: null,
@@ -35,34 +34,7 @@ describe('EventStartGuard', () => {
   const service = createSpyObj('service', ['getTasksByCaseIdAndEventId']);
   const appConfig = createSpyObj<AbstractAppConfig>('appConfig', ['getWorkAllocationApiUrl']);
   const sessionStorageService = createSpyObj('sessionStorageService', ['getItem', 'removeItem', 'setItem']);
-  sessionStorageService.getItem.and.returnValue(JSON.stringify({cid: '1620409659381330', caseType: 'caseType', jurisdiction: 'IA'}));
-  it('canActivate should return false', () => {
-    appConfig.getWorkAllocationApiUrl.and.returnValue(WORK_ALLOCATION_2_API_URL);
-    const guard = new EventStartGuard(service, router, appConfig, sessionStorageService);
-    const payload: TaskPayload = {
-      task_required_for_event: true,
-      tasks
-    }
-    service.getTasksByCaseIdAndEventId.and.returnValue(of(payload));
-    const canActivate$ = guard.canActivate(route);
-    canActivate$.subscribe(canActivate => {
-      expect(canActivate).toEqual(false);
-    });
-  });
-
-  it('canActivate should return true', () => {
-    appConfig.getWorkAllocationApiUrl.and.returnValue(WORK_ALLOCATION_2_API_URL);
-    const guard = new EventStartGuard(service, router, appConfig, sessionStorageService);
-    const payload: TaskPayload = {
-      task_required_for_event: false,
-      tasks: []
-    }
-    service.getTasksByCaseIdAndEventId.and.returnValue(of(payload));
-    const canActivate$ = guard.canActivate(route);
-    canActivate$.subscribe(canActivate => {
-      expect(canActivate).toEqual(true);
-    });
-  });
+  sessionStorageService.getItem.and.returnValue(JSON.stringify({cid: '1620409659381330', caseType: 'caseType', jurisdiction: 'IA'}));  
 
   it('canActivate should return true for work allocation 1', () => {
     appConfig.getWorkAllocationApiUrl.and.returnValue(WORK_ALLOCATION_1_API_URL);
