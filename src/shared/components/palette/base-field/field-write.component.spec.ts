6ebc138de410fbbbc4f1a1f0c6c424a4077ada5e
maliumer <Muhammad.Umer@hmcts.net>
2022-09-08 16:09:28 +0100

    fixing linting issue
    linting issue

--- src/shared/components/palette/base-field/field-write.component.spec.ts
+++ src/shared/components/palette/base-field/field-write.component.spec.ts
@@ -89,7 +89,7 @@ describe('FieldWriteComponent', () => {
   it('should get field write class from PaletteService to be call with case file and writeMode params', () => {
     component.caseField.display_context === 'OPTIONAL'
     expect(paletteService.getFieldComponentClass).toHaveBeenCalledWith(CASE_FIELD, true);
-  })
+  });
 
   it('should inject component instance as child', () => {
     let divWrapper = de.children[0];
