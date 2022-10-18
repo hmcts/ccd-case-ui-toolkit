3698481450dfd91078d6ad709a84650a866cba34
John Benjamin <john.benjamin@hmcts.net>
2022-10-18 13:38:37 +0100

    EUI-6822 Two sections for the case file view

---
+++ src/shared/components/palette/case-file-view/case-file-view-filed.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+import { AbstractFieldReadComponent } from "../base-field/abstract-field-read.component";
+
+@Component({
+  selector: 'ccd-case-file-view-field',
+  templateUrl: './case-file-view-field.component.html'
+})
+export class CaseFileViewFieldComponent extends AbstractFieldReadComponent {}
---
+++ src/shared/components/palette/case-file-view/index.ts
@@ -0,0 +1,1 @@
+export * from './case-file-view-filed.component';
