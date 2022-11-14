75180a958439feb9b72ae5caccec4d8189e86bbb
John Benjamin <john.benjamin@hmcts.net>
2022-11-14 16:17:52 +0000

    EUI-5377 Search documents V2

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/case-file-view/components/case-file-view-folder/case-file-view-folder.component.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/case-file-view/components/case-file-view-folder/case-file-view-folder.component.ts
@@ -10,7 +10,6 @@ import {
   DocumentTreeNode
 } from '../../../../../domain/case-file-view';
 import { categoriesAndDocuments } from '../../test-data/categories-and-documents-test-data';
-import { treeData } from '../../test-data/document-tree-node-test-data';
 
 @Component({
   selector: 'ccd-case-file-view-folder',
@@ -30,6 +29,7 @@ export class CaseFileViewFolderComponent implements OnInit, OnDestroy {
   public categoriesAndDocumentsSubscription: Subscription;
   public documentFilterFormGroup: FormGroup;
   public documentSearchFormControl: FormControl;
+  public documentTreeData: DocumentTreeNode[];
 
   private getChildren = (node: DocumentTreeNode) => of(node.children);
   public nestedChildren = (_: number, nodeData: DocumentTreeNode) => nodeData.children;
@@ -42,35 +42,34 @@ export class CaseFileViewFolderComponent implements OnInit, OnDestroy {
     this.documentFilterFormGroup = new FormGroup({});
     this.documentSearchFormControl = new FormControl('');
     this.documentFilterFormGroup.addControl('documentSearchFormControl', this.documentSearchFormControl);
+    
+    // Listen to search input and initiate filter documents if at least three characters entered
     this.documentSearchFormControl.valueChanges.pipe(
       tap(() => console.log(this.documentFilterFormGroup.value)),
       debounceTime(300),
       filter((searchTerm: string) => searchTerm && searchTerm.length > CaseFileViewFolderComponent.MINIMUM_SEARCH_CHARACTERS),
-      switchMap((searchTerm: string) => this.filter(searchTerm).pipe(
+      switchMap((searchTerm: string) => this.filter(searchTerm.toLowerCase(), this.documentTreeData).pipe(
         tap(() => console.log('SEARCH TERM', searchTerm))
       ))
-    ).subscribe(result => {
-      console.log('RESULT', result);
+    ).subscribe(documentTreeData => {
+      this.nestedDataSource = documentTreeData;
     });
+    
+    // Subscribe to the input categories and documents, and generate tree data and initialise cdk tree
     this.categoriesAndDocumentsSubscription = this.categoriesAndDocuments.subscribe(categoriesAndDocumentsResult => {
       // Using the mock data for now as we have to display the documents as well for demo purpose
       const categories = categoriesAndDocuments.categories; // categoriesAndDocuments.categories;
       // Generate document tree data from categories
-      const treeData = this.generateTreeData(categories);
+      this.documentTreeData = this.generateTreeData(categories);
       // Append uncategorised documents
       if (categoriesAndDocumentsResult.uncategorised_documents && categoriesAndDocumentsResult.uncategorised_documents.length > 0) {
         const uncategorisedDocuments = this.getUncategorisedDocuments(categoriesAndDocumentsResult.uncategorised_documents);
-        treeData.push(uncategorisedDocuments);
+        this.documentTreeData.push(uncategorisedDocuments);
       }
       // Initialise cdk tree with generated data
-      this.nestedDataSource = treeData;
+      this.nestedDataSource = this.documentTreeData;
     });
   }
-
-  public filter(searchTerm: string): Observable<any> {
-
-    return of(treeData);
-  };
 
   public generateTreeData(categories: CaseFileViewCategory[]): DocumentTreeNode[] {
     return categories.reduce((tree, node) => [
@@ -99,6 +98,28 @@ export class CaseFileViewFolderComponent implements OnInit, OnDestroy {
     });
     return { name: CaseFileViewFolderComponent.UNCATEGORISED_DOCUMENTS_TITLE, children: documents };
   }
+
+  public filter(searchTerm: string, documentTreeData: DocumentTreeNode[]): Observable<DocumentTreeNode[]> {
+    // Make a copy of the data so we do not mutate the original
+    function copy(node: DocumentTreeNode) {
+      return Object.assign({}, node);
+    }
+
+    let filteredData = documentTreeData;
+    if (searchTerm) {
+      filteredData = documentTreeData.map(copy).filter(function filterTreeData(node: DocumentTreeNode) {
+        if (node.name && node.name.toLowerCase().includes(searchTerm)) {
+          return true;
+        }
+        // Call recursively if node has children
+        if (node.children) {
+          return (node.children = node.children.map(copy).filter(filterTreeData)).length;
+        }
+      });
+    }
+
+    return of(filteredData);
+  };
 
   public ngOnDestroy(): void {
     if (this.categoriesAndDocumentsSubscription) {
