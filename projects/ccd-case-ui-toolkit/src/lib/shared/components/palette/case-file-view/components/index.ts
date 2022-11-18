4f3d3a1bc7ca95f4f0b8e09b7205b4448e1d36d2
Matei Baciu <matei.baciu@hmcts.net>
2022-11-18 10:19:04 +0000

    bump ccd toolkit version

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/case-file-view/components/index.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/case-file-view/components/index.ts
@@ -2,6 +2,3 @@ export * from './case-file-view-folder/case-file-view-folder.component';
 export * from './case-file-view-folder/case-file-view-folder-sort/case-file-view-folder-sort.component';
 
 export * from './shared';
-export * from './case-file-view-folder/case-file-view-folder-sort/case-file-view-folder-sort.component';
-
-export * from './shared';
