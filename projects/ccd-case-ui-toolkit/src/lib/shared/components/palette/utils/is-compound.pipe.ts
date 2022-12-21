9a0c0cbe3b68a9e391c61e32ffcef818b3c70374
John Benjamin <john.benjamin@hmcts.net>
2022-12-21 10:31:38 +0000

    EUI-7457 Linked cases not proceeding after the first page

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/utils/is-compound.pipe.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/utils/is-compound.pipe.ts
@@ -7,6 +7,8 @@ import { FieldTypeEnum } from '../../../domain/definition/field-type-enum.model'
 })
 export class IsCompoundPipe implements PipeTransform {
 
+  private static readonly CASE_LINK_FIELD_TYPE = 'CaseLink';
+
   private static readonly COMPOUND_TYPES: FieldTypeEnum[] = [
     'Complex',
     'Label',
@@ -30,6 +32,15 @@ export class IsCompoundPipe implements PipeTransform {
   public transform(field: CaseField): boolean {
     if (!field || !field.field_type || !field.field_type.type) {
       return false;
+    }
+
+    // Case link tab spacing alignment fix
+    // This Pipe needs to be re-visited
+    // to make it more generic to support and fix alignments issues for multiple tabs
+    if (field.field_type.collection_field_type &&
+        field.field_type.collection_field_type.id &&
+        field.field_type.collection_field_type.id === IsCompoundPipe.CASE_LINK_FIELD_TYPE) {
+      return true;
     }
 
     if (IsCompoundPipe.COMPOUND_TYPES.indexOf(field.field_type.type) !== -1) {
