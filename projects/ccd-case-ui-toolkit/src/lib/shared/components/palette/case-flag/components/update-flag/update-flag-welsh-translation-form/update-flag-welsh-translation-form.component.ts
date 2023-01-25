bb35cc21a0bd0e03ddde28c1ec6f5f24ca8f0b71
Matei Baciu <matei.baciu@hmcts.net>
2023-01-25 13:37:54 +0000

    EUI-7349

---
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/case-flag/components/update-flag/update-flag-welsh-translation-form/update-flag-welsh-translation-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'ccd-update-flag-welsh-translation-form',
+  templateUrl: './update-flag-welsh-translation-form.component.html',
+  styleUrls: ['./update-flag-welsh-translation-form.component.scss']
+})
+export class UpdateFlagWelshTranslationFormComponent implements OnInit {
+  @Input() public formGroup: FormGroup;
+  @Input() public readonly FLAG_COMMENTS_CONTROL_NAME = 'flagComment';
+  @Input() public readonly FLAG_STATUS_CHANGE_REASON_CONTROL_NAME = 'flagStatusReasonChange';
+  public readonly FLAG_COMMENTS_WELSH_CONTROL_NAME = 'flagCommentWelsh';
+  public readonly FLAG_STATUS_CHANGE_REASON_WELSH_CONTROL_NAME = 'flagStatusReasonChangeWelsh';
+
+  constructor() {
+  }
+
+  public ngOnInit(): void {
+    if (this.formGroup) {
+      this.formGroup.addControl(this.FLAG_COMMENTS_WELSH_CONTROL_NAME, new FormControl(''));
+      this.formGroup.addControl(this.FLAG_STATUS_CHANGE_REASON_WELSH_CONTROL_NAME, new FormControl(''));
+    }
+  }
+}
