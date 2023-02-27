6150a73482f6b10cda8a04817184214964985a87
Matei <108479475+MateiHMCTS@users.noreply.github.com>
2023-02-27 17:10:36 +0000

    EUI-7726 (#1358)
    * EUI-7726
    
    * 7726
    
    * replace async with waitForAsync
    
    * QA comments

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/case-flag/enums/case-flag-wizard-step-title.enum.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/case-flag/enums/case-flag-wizard-step-title.enum.ts
@@ -1,8 +1,6 @@
 export enum CaseFlagWizardStepTitle {
   SELECT_FLAG_LOCATION = 'Where should this flag be added?',
   SELECT_CASE_FLAG = 'Select flag type',
-  SEARCH_LANGUAGE_INTERPRETER = 'Search for a language interpreter',
-  SEARCH_SIGN_LANGUAGE_INTERPRETER = 'Search for a sign language interpreter',
   ADD_FLAG_COMMENTS = 'Add comments for this flag',
   ADD_FLAG_COMMENTS_EXTERNAL_MODE = 'Tell us more about the request',
   CONFIRM_FLAG_STATUS = 'Confirm the status of the flag',
