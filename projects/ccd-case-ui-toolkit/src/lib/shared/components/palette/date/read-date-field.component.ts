2a30616f1e97bbb3596adcdc3f58029d6a69bbd2
Kartheek-S <kartheek.sarabu@hmcts.net>
2023-03-23 12:26:26 +0000

    bug/EUI-7687-fix temp fix

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/date/read-date-field.component.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/date/read-date-field.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormatTranslatorService } from '../../../services';
 import { AbstractFieldReadComponent } from '../base-field/abstract-field-read.component';
+import { DatePipe } from '../utils';
 
 @Component({
   selector: 'ccd-read-date-field',
-  template: `<span class="text-16">{{caseField.value | ccdDate:'utc':caseField.dateTimeDisplayFormat}}</span>`
+  template: '<span class="text-16">{{formattedDate}}</span>'
 })
-export class ReadDateFieldComponent extends AbstractFieldReadComponent {
-  public timeZone = 'utc';
+export class ReadDateFieldComponent extends AbstractFieldReadComponent implements OnInit {
+  public static readonly datePipe: DatePipe = new DatePipe(new FormatTranslatorService());
+
+  public formattedDate: string;
+
+  private get displayFormat(): string {
+    if (this.caseField.display_context_parameter) {
+      return this.extractFormat(this.caseField.display_context_parameter, '#DATETIMEDISPLAY');
+    }
+    return null;
+  }
+
+  private extractFormat(fmt: string, paramName: string, leftBracket = '(', rightBracket = ')'): string {
+    fmt = fmt.split(',')
+      .find(a => a.trim().startsWith(paramName));
+    if (fmt) {
+      const s = fmt.indexOf(leftBracket) + 1;
+      const e = fmt.indexOf(rightBracket, s);
+      if (e > s && s >= 0) {
+        return fmt.substr(s, (e - s));
+      }
+    }
+    return null;
+  }
+
+  public ngOnInit() {
+    this.formattedDate = ReadDateFieldComponent.datePipe.transform(
+      this.caseField.value,
+      'utc',
+      this.displayFormat
+    );
+  }
 }
