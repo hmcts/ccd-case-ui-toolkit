a9b95282063dc12703283766c9aefd06f58b99b3
John Benjamin <john.benjamin@hmcts.net>
2023-04-06 17:20:04 +0100

    EUI-7819 Case file view display document upload date

--- projects/ccd-case-ui-toolkit/src/lib/shared/domain/case-file-view/case-file-view-document.model.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/domain/case-file-view/case-file-view-document.model.ts
@@ -3,5 +3,5 @@ export class CaseFileViewDocument {
   public document_filename: string;
   public document_binary_url: string;
   public attribute_path: string;
-  public upload_timestamp: string;
+  public upload_timestamp: Date;
 }
