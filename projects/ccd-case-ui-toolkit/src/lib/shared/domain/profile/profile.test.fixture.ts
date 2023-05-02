b9fd53e5b898e18be8460eae10897e1fe7f7ae70
John Benjamin <john.benjamin@hmcts.net>
2023-05-02 12:03:34 +0100

    EUI-8286 Reasonable adjustments CYA hide unwanted fields

--- projects/ccd-case-ui-toolkit/src/lib/shared/domain/profile/profile.test.fixture.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/domain/profile/profile.test.fixture.ts
@@ -23,25 +23,3 @@ export const createAProfile = () => {
   return p;
 };
 
-export const createASolicitorProfile = () => {
-  const p = new Profile();
-  p.user = {
-    idam: {
-      id: '44',
-      email: 'solicitor@test.com',
-      forename: 'Test',
-      surname: 'Solicitor',
-      roles: ['divorce-solicitor']
-    }
-  };
-
-  p.default = {
-    workbasket: {
-      jurisdiction_id: 'TEST',
-      case_type_id: 'TEST',
-      state_id: 'TEST'
-    }
-  };
-
-  return p;
-};
