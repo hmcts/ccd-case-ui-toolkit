7b92d0c818ed62a460201d1bc0da144d634148dd
Matei Baciu <matei.baciu@hmcts.net>
2023-05-09 16:59:55 +0100

    EUI-8112

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/query-list/query-list.component.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/query-list/query-list.component.ts
@@ -4,15 +4,15 @@ import { PartyMessages } from '../../domain';
 @Component({
   selector: 'ccd-query-list',
   templateUrl: './query-list.component.html',
+  styleUrls: ['./query-list.component.scss']
 })
 export class QueryListComponent implements OnInit {
-
-  @Input() public partyMessages: PartyMessages[];
+  @Input() public partyMessages: PartyMessages;
 
   constructor() {
   }
 
   public ngOnInit(): void {
-
+    console.log(this.partyMessages);
   }
 }
