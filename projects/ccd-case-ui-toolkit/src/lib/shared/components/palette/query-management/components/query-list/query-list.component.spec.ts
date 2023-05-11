90be2e400a7973b0d2871f65f83530f050a74417
Matei Baciu <matei.baciu@hmcts.net>
2023-05-11 09:56:13 +0100

    change responseStatus to responseBy

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/query-list/query-list.component.spec.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/query-list/query-list.component.spec.ts
@@ -116,7 +116,7 @@ describe('QueryListComponent', () => {
       expect(tableCells[1].innerText).toEqual(firstRowItem.lastSubmittedBy);
       expect(new Date(tableCells[2].innerText)).toEqual(firstRowItem.lastSubmittedDate);
       expect(new Date(tableCells[3].innerText)).toEqual(firstRowItem.lastResponseDate);
-      expect(tableCells[4].innerText).toEqual(firstRowItem.responseStatus);
+      expect(tableCells[4].innerText).toEqual(firstRowItem.lastResponseBy);
     });
   });
 });
