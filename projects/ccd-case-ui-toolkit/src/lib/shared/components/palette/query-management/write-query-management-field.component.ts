0d64f6ed48b50b2ea004dceac57df1a7fc2ebe54
Matei Baciu <matei.baciu@hmcts.net>
2023-05-18 12:51:01 +0100

    remove unused import

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/write-query-management-field.component.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/write-query-management-field.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AbstractFieldWriteComponent } from '../base-field';
 import { QueryListItem } from './models';
