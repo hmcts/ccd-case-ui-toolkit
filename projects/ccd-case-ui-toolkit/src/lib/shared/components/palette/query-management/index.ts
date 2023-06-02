147c2b9475ab241dd9b6b731a259de2d8812fccf
Matei Baciu <matei.baciu@hmcts.net>
2023-06-02 12:07:30 +0100

    fix conflicts

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/index.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/index.ts
@@ -1,6 +1,5 @@
 export * from './__mocks__';
-export * from './components';
-export * from './domain';
+export * from './models';
 export * from './query-management.module';
 export * from './read-query-management-field.component';
 export * from './write-query-management-field.component';
