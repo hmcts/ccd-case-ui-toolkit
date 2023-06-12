9613a8b87432359eec960230a693f8de6ca430be
Ritesh Dsouza <ritesh.dsouza@HMCTS.net>
2023-06-12 12:27:31 +0100

    EXUI-343-button-text-missing

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/case-editor/case-edit-page/case-edit-page.component.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/case-editor/case-edit-page/case-edit-page.component.ts
@@ -103,6 +103,7 @@ export class CaseEditPageComponent implements OnInit, AfterViewChecked {
           }
           this.hasPreviousPage$.next(this.caseEdit.hasPrevious(this.currentPage?.id));
         }
+        this.triggerText = this.getTriggerText();
       });
     CaseEditPageComponent.setFocusToTop();
     this.caseEditDataService.caseEditForm$.subscribe({
@@ -119,7 +120,6 @@ export class CaseEditPageComponent implements OnInit, AfterViewChecked {
         }
       }
     });
-    this.triggerText = this.getTriggerText();
   }
 
   public ngAfterViewChecked(): void {
