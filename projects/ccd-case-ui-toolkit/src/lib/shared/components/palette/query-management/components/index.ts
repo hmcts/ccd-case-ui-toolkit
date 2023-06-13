dc72abf2cc0a2ea406a501489b80299c763d51cd
John Benjamin <john.benjamin@hmcts.net>
2023-06-13 16:11:26 +0100

    EUI-8453 QM raise a query error scenarios

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/index.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/index.ts
@@ -1,9 +1,9 @@
 export * from './query-case-details-header/query-case-details-header.component';
 export * from './query-create/query-create.component';
-export * from './query-details/query-details.component';
 export * from './query-details-table/query-details-table.component';
+export * from './query-details/query-details.component';
 export * from './query-list/query-list.component';
-
 export * from './query-write/query-write-add-documents/query-write-add-documents.component';
+export * from './query-write/query-write-date-input/query-write-date-input.component';
 export * from './query-write/query-write-raise-query/query-write-raise-query.component';
 export * from './query-write/query-write-respond-to-query/query-write-respond-to-query.component';
