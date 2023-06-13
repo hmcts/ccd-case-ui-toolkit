6f3a59dcfabfbfa0f84414410ac795c68baf670b
Matei Baciu <matei.baciu@hmcts.net>
2023-06-13 17:59:53 +0100

    EUI-8451

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/query-write/query-write-respond-to-query/query-write-respond-to-query.component.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/query-write/query-write-respond-to-query/query-write-respond-to-query.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { QueryItemType, QueryListItem } from '../../../models';
+import { QueryCreateContext, QueryListItem } from '../../../models';
 
 @Component({
   selector: 'ccd-query-write-respond-to-query',
@@ -11,6 +11,6 @@ export class QueryWriteRespondToQueryComponent {
   @Input() public queryItem: QueryListItem;
   @Input() public formGroup: FormGroup;
   // Set default value as false for testing follow up EUI-8454
-  @Input() public QueryCreateContext: QueryItemType = QueryItemType.FOLLOWUP;
-  public readonly queryItemTypeEnum = QueryItemType;
+  @Input() public QueryCreateContext: QueryCreateContext = QueryCreateContext.FOLLOWUP;
+  public readonly queryItemTypeEnum = QueryCreateContext;
 }
