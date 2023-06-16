b83302571a503b876e1e206fb91f764e28d66d8c
John Benjamin <john.benjamin@hmcts.net>
2023-06-16 12:59:45 +0100

    EUI-8523 QQ create component

---
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/qualifying-questions/qualifying-questions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ccd-qualifying-questions',
+  templateUrl: './qualifying-questions.component.html'
+})
+export class QualifyingQuestionsComponent implements OnInit {
+
+  
+
+  public ngOnInit(): void {
+
+  }
+
+}
