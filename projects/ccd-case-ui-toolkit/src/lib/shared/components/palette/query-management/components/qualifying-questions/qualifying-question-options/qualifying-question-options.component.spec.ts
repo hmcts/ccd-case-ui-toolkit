24a46b22399d3fca3158a6f0030327f253f3b38e
John Benjamin <john.benjamin@hmcts.net>
2023-06-22 15:01:07 +0100

    EUI-8524 QQ create component step 2

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/qualifying-questions/qualifying-question-options/qualifying-question-options.component.spec.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/qualifying-questions/qualifying-question-options/qualifying-question-options.component.spec.ts
@@ -1,28 +1,80 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormControl, Validators } from '@angular/forms';
 import { MockRpxTranslatePipe } from '../../../../../../../shared/test/mock-rpx-translate.pipe';
+import { By } from '@angular/platform-browser';
+import { QualifyingQuestionsErrorMessage } from '../../../enums';
 import { QualifyingQuestionOptionsComponent } from './qualifying-question-options.component';
 
 describe('QualifyingQuestionOptionsComponent', () => {
   let component: QualifyingQuestionOptionsComponent;
   let fixture: ComponentFixture<QualifyingQuestionOptionsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
       declarations: [
         QualifyingQuestionOptionsComponent,
         MockRpxTranslatePipe
       ]
     })
     .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(QualifyingQuestionOptionsComponent);
     component = fixture.componentInstance;
+    component.qualifyingQuestionsControl = new FormControl(null, Validators.required);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  afterEach(() => {
+    fixture.destroy();
+  });
+
+  it('should create component', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('displayError', () => {
+    describe('true', () => {
+      it('should return true when qualifyingQuestionsControl is touched and has required error', () => {
+        component.qualifyingQuestionsControl.markAsTouched();
+        component.qualifyingQuestionsControl.setValue(null);
+        expect(component.displayError).toBe(true);
+      });
+    });
+
+    describe('false', () => {
+      it('should return false when qualifyingQuestionsControl is not touched', () => {
+        expect(component.qualifyingQuestionsControl.touched).toBe(false);
+        expect(component.displayError).toBe(false);
+      });
+
+      it('should return false when qualifyingQuestionsControl is touched and does not have required error', () => {
+        component.qualifyingQuestionsControl.markAsTouched();
+        component.qualifyingQuestionsControl.setValue('test');
+        expect(component.displayError).toBe(false);
+      });
+    });
+  });
+
+  describe('should display errors in template', () => {
+    beforeEach(() => {
+      component.qualifyingQuestionsControl.markAsTouched();
+      component.qualifyingQuestionsControl.setValue(null);
+      fixture.detectChanges();
+    });
+
+    it('should have the govuk-form-group-error class', () => {
+      const formGroupEl = fixture.debugElement.query(By.css('.govuk-form-group'));
+      expect(formGroupEl.nativeElement.classList).toContain('govuk-form-group--error');
+    });
+
+    it('should display govuk-error-message', () => {
+      const errorMessageEl = fixture.debugElement.query(By.css('.govuk-error-message'));
+      expect(errorMessageEl).toBeTruthy();
+      expect(errorMessageEl.nativeElement.textContent.trim()).toBe(`Error: ${QualifyingQuestionsErrorMessage.SELECT_AN_OPTION}`);
+    });
   });
 });
--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/qualifying-questions/qualifying-question-options/qualifying-question-options.component.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/qualifying-questions/qualifying-question-options/qualifying-question-options.component.ts
@@ -9,12 +9,13 @@ import { QualifyingQuestion, QueryItemType } from '../../../models';
   templateUrl: './qualifying-question-options.component.html'
 })
 export class QualifyingQuestionOptionsComponent {
-  @Input() public qualifyingQuestionControl: FormControl;
+  @Input() public qualifyingQuestionsControl: FormControl;
   @Input() public qualifyingQuestions$: Observable<QualifyingQuestion[]>;
   public queryItemTypeEnum = QueryItemType;
   public qualifyingQuestionsErrorMessage = QualifyingQuestionsErrorMessage;
 
   public get displayError(): boolean {
-    return this.qualifyingQuestionControl.touched && this.qualifyingQuestionControl.hasError('required');
+    return this.qualifyingQuestionsControl.touched && this.qualifyingQuestionsControl.hasError('required');
   }
 }
+
