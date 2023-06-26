83bcc85a5f81e3e758ff8264016c8611d7933b3c
Matei Baciu <matei.baciu@hmcts.net>
2023-06-26 12:36:08 +0100

    EUI-8401

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/query-list/query-list.component.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/query-list/query-list.component.ts
@@ -12,7 +12,7 @@ export class QueryListComponent implements OnChanges {
   @Output() public selectedQuery: EventEmitter<QueryListItem> = new EventEmitter();
   public queryListData: QueryListData | undefined;
   public displayedColumns: QueryListColumn[] = [
-    { name: 'subject', displayName: 'Queries', sortOrder: SortOrder.UNSORTED },
+    { name: 'subject', displayName: 'Query subject', sortOrder: SortOrder.UNSORTED },
     { name: 'lastSubmittedBy', displayName: 'Last submitted by', sortOrder: SortOrder.UNSORTED },
     { name: 'lastSubmittedDate', displayName: 'Last submission date', sortOrder: SortOrder.UNSORTED },
     { name: 'lastResponseDate', displayName: 'Last response date', sortOrder: SortOrder.UNSORTED },
@@ -40,13 +40,16 @@ export class QueryListComponent implements OnChanges {
     }
   }
 
-  public sortWidget(col: QueryListColumn): string {
+  public getAriaSortHeaderValue(col: QueryListColumn): 'ascending' | 'descending' | 'none' {
     switch (col.sortOrder) {
       case SortOrder.ASCENDING: {
-        return '&#9650;';
+        return 'ascending';
+      }
+      case SortOrder.DESCENDING: {
+        return 'descending';
       }
       default: {
-        return '&#9660;';
+        return 'none';
       }
     }
   }
