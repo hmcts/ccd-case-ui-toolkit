d44afff43670d5d7f70221561b4aea3472836e78
Matei Baciu <matei.baciu@hmcts.net>
2023-06-27 15:37:54 +0100

    EUI-8526

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/query-check-your-answers/query-check-your-answers.component.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/query-check-your-answers/query-check-your-answers.component.ts
@@ -1,9 +1,9 @@
 import { Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { CaseField } from '../../../../../domain';
-import { caseFieldMockData } from '../../__mocks__';
 import { QueryItemType, QueryListItem } from '../../models';
 import { QueryManagementUtils } from '../../utils/query-management.utils';
+import { caseFieldMockData } from '../../__mocks__';
 
 @Component({
   selector: 'ccd-query-check-your-answers',
@@ -12,7 +12,6 @@ import { QueryManagementUtils } from '../../utils/query-management.utils';
   encapsulation: ViewEncapsulation.None
 })
 export class QueryCheckYourAnswersComponent implements OnInit {
-
   @Input() public formGroup: FormGroup;
   @Input() public queryItem: QueryListItem;
   @Input() public queryCreateContext: QueryItemType;
