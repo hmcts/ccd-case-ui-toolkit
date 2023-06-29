440dfc63458696b64f25aa4098f47acdac8bb024
Matei Baciu <matei.baciu@hmcts.net>
2023-06-29 11:00:20 +0100

    EUI-8481

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/index.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/index.ts
@@ -2,7 +2,6 @@ export * from './qualifying-questions/qualifying-question-detail/qualifying-ques
 export * from './qualifying-questions/qualifying-question-options/qualifying-question-options.component';
 export * from './query-case-details-header/query-case-details-header.component';
 export * from './query-check-your-answers/query-check-your-answers.component';
-export * from './query-details-table/query-details-table.component';
 export * from './query-details/query-details.component';
 export * from './query-list/query-list.component';
 export * from './query-write/query-write-date-input/query-write-date-input.component';
