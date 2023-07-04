7e238433cd8c7b4af900e82098904e327c5075cf
DavidJayakumar <David.Jayakumar@hmcts.net>
2023-07-04 09:30:09 +0100

    Show caseworker name

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/query-details/query-details.component.spec.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/query-details/query-details.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { SessionStorageService } from '../../../../../services';
 import { QueryDetailsComponent } from './query-details.component';
 
 describe('QueryDetailsComponent', () => {
   let component: QueryDetailsComponent;
   let fixture: ComponentFixture<QueryDetailsComponent>;
+  const mockSessionStorageService = jasmine.createSpyObj<SessionStorageService>('SessionStorageService', ['getItem']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ QueryDetailsComponent ]
+      declarations: [ QueryDetailsComponent ],
+      providers: [ {provide: SessionStorageService, useValue: mockSessionStorageService } ]
     })
     .compileComponents();
   });
@@ -28,4 +30,33 @@ describe('QueryDetailsComponent', () => {
     component.onBack();
     expect(component.backClicked.emit).toHaveBeenCalled();
   });
+
+  describe('isCaseworker', () => {
+    const USER = {
+      roles: [
+        'caseworker'
+      ]
+    };
+
+    it('should return true if the user doesnt have pui-case-manager', () => {
+      mockSessionStorageService.getItem.and.returnValue(JSON.stringify(USER));
+      fixture.detectChanges();
+      expect(component.isCaseworker()).toBeTruthy();
+    });
+
+    it('should return true if the user doesnt have pui-case-manager', () => {
+      USER.roles.push('pui-case-manager');
+      mockSessionStorageService.getItem.and.returnValue(JSON.stringify(USER));
+      fixture.detectChanges();
+      expect(component.isCaseworker()).toBeFalsy();
+      USER.roles.pop();
+    });
+
+    it('should return true if the user doesnt have pui-case-manager', () => {
+      USER.roles.push('Civil-Judge');
+      mockSessionStorageService.getItem.and.returnValue(JSON.stringify(USER));
+      fixture.detectChanges();
+      expect(component.isCaseworker()).toBeFalsy();
+    });
+  })
 });
--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/query-details/query-details.component.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/components/query-details/query-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { SessionStorageService } from '../../../../../services';
 import { QueryListItem } from '../../models';
 
 @Component({
@@ -10,7 +11,16 @@ export class QueryDetailsComponent {
   @Input() public query: QueryListItem;
   @Output() public backClicked: EventEmitter<boolean> = new EventEmitter();
 
+  constructor(private sessionStorage: SessionStorageService) { }
+
   public onBack(): void {
     this.backClicked.emit(true);
+  }
+
+  public isCaseworker(): boolean {
+    const userDetails = JSON.parse(this.sessionStorage.getItem('userDetails'));
+    return userDetails && userDetails.roles
+      && !(userDetails.roles.includes('pui-case-manager')
+        || userDetails.roles.some((role) => role.toLowerCase().includes('judge')))
   }
 }
