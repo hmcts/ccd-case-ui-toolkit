b1b5b99bc3394da95b70bf6de1eb12f9eb15d82e
RiteshHMCTS <74713687+RiteshHMCTS@users.noreply.github.com>
2023-09-28 16:49:54 +0100

    Revert "fix for exui-837 (#1559)"
    This reverts commit 71c6cd2a790e82157b4744bfd3caf06ae577ddbc.

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/linked-cases/write-linked-cases-field.component.spec.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/linked-cases/write-linked-cases-field.component.spec.ts
@@ -28,7 +28,6 @@ import { LinkedCasesService } from './services';
 import { WriteLinkedCasesFieldComponent } from './write-linked-cases-field.component';
 
 import createSpyObj = jasmine.createSpyObj;
-import { ValidPageListCaseFieldsService } from '../../case-editor/services/valid-page-list-caseFields.service';
 
 describe('WriteLinkedCasesFieldComponent', () => {
   let component: WriteLinkedCasesFieldComponent;
@@ -86,7 +85,7 @@ describe('WriteLinkedCasesFieldComponent', () => {
   const caseFieldService = new CaseFieldService();
   const pageValidationService = new PageValidationService(caseFieldService);
   caseEditPageComponent = new CaseEditPageComponent(caseEditComponentStub,
-    route, formValueService, formErrorService, null, pageValidationService, dialog, caseFieldService, new CaseEditDataService(), new LoadingService(), new ValidPageListCaseFieldsService());
+    route, formValueService, formErrorService, null, pageValidationService, dialog, caseFieldService, new CaseEditDataService(), new LoadingService());
 
   const caseInfo = {
     case_id: '1682374819203471',
