f6d4384dba6d18be6baa08ad4e2c679ad408a727
John Benjamin <john.benjamin@hmcts.net>
2023-10-17 13:53:47 +0100

    EUI-8816 Restricted Case Access - Feature toggle for deployment to Production

--- RELEASE-NOTES.md
+++ RELEASE-NOTES.md
@@ -1,5 +1,5 @@
 ## RELEASE NOTES
-### Version 6.19.6-restricted-case-access-v2
+### Version 6.19.6-restricted-case-access-v3
 **EUI-6645** Restricted case access
 
 ### Version 6.19.5
--- projects/ccd-case-ui-toolkit/src/lib/shared/components/case-viewer/services/case.resolver.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/case-viewer/services/case.resolver.ts
@@ -3,7 +3,7 @@ import { ActivatedRouteSnapshot, NavigationEnd, Resolve, Router } from '@angular
 import { plainToClassFromExist } from 'class-transformer';
 import { of, throwError } from 'rxjs';
 import { catchError, filter, map } from 'rxjs/operators';
-
+import { AbstractAppConfig } from '../../../../app.config';
 import { CaseView, Draft } from '../../../domain';
 import { DraftService, NavigationOrigin, SessionStorageService } from '../../../services';
 import { NavigationNotifierService } from '../../../services/navigation/navigation-notifier.service';
@@ -25,7 +25,8 @@ export class CaseResolver implements Resolve<CaseView> {
               private draftService: DraftService,
               private navigationNotifierService: NavigationNotifierService,
               private router: Router,
-              private sessionStorage: SessionStorageService) {
+              private sessionStorage: SessionStorageService,
+              private readonly appConfig: AbstractAppConfig) {
     router.events.pipe(filter(event => event instanceof NavigationEnd))
       .subscribe((event: NavigationEnd) => {
         this.previousUrl = event.url;
@@ -104,8 +105,8 @@ export class CaseResolver implements Resolve<CaseView> {
       this.router.navigate(['/list/case']);
       return of(null);
     }
-    // Error 403, navigate to restricted case access page
-    if (error.status === 403) {
+    // Error 403 and enable-restricted-case-access Launch Darkly flag is enabled, navigate to restricted case access page
+    if (error.status === 403 && this.appConfig.getEnableRestrictedCaseAccessConfig()) {
       this.router.navigate([`/cases/restricted-case-access/${caseReference}`]);
       return of(null);
     }
