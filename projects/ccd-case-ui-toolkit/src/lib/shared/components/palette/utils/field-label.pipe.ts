d8b93a26a52c7094233ae67ff0a926ae6f4f7a73
DavidJayakumar <David.Jayakumar@hmcts.net>
2023-12-18 09:44:41 +0000

    Fix issue with optional and CYA translation

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/utils/field-label.pipe.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/utils/field-label.pipe.ts
@@ -1,17 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { RpxTranslatePipe } from 'rpx-xui-translation';
 import { CaseField } from '../../../domain/definition/case-field.model';
 
 @Pipe({
-  name: 'ccdFieldLabel'
+  name: 'ccdFieldLabel',
+  pure: false
 })
 export class FieldLabelPipe implements PipeTransform {
 
+  constructor(
+    private readonly rpxTranslationPipe: RpxTranslatePipe
+  ) {}
+  
   public transform (field: CaseField): string {
     if (!field || !field.label) {
       return '';
     } else if (!field.display_context) {
-      return field.label;
+      return this.rpxTranslationPipe.transform(field.label);
     }
-    return field.label + (field.display_context.toUpperCase() === 'OPTIONAL' ? ' (Optional)' : '');
+    return this.rpxTranslationPipe.transform(field.label) + (field.display_context.toUpperCase() === 'OPTIONAL' ? 
+      ' (' + this.rpxTranslationPipe.transform('Optional') + ')' : '');
   }
 }
