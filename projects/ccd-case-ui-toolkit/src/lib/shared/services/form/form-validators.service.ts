8e09b23ca8c945ce9be96485eeb1e891c5b47278
olusegun odunukan <olusegun.odunukan@hmcts.net>
2024-01-09 09:17:55 +0000

    revert back regex pattern

--- projects/ccd-case-ui-toolkit/src/lib/shared/services/form/form-validators.service.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/services/form/form-validators.service.ts
@@ -58,7 +58,7 @@ export class FormValidatorsService {
   }
 
   public static markDownPatternValidator(): ValidatorFn {
-    const pattern = /(?:\[.*?\]\(.*?\)|!\[.*?\]\(.*?\)|<img[^>]*>|<a[^>]*>.*?<\/a>)/g;
+    const pattern = /(\[[^\]]{0,500}\]\([^)]{0,500}\)|!\[[^\]]{0,500}\]\([^)]{0,500}\)|<img[^>]{0,500}>|<a[^>]{0,500}>.*?<\/a>)/g;
 
     return (control: AbstractControl): ValidationErrors | null => {
       return control?.value?.toString().trim().length > 0 && pattern.test(control.value)
