3412d1c7c9d343f9a28009784b15cda0cba9ba9a
olusegun odunukan <olusegun.odunukan@hmcts.net>
2024-03-19 11:26:15 +0000

    testing

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/search-result/search-result.module.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/search-result/search-result.module.ts
@@ -4,13 +4,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { RpxTranslationModule } from 'rpx-xui-translation';
-import { LabelSubstitutorModule } from '../../directives/substitutor/label-substitutor.module';
-import { PipesModule } from '../../pipes/pipes.module';
+import { LabelSubstitutorModule } from '../../directives/substitutor';
+import { PipesModule } from '../../pipes';
 import { BrowserService } from '../../services/browser/browser.service';
 import { SearchResultViewItemComparatorFactory } from '../../services/search-result/sorting/search-result-view-item-comparator-factory';
-import { ActivityModule } from '../activity/activity.module';
-import { PaginationModule } from '../pagination/pagination.module';
-import { PaletteModule } from '../palette/palette.module';
+import { ActivityModule } from '../activity';
+import { PaginationModule } from '../pagination';
+import { PaletteModule } from '../palette';
 import { SearchResultComponent } from './search-result.component';
 
 @NgModule({
