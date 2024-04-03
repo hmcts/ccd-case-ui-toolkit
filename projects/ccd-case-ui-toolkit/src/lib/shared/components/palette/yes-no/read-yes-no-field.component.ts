5c4d7a14a2e440f8b3440f4e1a28095801cd82a5
olusegun odunukan <olusegun.odunukan@hmcts.net>
2024-04-03 15:09:55 +0100

    fix missing label

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/yes-no/read-yes-no-field.component.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/yes-no/read-yes-no-field.component.ts
@@ -4,7 +4,7 @@ import { YesNoService } from './yes-no.service';
 
 @Component({
   selector: 'ccd-read-yes-no-field',
-  template: `<span class="text-16">{{formattedValue ? (caseField.label | rpxTranslate : null : formattedValue) : null}}</span>`
+  template: `<span class="text-16">{{formattedValue ? (caseField.value | rpxTranslate : null : formattedValue) : null}}</span>`
 })
 export class ReadYesNoFieldComponent extends AbstractFieldReadComponent implements OnInit {
   public formattedValue: string;
