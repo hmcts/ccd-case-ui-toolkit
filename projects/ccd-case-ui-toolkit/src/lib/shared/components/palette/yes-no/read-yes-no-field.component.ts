ee303ae56fb589ac0969883bb68b84761f316c5f
olusegun odunukan <olusegun.odunukan@hmcts.net>
2024-04-05 11:09:31 +0100

    Update read-yes-no-field.component.ts

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/yes-no/read-yes-no-field.component.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/yes-no/read-yes-no-field.component.ts
@@ -4,7 +4,7 @@ import { YesNoService } from './yes-no.service';
 
 @Component({
   selector: 'ccd-read-yes-no-field',
-  template: `<span class="text-16">{{formattedValue ? (caseField.value | rpxTranslate : null : formattedValue) : null}}</span>`
+  template: `<span class="text-16">{{formattedValue | rpxTranslate}}</span>`
 })
 export class ReadYesNoFieldComponent extends AbstractFieldReadComponent implements OnInit {
   public formattedValue: string;
