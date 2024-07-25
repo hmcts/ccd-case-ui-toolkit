04dc9885f38a75307607667f18e5dc5a7007f68d
Andy Wilkins <andy.wilkins@hmcts.net>
2024-07-25 17:12:43 +0100

    clean up

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/dynamic-radio-list/write-dynamic-radio-list-field.component.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/dynamic-radio-list/write-dynamic-radio-list-field.component.ts
@@ -18,7 +18,7 @@ export class WriteDynamicRadioListFieldComponent extends AbstractFieldWriteCompo
       this.caseField.list_items = this.caseField.formatted_value.list_items;
     }
 
-    if (!this.caseField.value && this.caseField.formatted_value && this.caseField.formatted_value.value) {
+    if (!this.caseField.value && this.caseField.formatted_value?.value) {
       this.caseField.value = this.caseField.formatted_value.value?.code;
     }
 
