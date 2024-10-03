5a05ae6f4c35b2798bb415e92fe90dac68a20458
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2024-10-03 17:06:23 +0100

    Add the fix for arrays within case read field table
    Add dummy path and test to ensure show condition actually works as expected

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/complex/read-complex-field-table.component.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/complex/read-complex-field-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CaseField } from '../../../domain/definition';
 import { AbstractFieldReadComponent } from '../base-field/abstract-field-read.component';
 
@@ -7,7 +7,27 @@ import { AbstractFieldReadComponent } from '../base-field/abstract-field-read.co
   templateUrl: './read-complex-field-table.html',
   styleUrls: ['./read-complex-field-table.scss']
 })
-export class ReadComplexFieldTableComponent extends AbstractFieldReadComponent {
+export class ReadComplexFieldTableComponent extends AbstractFieldReadComponent implements OnInit {
+  // parent_ can be replaced with any ***_ - underscore is only important character
+  // value can also be replaced with anything
+  public static readonly DUMMY_STRING_PRE = 'parent_';
+  public static readonly DUMMY_STRING_POST = 'value';
+
   @Input()
   public caseFields: CaseField[] = [];
+
+  public path: string;
+
+  public ngOnInit(): void {
+    this.setDummyPathForChildArrays();
+  }
+
+  /* In order to get child arrays (within casefield) to display their logic
+    we need to add a path. This path needs to include the idPrefix as that
+    is the part of the path that is used to display the elements.
+    The joining strings will allow us to use the existing show condition to
+    match against this path. */
+  private setDummyPathForChildArrays(): void {
+    this.path = ReadComplexFieldTableComponent.DUMMY_STRING_PRE + this.idPrefix + ReadComplexFieldTableComponent.DUMMY_STRING_POST;
+  }
 }
