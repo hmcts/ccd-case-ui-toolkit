fea03cc048e9758188752444f94650671841d850
Josh <josh.glasgow@hmcts.net>
2024-10-07 12:47:38 +0100

    add test for external user not being shown hyperlink

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/history/event-log/event-log-table.component.spec.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/history/event-log/event-log-table.component.spec.ts
@@ -59,6 +59,7 @@ describe('EventLogTableComponent', () => {
   const $TABLE_ROW_LINKS_TIMELINE = By.css('table>tbody>tr>td>div#case-timeline>a');
   const $TABLE_ROW_LINKS_STANDALONE = By.css('table>tbody>tr>td>div:not(.tooltip)>a');
   const $TABLE_COLUMNS = By.css('table>tbody>tr>td');
+  const $TABLE_ROW_LINKS_TIMELINE_EXTERNAL = By.css('table>tbody>tr>td>div>span.event-link');
 
   const USER = {
     roles: [
@@ -210,10 +211,8 @@ describe('EventLogTableComponent', () => {
   });
 
   describe('Case timeline use', () => {
-
     let caseHistoryHandler;
     beforeEach(waitForAsync(() => {
-
       caseHistoryHandler = createSpyObj('caseHistoryHandler', ['apply']);
 
       TestBed
@@ -251,6 +250,50 @@ describe('EventLogTableComponent', () => {
       fixture.detectChanges();
 
       expect(caseHistoryHandler.apply).toHaveBeenCalledWith(4);
+    });
+  });
+
+  describe('External user', () => {
+    beforeEach(waitForAsync(() => {
+      TestBed
+        .configureTestingModule({
+          imports: [RouterTestingModule],
+          declarations: [
+            EventLogTableComponent,
+            DatePipe,
+            MockRpxTranslatePipe
+          ],
+          providers: [FormatTranslatorService,
+            { provide: SessionStorageService, useValue: mockSessionStorageService }
+          ]
+        })
+        .compileComponents();
+
+      fixture = TestBed.createComponent(EventLogTableComponent);
+      component = fixture.componentInstance;
+      const EXTERNAL_USER = {
+        roles: [
+          'pui-case-manager'
+        ]
+      };
+      mockSessionStorageService.getItem.and.returnValue(JSON.stringify(EXTERNAL_USER));
+
+      component.events = EVENTS;
+      component.selected = SELECTED_EVENT;
+      component.isPartOfCaseTimeline = false;
+
+      de = fixture.debugElement;
+      fixture.detectChanges();
+    }));
+
+    it('should not render hyperlink for each row and link to event id', () => {
+      const links = de.queryAll($TABLE_ROW_LINKS_TIMELINE_EXTERNAL);
+      expect(component.isUserExternal).toBeTruthy();
+      expect(links.length).toBe(2);
+      expect(links[0].nativeElement.getAttribute('href')).toBeNull();
+      expect(links[1].nativeElement.getAttribute('href')).toBeNull();
+      expect(links[0].nativeElement.textContent).toContain('Update a case');
+      expect(links[1].nativeElement.textContent).toContain('Update a case');
     });
   });
 });
