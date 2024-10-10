7de7b79c86c2b68a09dff21fed0bfa5a17de407b
Josh <josh.glasgow@hmcts.net>
2024-10-10 10:54:46 +0100

    Update to yarn v4.5.0

--- bin/run-yarn-audit.sh
+++ bin/run-yarn-audit.sh
@@ -1,38 +1,43 @@
-#!/usr/bin/env bash
-set +e
-yarn audit
-result=$?
-set -e
+#!/bin/bash
 
-if [[ "$result" != 0 ]]; then
-  if [[ -f yarn-audit-known-issues ]]; then
-    set +e
-    yarn audit --json | grep auditAdvisory > yarn-audit-issues
-    set -e
-    new_vulnerabilities=false
-    while read -r line; do
-        url=$(node -pe 'JSON.parse(process.argv[1]).data.advisory.url' "$line")
-        if ! grep -q "$url" yarn-audit-known-issues; then
-          echo "unknown vulnerability:$url"
-          new_vulnerabilities=true
-        fi
-    done < yarn-audit-issues
+# Define the file paths
+upToDateVulnerabilities=$(mktemp)
+vulnerabilitiesInRepo="./yarn-audit-known-issues"
 
-    if [[ "$new_vulnerabilities" = true ]] ; then
-      echo
-      echo Security vulnerabilities were found that were not ignored
-      echo
-      echo Check to see if these vulnerabilities apply to production
-      echo and/or if they have fixes available. If they do not have
-      echo fixes and they do not apply to production, you may ignore them
-      echo
-      echo To ignore these vulnerabilities, please add advisories urls
-      echo "to yarn-audit-known-issues (eg: https://npmjs.com/advisories/755)"
-      echo
-      echo and commit the yarn-audit-known-issues file.
+# Run yarn npm audit and save the output to upToDateVulnerabilities
+yarn npm audit --recursive --environment production --json > "$upToDateVulnerabilities"
 
-      exit "$result"
-    fi
-
-    fi
+# Check if both files exist
+if [[ ! -f "$upToDateVulnerabilities" ]]; then
+  echo "File $upToDateVulnerabilities does not exist."
+  exit 1
 fi
+
+if [[ ! -f "$vulnerabilitiesInRepo" ]]; then
+  echo "File $vulnerabilitiesInRepo does not exist."
+  exit 1
+fi
+
+# Compare the files and output the differences
+diff_output=$(diff "$upToDateVulnerabilities" "$vulnerabilitiesInRepo")
+
+if [[ -n "$diff_output" ]]; then
+  echo
+  echo "Security vulnerabilities were found that were not ignored"
+  echo
+  echo "Check to see if these vulnerabilities apply to production"
+  echo "and/or if they have fixes available. If they do not have"
+  echo "fixes and they do not apply to production, you may ignore them"
+  echo
+  echo "To ignore these vulnerabilities, run:"
+  echo 'yarn npm audit --recursive --environment production --json > yarn-audit-known-issues'
+  echo
+  echo "$diff_output"
+  
+  exit 1
+else
+  echo "All vulnerabilities are suppressed."
+fi
+
+# Clean up the temporary file
+rm "$upToDateVulnerabilities"
