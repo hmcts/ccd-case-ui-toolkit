6b1a0eb714934e0f809858884a3f79da7b187813
Josh <josh.glasgow@hmcts.net>
2025-02-27 10:59:04 +0000

    add additional checks for values being there

--- projects/ccd-case-ui-toolkit/src/lib/shared/services/form/form-value.service.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/services/form/form-value.service.ts
@@ -303,11 +303,13 @@ export class FormValueService {
       const fieldId = data[field].id;
       if (Array.isArray(caseField.data[fieldId])) {
         for (const subField in caseField.data[fieldId]) {
-          if (caseField.data[fieldId][subField] !== data[field].formatted_value[subField]) {
-            caseField.data[fieldId][subField] = data[field].formatted_value[subField];
+          if (data[field]?.formatted_value){
+            if (caseField.data[fieldId][subField] !== data[field]?.formatted_value[subField]) {
+              caseField.data[fieldId][subField] = data[field].formatted_value[subField];
+            }
           }
         }
-      } else if (caseField.data[fieldId] !== data[field].formatted_value) {
+      } else if ((data[field].formatted_value) && (caseField.data[fieldId] !== data[field].formatted_value)) {
         caseField.data[fieldId] = data[field].formatted_value;
       }
     }
