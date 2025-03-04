ce3754ebbcc398ddbfe5d89d8b3e1bbc172213a2
StoeBenHMCTS <Ben.Stone@HMCTS.NET>
2025-03-04 12:50:44 +0000

    updates to a callback function to get around issues

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/case-file-view/components/shared/case-file-view-overlay-menu/case-file-view-overlay-menu.component.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/case-file-view/components/shared/case-file-view-overlay-menu/case-file-view-overlay-menu.component.ts
@@ -19,4 +19,9 @@ export class CaseFileViewOverlayMenuComponent {
     this.isOpen = isOpen;
     this.isOpenChange.emit(isOpen);
   }
+
+  public callback(func: any): void {
+    func();
+    this.closeOverlay();
+  }
 }
