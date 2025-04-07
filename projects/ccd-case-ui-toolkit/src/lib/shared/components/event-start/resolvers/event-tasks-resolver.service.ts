59fc42038523845be0e4e79be8e88e44e5984a18
Josh <josh.glasgow@hmcts.net>
2025-04-07 15:06:39 +0100

    event task resolver - remove session storage

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/event-start/resolvers/event-tasks-resolver.service.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/event-start/resolvers/event-tasks-resolver.service.ts
@@ -4,30 +4,36 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Task } from '../../../domain/work-allocation/Task';
 import { TaskPayload } from '../../../domain/work-allocation/TaskPayload';
-import { SessionStorageService } from '../../../services/session/session-storage.service';
 import { WorkAllocationService } from '../../case-editor/services/work-allocation.service';
 import { AbstractAppConfig } from '../../../../app.config';
+import { CaseNotifier } from '../../case-editor';
 
 @Injectable()
 export class EventTasksResolverService implements Resolve<Task[]> {
+  jurisdiction: any;
+  caseType: any;
 
   constructor(private readonly service: WorkAllocationService,
-              private readonly sessionStorageService: SessionStorageService,
-              private readonly abstractConfig: AbstractAppConfig) {
+              private readonly abstractConfig: AbstractAppConfig,
+              private readonly caseNotifier: CaseNotifier) {
+    this.caseNotifier.caseView.subscribe((caseDetails) => {
+      if (caseDetails) {
+        this.jurisdiction = caseDetails?.case_type?.jurisdiction?.id;
+        this.caseType = caseDetails?.case_type?.id;
+      }
+    });
   }
 
   public resolve(route: ActivatedRouteSnapshot): Observable<Task[]> {
     const eventId = route.queryParamMap.get('eventId');
     const caseId = route.queryParamMap.get('caseId');
-    const caseInfoStr = this.sessionStorageService.getItem('caseInfo');
-    const caseInfo = JSON.parse(caseInfoStr);
-    if (caseInfo) {
-      return this.service.getTasksByCaseIdAndEventId(eventId, caseId, caseInfo.caseType, caseInfo.jurisdiction)
-      .pipe(
-        map((payload: TaskPayload) => payload.tasks)
-      );
-    } else {
-      this.abstractConfig.logMessage(`EventTasksResolverService: caseInfo details not available in session storage for ${caseId}`);
+    console.log(this.caseType, this.jurisdiction)
+    if (this.caseType && this.jurisdiction) {
+      return this.service.getTasksByCaseIdAndEventId(eventId, caseId, this.caseType, this.jurisdiction)
+        .pipe(
+          map((payload: TaskPayload) => payload.tasks)
+        );
     }
+    this.abstractConfig.logMessage(`EventTasksResolverService: caseInfo details not available in session storage for ${caseId}`);
   }
 }
