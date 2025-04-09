94e83f28ef2cb71553facb9a847ba67b579eaced
RiteshHMCTS <ritesh.dsouza@hmcts.net>
2025-04-09 11:59:52 +0100

    url changes

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/linked-cases/read-linked-cases-field.component.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/linked-cases/read-linked-cases-field.component.ts
@@ -72,7 +72,7 @@ export class ReadLinkedCasesFieldComponent implements OnInit, AfterViewInit {
   }
 
   public reloadCurrentRoute(): void {
-    this.router.navigate(['cases', 'case-details', this.linkedCasesService.caseDetails.case_id], { fragment: 'Linked cases' });
+    this.router.navigate(['cases', 'case-details', this.linkedCasesService.caseJurisdictionID, this.linkedCasesService.caseDetails.case_type, this.linkedCasesService.caseDetails.case_id], { fragment: 'Linked cases' });
   }
 
   public getFailureLinkedToNotification(evt): void {
