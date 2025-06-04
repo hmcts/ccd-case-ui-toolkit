eeee971ba30f5e226c5a51625b4ffe785915ac16
Olu <142989683+olusegz07@users.noreply.github.com>
2025-06-04 16:21:17 +0100

    Update query-list-item.model.spec.ts

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/models/query-list/query-list-item/query-list-item.model.spec.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/query-management/models/query-list/query-list-item/query-list-item.model.spec.ts
@@ -98,13 +98,7 @@ describe('QueryListItem', () => {
 
   describe('lastSubmittedBy', () => {
     it('should return the name of the person of the lastSubmittedMessage', () => {
-      const index = queryListItem.children.length === 1
-        ? 0
-        : queryListItem.children.length % 2 === 1
-          ? queryListItem.children.length - 1
-          : queryListItem.children.length - 2;
-
-      expect(queryListItem.lastSubmittedBy).toEqual(queryListItem.children[index].name);
+      expect(queryListItem.lastSubmittedBy).toEqual(lastSubmittedBy.name);
     });
   });
 
