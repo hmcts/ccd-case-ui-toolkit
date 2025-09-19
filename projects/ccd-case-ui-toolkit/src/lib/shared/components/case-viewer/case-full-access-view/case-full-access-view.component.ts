22b5b58608b9c096f917b0ac50aa838034c20ca6
RiteshHMCTS <ritesh.dsouza@hmcts.net>
2025-09-19 17:31:01 +0100

    fix for markup event link

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/case-viewer/case-full-access-view/case-full-access-view.component.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/case-viewer/case-full-access-view/case-full-access-view.component.ts
@@ -136,6 +136,8 @@ export class CaseFullAccessViewComponent implements OnInit, OnDestroy, OnChanges
 
     this.checkRouteAndSetCaseViewTab();
 
+    this.setCaseInfo();
+
     // Check for active Case Flags
     this.activeCaseFlags = this.hasActiveCaseFlags();
     this.linkedCasesService.resetLinkedCaseData();
@@ -147,6 +149,19 @@ export class CaseFullAccessViewComponent implements OnInit, OnDestroy, OnChanges
       this.init();
       this.crf.detectChanges();
       this.organiseTabPosition();
+    }
+  }
+
+  private setCaseInfo(): void {
+    const caseInfo = JSON.parse(this.sessionStorageService.getItem('caseInfo') || '{}');
+    console.log('Case Info from session storage: ', caseInfo);
+    if (caseInfo?.caseId !== this.caseDetails.case_id) {
+      const newCaseInfo = {
+        caseId: this.caseDetails.case_id,
+        jurisdiction: this.caseDetails.case_type.jurisdiction.id,
+        caseType: this.caseDetails.case_type.id
+      };
+      this.sessionStorageService.setItem('caseInfo', JSON.stringify(newCaseInfo));
     }
   }
 
