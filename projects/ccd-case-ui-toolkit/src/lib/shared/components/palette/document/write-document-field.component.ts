0c591523ddeb01c5f1edf7c203301316cfc5f423
Josh <josh.glasgow@hmcts.net>
2025-11-17 16:32:12 +0000

    Get casetype from url after new url changes

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/document/write-document-field.component.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/document/write-document-field.component.ts
@@ -70,20 +70,20 @@ export class WriteDocumentFieldComponent extends AbstractFieldWriteComponent imp
 
   public ngOnInit(): void {
     // Wait for both observables to emit at least once
+    const currUrl = window.location.pathname;
+    if (currUrl.indexOf('/case-details/') > -1) {
+      this.caseTypeId = currUrl.split('/')[4];
+    }
     this.caseNotifierSubscription = combineLatest([
       this.caseNotifier.caseView.pipe(take(1)),
       this.jurisdictionService.getSelectedJurisdiction()
     ]).subscribe(([caseDetails, jurisdiction]) => {
       if (caseDetails) {
-        this.caseTypeId = caseDetails?.case_type?.id;
         this.jurisdictionId = caseDetails?.case_type?.jurisdiction?.id;
         this.caseId = caseDetails?.case_id;
       }
       if (jurisdiction) {
         this.jurisdictionId = jurisdiction.id;
-        if (jurisdiction.currentCaseType) {
-          this.caseTypeId = jurisdiction.currentCaseType.id;
-        }
       }
       //if we havent set the value of caseTypeId yet, we can check if its in the url. e.g. case-creation
       if (!this.caseTypeId) {
