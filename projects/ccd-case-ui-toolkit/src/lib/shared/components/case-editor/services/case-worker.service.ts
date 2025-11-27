a0a671c28aa76a9d71e8e43617b0a0594821d3ec
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2025-11-27 11:46:30 +0000

    Add version and remove caseworkersbyservice

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/case-editor/services/case-worker.service.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/case-editor/services/case-worker.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { AbstractAppConfig } from '../../../../app.config';
-import { Caseworker, CaseworkersByService } from '../../../domain/work-allocation/case-worker.model';
+import { Caseworker } from '../../../domain/work-allocation/case-worker.model';
 import { HttpErrorService } from '../../../services/http/http-error.service';
 import { HttpService } from '../../../services/http/http.service';
 
