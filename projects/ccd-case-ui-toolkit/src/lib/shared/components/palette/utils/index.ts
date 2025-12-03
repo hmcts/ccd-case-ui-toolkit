cbc569ccdfa8dcbaff6b8af33fa0fc9fe1caf879
Chris Jones <christopher.jones@hmcts.net>
2025-12-03 13:38:42 +0000

    added missing export

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/utils/index.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './dash.pipe';
 export * from './date.pipe';
+export * from './text-date-time.pipe';
 export * from './field-label.pipe';
 export * from './first-error.pipe';
 export * from './is-compound.pipe';
