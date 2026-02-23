a3b63eb3d16cec7fffc16e8586c0ba0cb7797653
padmapriyanalam <67232857+padmapriyanalam@users.noreply.github.com>
2026-02-23 12:53:22 +0000

    Add spaces

--- projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/money-gbp/money-gbp-input.component.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/money-gbp/money-gbp-input.component.ts
@@ -2,8 +2,8 @@ import { Component, forwardRef, Input } from '@angular/core';
 import { ControlValueAccessor, FormControl, NG_VALIDATORS, NG_VALUE_ACCESSOR, ValidationErrors, Validator } from '@angular/forms';
 
 @Component({
-  selector: 'ccd-money-gbp-input',
-  template: `<input class="form-control form-control-1-8"
+    selector: 'ccd-money-gbp-input',
+    template: `<input class="form-control form-control-1-8"
                     type="text"
                     [id]="id"
                     [name]="name"
