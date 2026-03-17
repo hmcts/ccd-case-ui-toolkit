9ddc546a1319104a1bbde2760949bc3dacf7ce4f
Chris Jones <christopher.jones@hmcts.net>
2026-03-17 12:34:41 +0000

    added case-flag-priority.utils from exui-4090

---
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/case-flag/utils/case-flag-priority.utils.ts
@@ -0,0 +1,59 @@
+import { FlagDetail, FlagsWithFormGroupPath } from '../domain';
+import { CaseFlagStatus } from '../enums';
+
+export const PVP_FLAG_CODE = 'PF0021';
+export const PVP_DISPLAY_TEXT = 'POTENTIALLY VIOLENT PERSON';
+
+export function isPvpFlag(flagDetail: FlagDetail): boolean {
+  return flagDetail?.flagCode === PVP_FLAG_CODE;
+}
+
+export function hasPvpFlag(flagsWithFormGroupPath: FlagsWithFormGroupPath): boolean {
+  return !!flagsWithFormGroupPath?.flags?.details?.some(isPvpFlag);
+}
+
+export function isActivePvpFlag(flagDetail: FlagDetail): boolean {
+  return isPvpFlag(flagDetail) && flagDetail?.status === CaseFlagStatus.ACTIVE;
+}
+
+export function hasActivePvpFlag(flagsWithFormGroupPath: FlagsWithFormGroupPath): boolean {
+  return !!flagsWithFormGroupPath?.flags?.details?.some(isActivePvpFlag);
+}
+
+export function prioritisePvpFlags(flagDetails: FlagDetail[] = []): FlagDetail[] {
+  const activePvpFlags: FlagDetail[] = [];
+  const remainingFlags: FlagDetail[] = [];
+
+  flagDetails.forEach((flagDetail) => {
+    if (isActivePvpFlag(flagDetail)) {
+      activePvpFlags.push(flagDetail);
+    } else {
+      remainingFlags.push(flagDetail);
+    }
+  });
+
+  return [...activePvpFlags, ...remainingFlags];
+}
+
+export function prioritisePvpParties(partyFlagsData: FlagsWithFormGroupPath[] = []): FlagsWithFormGroupPath[] {
+  const pvpParties: FlagsWithFormGroupPath[] = [];
+  const nonPvpParties: FlagsWithFormGroupPath[] = [];
+
+  partyFlagsData.forEach((partyFlags) => {
+    const prioritisedPartyFlags: FlagsWithFormGroupPath = {
+      ...partyFlags,
+      flags: {
+        ...partyFlags.flags,
+        details: partyFlags?.flags?.details ? prioritisePvpFlags(partyFlags.flags.details) : partyFlags?.flags?.details
+      }
+    };
+
+    if (hasActivePvpFlag(prioritisedPartyFlags)) {
+      pvpParties.push(prioritisedPartyFlags);
+    } else {
+      nonPvpParties.push(prioritisedPartyFlags);
+    }
+  });
+
+  return [...pvpParties, ...nonPvpParties];
+}
