d5d4456be6fb5faa282807d81e7693af065ce906
RiteshHMCTS <ritesh.dsouza@hmcts.net>
2026-06-04 12:01:09 +0100

    test

--- src/app.config.ts
+++ src/app.config.ts
@@ -8,9 +8,6 @@ export interface AccessManagementBasicViewMockModel {
   };
   accessProcess?: string;
 }
-
-const toolkitPrepushLintFailure = "lint-test";
-
 export interface AccessManagementRequestReviewMockModel {
   active?: boolean;
   details?: {
