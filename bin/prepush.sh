54ca373ec734d050189d64faad11e6f64eaae055
RiteshHMCTS <ritesh.dsouza@hmcts.net>
2026-06-08 15:48:17 +0100

    updated error message

--- bin/prepush.sh
+++ bin/prepush.sh
@@ -6,7 +6,7 @@ lint_status=$?
 if [ "$lint_status" -ne 0 ]; then
   printf "=============================================================\n" >&2
   printf "The following command failed: lint\n" >&2
-  printf "Fix the lint errors shown above, then push again.\n" >&2
+  printf "Fix the lint errors, then push again.\n" >&2
   printf "=============================================================\n" >&2
   exit "$lint_status"
 fi
