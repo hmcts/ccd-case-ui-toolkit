01392f84bf54739f72015911b6a9affb31624e30
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2026-07-10 15:26:35 +0100

    Make changes based on PR comments

--- projects/ccd-case-ui-toolkit/src/lib/shared/utils.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/utils.ts
@@ -1,26 +1,26 @@
 import { SessionStorageService } from './services';
 import { safeJsonParse } from './json-utils';
+import { UserInfo } from './domain/user/user-info.model';
 
 export const USER_DETAILS = 'userDetails';
 export const PUI_CASE_MANAGER = 'pui-case-manager';
 export const JUDGE = 'judge';
 
 
-function getUserDetails(sessionStorageService: SessionStorageService): any {
+export function getUserDetails(sessionStorageService: SessionStorageService): UserInfo | null {
   const item = sessionStorageService?.getItem(USER_DETAILS);
   return safeJsonParse(item, null);
 }
 
-
 export function isInternalUser(sessionStorageService: SessionStorageService): boolean {
   const userDetails = getUserDetails(sessionStorageService);
-  return userDetails && userDetails?.roles
+  return !!userDetails && userDetails?.roles
     && !(userDetails.roles.includes(PUI_CASE_MANAGER)
       || userDetails.roles.some((role: string) => role.toLowerCase().includes(JUDGE)));
 }
 
 export function isJudiciaryUser(sessionStorageService: SessionStorageService): boolean {
   const userDetails = getUserDetails(sessionStorageService);
-  return userDetails && userDetails?.roles
+  return !!userDetails && userDetails?.roles
     && (userDetails.roles.some((role: string) => role.toLowerCase().includes(JUDGE)));
 }
