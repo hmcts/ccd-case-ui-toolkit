cefeb6ed1d5cbe3b3d1052e12e35c1c9aefc5637
chrisjones-hmcts <christopher.jones@hmcts.net>
2026-09-07 11:05:10 +0100

    EXUI-5063: Staff typeahead (#2247)
    * add staff user field contract
    
    * add role category search configuration
    
    * add cached staff user search
    
    * add staff user case field shell
    
    * implement staff user type ahead
    
    * register staff user palette support
    
    * use service names for staff user search
    
    * tidied up code
    
    * added email suffix to staff user in typeahead component
    
    * updated package version
    
    * fix for pallatte service
    
    * rework to use displayContextParameter instead of roleCategories
    
    * added unit tests
    
    * added null/undefined guard to subscriptions in constructor
    
    * removed displayName value from form data layer
    
    * fix for complexGroup form control
    
    * removed regions from search config and wip read-staff component
    
    * fix for getUserByIdamId bug
    
    * fix for loading selected staff user when editing a case
    
    * fix for duplicate api calls when selecting a staffuser
    
    * removed logs and tidied up code
    
    * fix for displaying selected judicial users
    
    * added fix for loading selected jucicial user in edit or read journeys
    
    * added loading spinner to read-staff-user component
    
    * tidied up code
    
    * fix for unit tests
    
    * updated unit tests
    
    * tidied up code
    
    * fix for sonar issues
    
    * fix for sonar issue
    
    * fix for sonar issues
    
    * fix for override
    
    * reverted hardcoded credentials and updated version number
    
    ---------
    
    Co-authored-by: connorpgpmcelroy <connor.mcelroy@hmcts.net>

---
+++ projects/ccd-case-ui-toolkit/src/lib/shared/components/palette/staff-user/index.ts
@@ -0,0 +1,2 @@
+export * from './read-staff-user-field.component';
+export * from './write-staff-user-field.component';
--- projects/ccd-case-ui-toolkit/src/lib/shared/domain/definition/field-type-enum.model.ts
+++ projects/ccd-case-ui-toolkit/src/lib/shared/domain/definition/field-type-enum.model.ts
@@ -32,4 +32,5 @@ export type FieldTypeEnum =
   | 'FlagDetail'
   | 'FlagLauncher'
   | 'CaseFlag' // Temporary field type for faking Flags field type; to be removed
-  | 'JudicialUser';
+  | 'JudicialUser'
+  | 'StaffUser';
