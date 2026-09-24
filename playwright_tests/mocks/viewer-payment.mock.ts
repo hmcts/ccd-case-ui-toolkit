88531db4d34747051e685e10775a065b4cc09e36
Andrew Grizhenkov <andrew.grizhenkov@hmcts.net>
2026-09-24 13:10:44 +0100

    test: use payment amount contract

--- playwright_tests/mocks/viewer-payment.mock.ts
+++ playwright_tests/mocks/viewer-payment.mock.ts
@@ -3,7 +3,7 @@ import { CaseField } from '../../projects/ccd-case-ui-toolkit/src/public-api';
 export const orderSummaryField = Object.assign(new CaseField(), {
   id: 'order-summary', label: 'Order summary', display_context: 'READONLY',
   field_type: { id: 'OrderSummary', type: 'OrderSummary' },
-  value: { Fees: [{ value: { FeeCode: 'FEE1', FeeDescription: 'Application fee', FeeAmount: '100.00' } }], PaymentTotal: '100.00' }
+  value: { Fees: [{ value: { FeeCode: 'FEE1', FeeDescription: 'Application fee', FeeAmount: '10000' } }], PaymentTotal: '10000' }
 });
 
 export const paymentHistoryField = Object.assign(new CaseField(), {
