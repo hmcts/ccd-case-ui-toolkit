f7a5573ab66a02900fc7a6ba44cd436aa6802c83
Andrew Grizhenkov <andrew.grizhenkov@hmcts.net>
2026-09-24 16:31:39 +0100

    Revert "test: wait for packaged consumer bundle"
    This reverts commit 2867eafdf8a19edffc5afb128be0e183ada2879f.

--- packaged-consumer/playwright.config.ts
+++ packaged-consumer/playwright.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  expect: { timeout: 30_000 },
   retries: 0,
   workers: 1,
   use: { baseURL: 'http://127.0.0.1:4301', video: 'off' },
-  webServer: { command: 'npm start', url: 'http://127.0.0.1:4301/main.js', reuseExistingServer: false }
+  webServer: { command: 'npm start', url: 'http://127.0.0.1:4301', reuseExistingServer: false }
 });
